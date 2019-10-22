13e6f2bfd0339d2012d76b07dd8295b426bb9beb
Chanty He <chanty.he90@googlemail.com>
2019-10-22 21:24:00 +0200

    1st iteration

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import IdCard from "./components/IdCard";
+import Data from "./data/berlin.json";
 
 class App extends Component {
   render() {
+    console.log("Data", Data);
     return (
       <div className="App">
         <h1>IdCard</h1>
-        {/* TODO: Use the IdCard component */}
-
+        {Data.map(e => {
+          return <IdCard birthDay={new Date("1992-07-14")} data={e} />;
+        })}
         <h1>Greetings</h1>
+
         {/* TODO: Use the Greetings component */}
       </div>
     );
---
+++ starter-code/src/components/IdCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class IdCard extends Component {
+  render() {
+    console.log("this.props", this.props);
+    console.log("this.props.birth", this.props.birth);
+    return (
+      <div className="Id" style={{ border: "1px solid black" }}>
+        <p></p>
+        <h1>{this.props.data.firstName}</h1>
+        <h1>{this.props.data.lastName}</h1>
+        <h1>{this.props.data.country}</h1>
+        <h1>{this.props.birthDay.toString()}</h1>
+      </div>
+    );
+  }
+}
+
+export default IdCard;
