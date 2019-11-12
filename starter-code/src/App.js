1f2dc2abae83591981176cd9437c5e5cd69cd0fe
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-11-12 17:58:07 -0500

    'CreditCard' component in process

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,6 +3,7 @@ import IDcard from "./Comps/IDcard";
 import Greetings from "./Comps/Greetings";
 import Random from "./Comps/Random";
 import BoxColor from "./Comps/BoxColor";
+import CreditCard from "./Comps/CreditCard";
 
 export default class App extends Component {
   render() {
@@ -25,19 +26,66 @@ export default class App extends Component {
           birth={String(new Date("1988-10-21"))}
           picture="https://randomuser.me/api/portraits/men/21.jpg"
         />
-
+        <br />
+        <br />
+        <br /> <br />
+        <br /> <br />
         <h1>Greetings Component:</h1>
         <Greetings lang="en">Good Morning</Greetings>
         <Greetings lang="es">Buenos días</Greetings>
         <Greetings lang="fr">Bonjour</Greetings>
-
+        <br />
+        <br />
+        <br /> <br />
+        <br /> <br />
         <h1>Random Component:</h1>
         <Random min={1} max={4} />
         <Random min={50} max={100} />
-
+        <br />
+        <br />
+        <br /> <br />
+        <br /> <br />
         <h1>BoxColor Component:</h1>
-        <BoxColor r={75} g={161} b={181} /><BoxColor r={97} g={183} b={203} /><BoxColor r={124} g={205} b={223} />
-        <BoxColor r={161} g={224} b={238} /><BoxColor r={194} g={240} b={250} /><BoxColor r={228} g={246} b={250} />
+        <BoxColor r={75} g={161} b={181} />
+        <BoxColor r={97} g={183} b={203} />
+        <BoxColor r={124} g={205} b={223} />
+        <BoxColor r={161} g={224} b={238} />
+        <BoxColor r={194} g={240} b={250} />
+        <BoxColor r={228} g={246} b={250} />
+        <br />
+        <br />
+        <br /> <br />
+        <br /> <br />
+        <CreditCard
+          type="visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white"
+        />
+        <CreditCard
+          type="master card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        />
+        <CreditCard
+          type="master card"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white"
+        />
       </div>
     );
   }
---
+++ starter-code/src/Comps/CreditCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function CreditCard(props) {
+  return props.type === "visa" ? (
+    <div className="card">
+      <img
+        className="credit"
+        src="https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.com/img/visa.png"
+      />
+    </div>
+  ) : (
+    <div className="card">
+      <img
+        className="credit"
+        src="https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.com/img/master-card.svg"
+      />
+    </div>
+  );
+}
