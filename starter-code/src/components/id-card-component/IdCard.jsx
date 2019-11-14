eba0045aa6b64782af9621a9e69c16e9ba20e60e
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-14 16:24:40 -0500

    add folders to src

---
+++ starter-code/src/components/id-card-component/IdCard.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export default class IdCard extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={this.props.picture} alt="picture" />
+        </div>
+        <div>
+          <p>
+            <span>First Name: </span>
+            {this.props.firstName}
+          </p>
+          <p>
+            <span>Last Name: </span>
+            {this.props.lastName}
+          </p>
+          <p>
+            <span>Gender: </span>
+            {this.props.gender}
+          </p>
+          <p>
+            <span>Height: </span>
+            {this.props.height / 100}m
+          </p>
+          <p>
+            <span>Birth: </span>
+            {this.props.birth.toDateString()}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
