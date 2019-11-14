2cf81db576a21e4ab5ebaca18943274a46c53759
mendosaurus <masaki.endo@nex.nikkei.com>
2019-11-14 17:14:26 -0500

    BoxColor

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import IdCard from "./components/id-card-component/IdCard";
 import Greetings from "./components/Greetings-component/Greetings";
 import Random from "./components/Random-component/Random";
+import BoxColor from "./components/BoxColor-component/BoxColor";
 
 class App extends Component {
   render() {
@@ -35,6 +36,10 @@ class App extends Component {
         <h1>Random</h1>
         <Random min={1} max={6} />
         <Random min={1} max={100} />
+
+        <h1>BoxColor</h1>
+        <BoxColor r={255} g={0} b={0} />
+        <BoxColor r={128} g={255} b={0} />
       </div>
     );
   }
