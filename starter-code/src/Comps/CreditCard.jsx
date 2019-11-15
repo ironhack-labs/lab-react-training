042f6471c5d20778e49cfacd7aa1a56abda152b7
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-11-15 16:01:41 -0500

    Modify bgColor and CreditPics

--- starter-code/src/Comps/CreditCard.jsx
+++ starter-code/src/Comps/CreditCard.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 
 export default function CreditCard(props) {
-  return props.type === "visa" ? (
-    <div className="card">
-      <img
-        className="credit"
-        src="https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.com/img/visa.png"
-      />
-    </div>
-  ) : (
-    <div className="card">
-      <img
-        className="credit"
-        src="https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.com/img/master-card.svg"
-      />
-    </div>
-  );
+  return props.type === "visa"
+    ? <div className="card" style={{ backgroundColor: props.bgColor }}>
+        <img
+          className="credit"
+          src="https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.com/img/visa.png"
+        />
+      </div>
+    : <div className="card" style={{ backgroundColor: props.bgColor }}>
+        <img
+          className="credit"
+          src="https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.com/img/master-card.svg"
+        />
+      </div>;
 }
