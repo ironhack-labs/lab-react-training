c4a806fcbbb3942b7ceb22cc4e9f943d7f3f8591
Jeferson Silva <jeferson.s.silva1@gmail.com>
2020-01-14 15:12:16 -0300

    Boxcolor

---
+++ starter-code/src/components/BoxColor.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./BoxColor.css";
+
+class BoxColor extends React.Component{
+  render(){
+
+    const divStyle = {
+      color: 
+    };
+
+    return(
+      <div className="BoxColor">
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default BoxColor;
