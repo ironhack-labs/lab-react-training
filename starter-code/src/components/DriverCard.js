a025de1286ff2dd25be1c9d5b08c593ff055a9d7
Víctor Peña Romero <vic.pero@icloud.com>
2020-02-18 17:41:54 -0600

    Add DriverCard

--- starter-code/src/components/DriverCard.js
+++ starter-code/src/components/DriverCard.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 function DriverCard(props) {
+
+    let rating = Math.round(props.rating)
+    let render = []
+
+    for (let i = 0; i <= rating - 1; i++) {
+        render.push('★')
+    }
+
+    for (let i = render.length; i < 5; i++) {
+        render.push('☆')
+    }
+
     return (
-        <div>
-            <h6>{props.name}</h6>
+        <div className="driver">
+            <img src={props.img} alt={props.name}/>
+            <div>
+                <h2>{props.name}</h2>
+                <h2>{render}</h2>
+                <p>{props.car.model} - {props.car.licensePlate}</p>
+
+            </div>
         </div>
     )
 }
