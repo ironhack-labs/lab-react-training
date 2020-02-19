198475d6c7a22d53a954a1845bed6fcf322d107c
Gustavo Cruz <gustavocruzb@outlook.com>
2020-02-18 20:47:39 -0600

    iteration 1 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import IdCard from './IdCard'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>IdCard</h1>
-        {/* TODO: Use the IdCard component */}
-
-        <h1>Greetings</h1>
-        {/* TODO: Use the Greetings component */}
-      </div>
+      <Fragment>
+        <IdCard 
+          lastName='Doe'
+          firstName='John'
+          gender='male'
+          height={178}
+          birth={new Date("1992-07-14")}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+        <IdCard 
+          lastName='Delores '
+          firstName='Obrien'
+          gender='female'
+          height={172}
+          birth={new Date("1988-05-11")}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </Fragment>
     );
   }
 }
---
+++ starter-code/src/IdCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const IdCard = props => {
+    return (
+        <div className="card">
+            <img className="card-image" src={props.picture} />
+            <div className="card-info">
+                <p>First Name: <span>{props.firstName}</span></p>
+                <p>Last Name: <span>{props.lastName}</span></p>
+                <p>Gender: <span>{props.gender}</span></p>
+                <p>Height: <span>{props.height}</span></p>
+                <p>Birth: <span>{props.birth.toDateString()}</span></p>
+            </div>
+        </div>
+    )
+};
+
+export default IdCard;
