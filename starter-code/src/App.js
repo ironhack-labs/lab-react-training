a97b35bd49d918a1c401ae089b6e4a54edbdf4f7
Víctor Peña Romero <vic.pero@icloud.com>
2020-02-18 18:23:21 -0600

    Add LikeButton

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,6 +7,7 @@ import BoxColor from './components/BoxColor'
 import CreditCard from './components/CreditCard'
 import Rating from './components/Rating'
 import DriverCard from './components/DriverCard'
+import LikeButton from './components/LikeButton'
 
 class App extends Component {
   render() {
@@ -101,6 +102,9 @@ class App extends Component {
         model: "Audi A3",
         licensePlate: "BE33ER"
         }} />
+
+        <h1>LikeButton</h1>
+        <LikeButton /> <LikeButton />
 
       </div>
     );
