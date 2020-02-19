24a02068a2e2ea0b33c8ff6315a09fd97d05c11c
Ale Ochoa <ale.ochoafi@gmail.com>
2020-02-18 21:59:36 -0600

    Raiting

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,6 +4,7 @@ import Greetings from './components/Greetings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 class App extends Component {
   render() {
@@ -72,6 +73,14 @@ class App extends Component {
           bgColor="#ddbb55"
           color="white" />
       </div>
+
+      <h1>Rating</h1>
+      <Rating>0</Rating>
+<Rating>1.49</Rating>
+<Rating>1.5</Rating>
+<Rating>3</Rating>
+<Rating>4</Rating>
+<Rating>5</Rating>
       </div>
     );
   }
---
+++ starter-code/src/components/Rating.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Rating extends Component { 
+  render() {
+    return (
+      <div id="Box">
+        <p>{Math.round(this.props.children)>=1 ? '★':'☆'}
+        {Math.round(this.props.children)>=2 ? '★':'☆'}
+        {Math.round(this.props.children)>=3 ? '★':'☆'}
+        {Math.round(this.props.children)>=4 ? '★':'☆'}
+        {Math.round(this.props.children)>=5 ? '★':'☆'}</p>
+      </div>
+    );
+  }
+}
