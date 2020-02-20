f3b8d7077ff615e8c6bc5f8560cc1ef50b47254a
Tonalli <tonalli@xanum.uam.mx>
2020-02-20 12:57:51 -0600

    Add BoxColor component

---
+++ starter-code/src/components/BoxColor.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BoxColor = (props) => {
+    const {r, g, b} = props;
+    const colors = `rgb(${r}, ${g}, ${b})`;
+    const value = {
+        "backgroundColor": colors,
+    };
+
+    return (
+        <div className="BoxColor box" style={value}>
+            {colors}<br/>
+        </div>
+    );
+};
+
+export default BoxColor;
