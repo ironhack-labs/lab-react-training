030ac1ada5fc5abb5a3875d4d5c73dde1a9489f9
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-02-22 12:15:57 -0600

    Fix Random component

--- starter-code/src/components/Random.jsx
+++ starter-code/src/components/Random.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Random = (props) => {
     const min = props.min;
     const max = props.max;
-    const number = min + Math.floor(Math.random() * max);
+    const number = min + Math.floor(Math.random() * (max - min));
 
     return (
         <div className="box">Random value between {min} and {max} =&gt; {number}</div>
