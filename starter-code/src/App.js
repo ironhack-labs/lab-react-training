213b9e0154dd7150e821e1956110590c18cd5334
Gustavo <gustavocruzb@outlook.com>
2020-02-22 14:58:49 -0600

    Credit Card done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,6 +3,7 @@ import IdCard from './IdCard';
 import Greetings from './Greetings';
 import Random from './Random';
 import BoxColor from './BoxColor';
+import CreditCard from './CreditCard';
 
 class App extends Component {
   render() {
@@ -34,6 +35,36 @@ class App extends Component {
         <h1>Box Color</h1>
         <BoxColor r={255} g={0} b={0} />
         <BoxColor r={128} g={255} b={0} />
+        <h1>Credit Card</h1>
+        <div class="card-container">
+          <CreditCard 
+            type="Visa"
+            number="0123456789018845"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="BNP"
+            owner="Maxence Bouret"
+            bgColor="#11aa99"
+            color="white" />
+          <CreditCard 
+            type="Master Card"
+            number="0123456789010995"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="N26"
+            owner="Maxence Bouret"
+            bgColor="#eeeeee"
+            color="#222222" />
+          <CreditCard 
+            type="Visa"
+            number="0123456789016984"
+            expirationMonth={12}
+            expirationYear={2019}
+            bank="Name of the Bank"
+            owner="Firstname Lastname"
+            bgColor="#ddbb55"
+            color="white" />
+          </div>
         </Fragment>
     );
   }
