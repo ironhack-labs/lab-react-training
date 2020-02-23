98aa92eba1c681826ad19a55235fcb52fe1783b3
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-02-23 15:26:48 -0600

    Finish DriverCard component

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -90,6 +90,14 @@ class App extends Component {
             model: "Toyota Corolla Altis",
             licensePlate: "CO42DE"
           }} />
+          <DriverCard 
+            name="Dara Khosrowshahi"
+            rating={4.9}
+            img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+            car={{
+              model: "Audi A3",
+              licensePlate: "BE33ER"
+            }} />
         
       </div>
     );
--- starter-code/src/components/DriverCard.jsx
+++ starter-code/src/components/DriverCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import Rating from "./Rating";
 
 const DriverCard = (props) => {
   const {name, rating, img, car} = props;
 
   return (
     <div className="DriverCard">
-      <img className="left" src="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&amp;height=428" alt="Driver"/>
+      <img className="left" src={img} alt="Driver"/>
       <div className="right">
-        <h2>Travis Kalanick</h2>
-        <div className="Rating">★★★★☆</div>
-        <p>Toyota Corolla Altis - CO42DE</p>
+        <h2>{name}</h2>
+        <div className="Rating">
+          <Rating>{rating}</Rating>
+        </div>
+        <p>{car.model} - {car.licensePlate}</p>
       </div>
     </div>
   );
