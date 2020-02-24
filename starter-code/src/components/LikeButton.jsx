d06405cdab61f36cbf7d490cdf06b0793dc7dae4
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-02-23 18:23:55 -0600

    Add color to LikeButton component

--- starter-code/src/components/LikeButton.jsx
+++ starter-code/src/components/LikeButton.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 
+let colors = ['purple','blue','green','yellow','orange','red'];
+
 class LikeButton extends React.Component {
     state = {
-        numberOfLikes: 0
+        numberOfLikes: 0,
+        color: colors[0]
     }
 
     handleClick = () => {
+        let newNumber = this.state.numberOfLikes + 1;
         this.setState({
-            numberOfLikes: this.state.numberOfLikes + 1
+            numberOfLikes: newNumber,
+            color: colors[newNumber % colors.length]
         })
     };
 
     render() {
+        const style = {
+            backgroundColor: this.state.color,
+            color: "white",
+            padding: "15px 32px",
+            margin: "4px 2px"
+        }
         return (
-            <button onClick={this.handleClick}>{this.state.numberOfLikes} likes</button>
+            <button style={style} onClick={this.handleClick}>{this.state.numberOfLikes} likes</button>
         );
     }
 }
