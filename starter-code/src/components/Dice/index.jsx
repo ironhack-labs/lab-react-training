0cd86ba6a4779a1919d1184942272ef385b0ec93
TemidoRocha <manueltemidorocha@gmail.com>
2020-03-07 17:55:43 +0000

    dice iteration

--- starter-code/src/components/Dice/index.jsx
+++ starter-code/src/components/Dice/index.jsx
@@ -1,13 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+// img={process.env.PUBLIC_URL + './img/persons/maxence.png'}
 
-export class index extends Component {
+class index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dice: './img/dice-empty.png'
+    };
+    this.changePicture = this.changePicture.bind(this);
+  }
+  changePicture() {
+    this.setState({
+      dice: './img/dice-empty.png'
+    });
+    setTimeout(x => {
+      let diceImg = '';
+      switch (Math.floor(Math.random() * 6) + 1) {
+        case 1:
+          diceImg = './img/dice1.png';
+          break;
+        case 2:
+          diceImg = './img/dice2.png';
+          break;
+        case 3:
+          diceImg = './img/dice3.png';
+          break;
+        case 4:
+          diceImg = './img/dice4.png';
+          break;
+        case 5:
+          diceImg = './img/dice5.png';
+          break;
+        default:
+          diceImg = './img/dice6.png';
+          break;
+      }
+      this.setState({
+        dice: diceImg
+      });
+    }, 1000);
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
-    )
+      <button onClick={this.changePicture}>
+        <img src={process.env.PUBLIC_URL + this.state.dice} alt="hello" />;
+      </button>
+    );
   }
 }
 
-export default index
+export default index;
