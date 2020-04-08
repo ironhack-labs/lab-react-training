e85ee8e349125cea75db3c6d0ff65756268549dc
Carles Galí Bou <carles_ga@hotmail.com>
2020-04-08 12:40:21 +0200

    Iteration 4 done, I did Catalonia flag for enjoying a little bit doing this exercise

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random';
-// import BoxColor from './components/BoxColor';
+import BoxColor from './components/BoxColor';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="titles">IdCard</h1>
-        {/* TODO: Use the IdCard component */}
         <IdCard
         lastName="Doe"
         firstName="John"
@@ -26,15 +25,27 @@ class App extends Component {
         picture="https://randomuser.me/api/portraits/women/44.jpg"/>
 
         <h1 className="titles">Greetings</h1>
-        {/* TODO: Use the Greetings component */}
         <Greetings lang="es">Carles</Greetings>
         <Greetings lang="en">Susi</Greetings>
         <Greetings lang="de">Berni</Greetings>
 
         <h1 className="titles">Random Number</h1>
-        {/* TODO: Use the Random component */}
         <Random min={1} max={6}/>
         <Random min={1} max={100}/>
+
+        <h1 className="titles">Box Color</h1>
+        <div className="flag">
+          <BoxColor r={255} g={255} b={0} />
+          <BoxColor r={255} g={0} b={0} />
+          <BoxColor r={255} g={255} b={0} />
+          <BoxColor r={255} g={0} b={0} />
+          <BoxColor r={255} g={255} b={0} />
+          <BoxColor r={255} g={0} b={0} />
+          <BoxColor r={255} g={255} b={0} />
+          <BoxColor r={255} g={0} b={0} />
+          <BoxColor r={255} g={255} b={0} />
+        </div>
+        
 
 
       </div>
--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -3,8 +3,12 @@ import React, { Component} from 'react';
 
 class BoxColor extends Component {
   render() {
+    const { r, g, b } = this.props;
+    const divstyle = {
+      backgroundColor: `rgb(${r}, ${g}, ${b})`,
+    };
     return (
-
+    <div style={divstyle} className="box-color"><p className="rgb-color-text">rgb({ r }, { g }, { b })</p></div>
     )
   }
 }
