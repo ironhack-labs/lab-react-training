ca18cbafb22322e623f1cac05f9f398defdaf165
Pablolo <pablo_olondriz@hotmail.com>
2020-04-08 20:15:56 +0200

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import IdCard from './components/idcard/idcard';
 import Greetings from './components/greetings';
 import Random from './components/random';
+import BoxColor from './components/boxcolor';
 import './App.css';
 
 class App extends Component {
@@ -35,6 +36,9 @@ class App extends Component {
         <Random min={1} max={100}/>
 
         <h1>BoxColor</h1>
+        <BoxColor r={255} g={0} b={0} />
+        <BoxColor r={128} g={255} b={0} />
+
         <h1>CreditCard</h1>
         <h1>Rating</h1>
         <h1>DriverCard</h1>
---
+++ src/components/boxcolor.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class BoxColor extends Component {
+  
+  componentToHex = (c) => {
+    let hex = c.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  }
+  
+  rgbToHex = (r, g, b) => {
+    return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+  }
+  
+  render () {
+    const { r, g, b } = this.props;
+    return (
+      <div style={
+        {
+        'background-color': [`rgb(${r}, ${g}, ${b})`], 
+        'border': '1px solid black',
+        'padding': '0 0.5rem',
+        'margin': '0 20rem 2rem',
+        'font-size': '2rem',
+        'text-align': 'center',
+        }
+      }>
+        <p>rgb({r}, {g}, {b})</p>
+        <p>{this.rgbToHex(r, g, b)}</p>
+      </div>
+    )
+  }
+}
+
+export default BoxColor;
