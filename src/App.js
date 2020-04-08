8a7aa93f4d77473a6af91fee6321f79f531345ae
Carles Galí Bou <carles_ga@hotmail.com>
2020-04-08 10:55:40 +0200

    Iteration 1 done. I had problems to render correctly the date

--- src/App.js
+++ src/App.js
@@ -9,10 +9,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>IdCard</h1>
+        <h1 className="titles">IdCard</h1>
         {/* TODO: Use the IdCard component */}
-
-        <h1>Greetings</h1>
+        <IdCard
+        lastName="Doe"
+        firstName="John"
+        gender="male"
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"/>
+        <IdCard lastName="Delores"
+        firstName="Obrien"
+        gender="female"
+        height={72}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"/>
+        <h1 className="titles">Greetings</h1>
         {/* TODO: Use the Greetings component */}
       </div>
     );
--- src/components/IdCard.js
+++ src/components/IdCard.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 
 
 class IdCard extends Component {
+  state = {
+  }
   render() {
+    const { picture, lastName, firstName, gender, height, birth } = this.props;
     return (
-
+      <div>
+        <div className="IdCard-box">
+        <img className="profile-img" src={ picture } alt="Profile pic"/> 
+          <div>
+            <p><strong>Last name:</strong> { lastName }</p>
+            <p><strong>First ame:</strong> { firstName }</p>
+            <p><strong>Gender:</strong> { gender }</p>
+            <p><strong>Height:</strong> { height }</p>
+            <p><strong>Birth:</strong> { birth.toDateString() }</p>
+          </div>
+        </div>
+      </div>
     )
   }
 }
