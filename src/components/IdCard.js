341d40ecd97f95704f5cf8cda5e7d5e068041806
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-04-09 13:25:28 +0200

    CSS firts iteration done

--- src/components/IdCard.js
+++ src/components/IdCard.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class IdCard extends Component {
     render() {
         return (
-            <div>
-                <img src={this.props.picture} alt='user'></img>
-                <div>
+            <div className='user-container'>
+                <div className='user-image'>
+                    <img src={this.props.picture} alt='user'></img>
+                </div>
+                <div className='user-information'>
                     <p>First name: {this.props.firstName}</p>
                     <p>Last name: {this.props.lastName}</p>
                     <p>Gender: {this.props.gender}</p>
@@ -13,9 +15,9 @@ class IdCard extends Component {
                     <p>Height: {this.props.birth}</p>
                 </div>
             </div>
-                )
-            }
-        }
-        
-        
+        )
+    }
+}
+
+
 export default IdCard;
