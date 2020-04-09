f5661297df8321e0ee05d2a472e803e2e925d697
Pablolo <pablo_olondriz@hotmail.com>
2020-04-09 11:44:51 +0200

    iteration 6 done

--- src/components/creditcard.js
+++ src/components/creditcard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+ 
 class CreditCard extends Component {
   
   cardType = (type) => {
@@ -34,7 +34,7 @@ class CreditCard extends Component {
   render () {
     const { type, number, expirationMonth, expirationYear, bank, owner, bgColor, color } = this.props;
     return (
-      <div className="creditcard-box" style={{'background-color': `${bgColor}`}}>
+      <div className="creditcard-box" style={{'background-color': `${bgColor}`, 'color': `${color}`}}>
         <img className="creditcard-img" src={this.cardType(type)} alt=""/>
         <p className="creditcard-num">{this.cardNumber(number)}</p>
         <p>Expires {this.monthExpire(expirationMonth)}/{this.yearExpire(expirationYear)} {bank}</p>
@@ -43,14 +43,5 @@ class CreditCard extends Component {
     )
   }
 }
-
-// type: A string that can be "Visa" or "Master Card"
-// number: A string that is a number of the credit card. You will only display the 4 last digits for security reasons wink
-// expirationMonth: A number that represents the month, between 1 and 12
-// expirationYear: A number that represents the year
-// bank: A string that represents the name of the bank
-// owner: A string that represents the name of the owner
-// bgColor: A string for the background color of the card
-// color: A string for the text color of the card
 
 export default CreditCard;
--- src/components/rating.js
+++ src/components/rating.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 
 class Rating extends Component {
+  
+  numOfStars = (num) => {
+    let rounded = Math.round(num);
+    let output = '';
+    switch(rounded) {
+      case 0:
+        output = '☆☆☆☆☆';
+        break;
+      case 1:
+        output = '★☆☆☆☆';
+        break;
+      case 2:
+        output = '★★☆☆☆';
+        break;
+      case 3:
+        output = '★★★☆☆';
+        break;
+      case 4:
+        output = '★★★★☆';
+        break;
+      case 5:
+        output = '★★★★★';
+        break;
+      default:
+        output = '☆☆☆☆☆';
+    }
+    return output;
+  }
+
   render () {
     const { children } = this.props;
     return (
       <div className="ratings-box">
-        ☆★
+        {this.numOfStars(children)}
       </div>
     )
   }
