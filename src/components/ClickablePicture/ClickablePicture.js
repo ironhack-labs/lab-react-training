a9a1c43477bb21f9ae08fd614b66c7b3ca52b11b
Jaime <jaimemoravallejo@gmail.com>
2020-04-12 22:10:43 +0200

    Iterarion 9 done

--- src/components/ClickablePicture/ClickablePicture.js
+++ src/components/ClickablePicture/ClickablePicture.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class ClickablePicture extends Component {
+  state = {
+      show: false,
+    };
+
+
+  changeShow = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+
   render(){
-    const { img } = this.props;
+    const { img, imgClicked } = this.props;
     return(
-      <div><img src={img} alt='' /></div>
+      <div><img onClick={this.changeShow} src={this.state.show ? imgClicked :img } alt='' /></div>
     )
   }
 }
