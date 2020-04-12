8718e18eb3b9379cf60262034f1cd7d7d4abf164
raquelmu <raquel.muniesa@gmail.com>
2020-04-12 23:51:27 +0200

    iteration 1 (falta fecha e imagen)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import IdCard from './components/IdCard';
+//import "./data/IdCard.css";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>IdCard</h1>
-        {/* TODO: Use the IdCard component */}
+        <IdCard
+          // src={"./personas/maxence.png"}
+          firstName={"John"}
+          lastName={"Doe"}
+          gender={"male"}
+          height={"1.78m"}
+          birth={"12"}>
+
+        </IdCard>
+        <IdCard
+          // src={"./personas/maxence.png"}
+          firstName={"Obrien"}
+          lastName={"Delores"}
+          gender={"female"}
+          height={"1.72m"}
+          birth={"12"}>
+        </IdCard>
 
         <h1>Greetings</h1>
         {/* TODO: Use the Greetings component */}
-      </div>
+    </div>
     );
   }
 }
---
+++ src/components/IdCard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import "./IdCard.css";
+
+class IdCard extends Component {
+    render() {
+        const { lastName, firstName, gender, height, birth, picture} = this.props;
+
+        return(
+            <div className="container-user">
+                <div className="container-img">
+                    {/* <img src={picture}></img> */}
+                </div>
+                <div className="container-data">
+                    <div>
+                        <span className="title-idCard">First name: </span> {firstName}
+                    </div>
+                    <div>
+                        <span className="title-idCard">Last name: </span> {lastName}
+                    </div>
+                    <div>
+                        <span className="title-idCard">Gender: </span> {gender}
+                    </div>
+                    <div>
+                       <span className="title-idCard">Height: </span>{height}
+                    </div>
+                    <div>
+                        <span className="title-idCard">Birth: </span> {birth}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default IdCard;
