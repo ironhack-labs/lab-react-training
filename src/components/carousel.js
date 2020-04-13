0fbbcfb04cd77a56bf1128f5093cd71a367bbacd
Pablolo <pablo_olondriz@hotmail.com>
2020-04-13 19:50:30 +0200

    iteration 11 done

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,29 +3,40 @@ import React, { Component } from "react";
 class Carousel extends Component {
 
   state = {
+    images: this.props.imgs,
     currentIndex: 0,
-    images: [],
-  }
-  
-  showImg = (imgs) => {
-    // const currentState = this.state.images;
-    // this.setState({images: currentState.push(imgs)});
-    // console.log(this.state);
   }
 
   previous = () => {
-
+    console.log(this.state.currentIndex);
+    if (this.state.currentIndex === 0) {
+      this.setState({
+        currentIndex: 3,
+      });
+    } else {
+      this.setState({
+        currentIndex: this.state.currentIndex - 1,
+      });
+    }
   }
 
   next = () => {
-
+    if (this.state.currentIndex === this.state.images.length - 1) {
+      this.setState({
+        currentIndex: 0,
+      });
+    } else {
+      this.setState({
+        currentIndex: this.state.currentIndex + 1,
+      });
+    }
   }
 
   render () {
     const { imgs } = this.props;
     return (
-      <div className="greetings-box">
-        <img src={this.showImg(imgs)} alt=""/>
+      <div className="carousel-box">
+        <img className="carousel-img" src={imgs[this.state.currentIndex]} alt=""/>
         <div> 
           <button onClick={this.previous}>Previous</button>
           <button onClick={this.next}>Next</button>
