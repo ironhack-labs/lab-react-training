2c6ad03244519ec6cd11fcb845db8de92c9175c4
Pablolo <pablo_olondriz@hotmail.com>
2020-04-13 20:39:24 +0200

    iteration 12 half

--- src/components/NumbersTable.js
+++ src/components/NumbersTable.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 
 class NumbersTable extends Component {
-  
+
+  createList = () => {
+    let numbers = [];
+    for (let i = 1; i <= this.props.limit; i++) {
+      numbers.push(i);
+    }
+    let listOfNumbers = numbers.map(value => <p className="number-value">{value}</p>);
+    return listOfNumbers;
+  }
+
   render () {
-    const { limit } = this.props;
     return (
-      <div className="">
-       <p>1</p>
+      <div className="number-list">
+        {this.createList()}
       </div>
     )
   }
