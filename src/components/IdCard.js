52216da4676161e358c9e0e40a1569c76eadde5f
elucserr <elenalucasserrano@gmail.com>
2020-04-13 22:59:51 +0200

    iteration1

---
+++ src/components/IdCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class IdCard extends Component {
+    render(){
+        const { firstName, lastName, gender, height, birth, picture } = this.props;
+        return (
+            <div className="">
+                <img className="" alt="" src={picture}/>
+                
+                <div className="">
+                <p>First Name:{firstName}</p>
+                <p> Last Name: {lastName}</p>
+                <p> Gender: {gender}</p>
+                <p> Height: {height}</p>
+                <p>Birth: {birth}</p>
+                </div>
+            
+            </div>
+        )
+    }
+}
+
+export default IdCard;
