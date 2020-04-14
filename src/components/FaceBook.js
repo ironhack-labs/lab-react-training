3c67b379d449555874af421ad340132ea6a3f66f
Pablolo <pablo_olondriz@hotmail.com>
2020-04-14 17:37:24 +0200

    iteration 14 half

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -3,6 +3,10 @@ import profiles from '../data/berlin.json';
 
 class FaceBook extends Component {
 
+  state = {
+    blue: true,
+  }
+
   studentOrTeacher = (isStudent) => {
     if (isStudent === true ) {
       return 'Student';
@@ -11,11 +15,29 @@ class FaceBook extends Component {
     }
   }
 
+  createBtns = () => {
+    let btnArray = [];
+      profiles.map(item => {
+        return btnArray.push(item.country);
+      })
+    let sortedArr = btnArray.slice().sort();
+    let cleanArray = sortedArr.filter((a, b) => sortedArr.indexOf(a) === b);
+    return cleanArray.map(item => <button className="country-btn">{item}</button>)
+  }
+
+  changeColor = () => {
+    this.setState({blue: !this.state.blue});
+ }
+
   render () {
+    let btnClass = this.state.blue ? "fb-box" : "fb-blue-box";
     return (
       <div>
+      <button onClick={this.changeColor} className="country-btn">All</button>
+      {this.createBtns()}
+ 
         {profiles.map(item => (
-          <div className="fb-box">
+          <div className={btnClass}>
             <img className="fb-img" src={item.img} alt=""/>
             <div>
               <p><b>First Name: {item.firstName} </b></p>
