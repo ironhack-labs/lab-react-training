c7096cc51aba41fc1fef68888bacb705ff4da06f
Pablolo <pablo_olondriz@hotmail.com>
2020-04-14 14:01:25 +0200

    iteration 12 done

--- src/components/NumbersTable.js
+++ src/components/NumbersTable.js
@@ -3,11 +3,29 @@ import React, { Component } from "react";
 class NumbersTable extends Component {
 
   createList = () => {
+    let styleBlack = {
+      'background-color': 'white',
+      'border': '2px solid black',
+      'padding': '25px',
+      'margin-left': '0.5rem',
+    };
+    let styleRed = {
+      'background-color': 'red',
+      'border': '2px solid black',
+      'padding': '25px',
+      'margin-left': '0.5rem',
+    };
     let numbers = [];
     for (let i = 1; i <= this.props.limit; i++) {
       numbers.push(i);
     }
-    let listOfNumbers = numbers.map(value => <p className="number-value">{value}</p>);
+    let listOfNumbers = numbers.map(value => { 
+      if (value % 2 === 0) {
+        return <p style={styleRed}>{value}</p>
+      } else {
+        return <p style={styleBlack}>{value}</p>
+      }
+    });
     return listOfNumbers;
   }
 
