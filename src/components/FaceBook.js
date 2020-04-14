96b6b23ce83e2ed0525fd140d6b4449a742e2fad
Pablolo <pablo_olondriz@hotmail.com>
2020-04-14 14:21:24 +0200

    iteration 13 done

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
+import profiles from '../data/berlin.json';
 
 class FaceBook extends Component {
+
+  studentOrTeacher = (isStudent) => {
+    if (isStudent === true ) {
+      return 'Student';
+    } else {
+      return 'Teacher';
+    }
+  }
+
   render () {
     return (
-      <div className="idcard-box">
-        <img className="idcard-img" src='' alt=""/>
-        <div>
-          <p><b>First Name: </b></p>
-          <p><b>Last Name: </b></p>
-          <p><b>Country: </b></p>
-          <p><b>Type: </b></p>
-        </div>
+      <div>
+        {profiles.map(item => (
+          <div className="fb-box">
+            <img className="fb-img" src={item.img} alt=""/>
+            <div>
+              <p><b>First Name: {item.firstName} </b></p>
+              <p><b>Last Name: {item.lastName}</b></p>
+              <p><b>Country: {item.country}</b></p>
+              <p><b>Type: {this.studentOrTeacher(item.isStudent)}</b></p>
+            </div>
+          </div>
+        ))}
       </div>
     )
   }
 }
 
 export default FaceBook;
+
+
