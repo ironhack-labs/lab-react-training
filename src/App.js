91f7b0bfd17c1f0ca6324df27048dcc0cc684144
Ben Balderas <benbalderas3@gmail.com>
2020-04-13 23:17:22 -0500

    iteration 5 complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import IdCard from './IdCard'
 import Greetings from './Greetings'
 import Random from './Random'
 import BoxColor from './BoxColor'
+import CreditCard from './CreditCard'
 
 class App extends Component {
 
@@ -57,6 +58,36 @@ class App extends Component {
         <BoxColor r={250} g={90} b={58} />
 
         <h1>Credit Card</h1>
+
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white" />
+
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222" />
+
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white" />
       </div>
     );
   }
--- src/CreditCard.js
+++ src/CreditCard.js
@@ -1,1 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const CreditCard = props => {
+    const CardBody = styled.div`
+        background-image: ${props.type === "Visa" ? "url(./img/visa.png)" : "url(./img/master-card.svg)"};
+        background-color: ${props.bgColor};
+        background-size: ${props.type === "Visa" ? "48px 18px" : "48px 28px"};
+        background-repeat: no-repeat;
+        background-position: 95% 16px;
+        color: ${props.color};
+    `
+
+    return (
+        <CardBody className="card--credit">
+            <h2>{props.number}</h2>
+
+            <div className="card--credit__info">
+                <p>Expires: {props.expirationMonth} / {props.expirationYear}</p>
+                <p>{props.bank}</p>
+            </div>
+
+            <p>{props.owner}</p>
+        </CardBody>
+    )
+
+}
+
+export default CreditCard
