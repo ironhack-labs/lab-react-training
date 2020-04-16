be8035da76c819e0c6757990c9317fb127c5c69d
Pablolo <pablo_olondriz@hotmail.com>
2020-04-16 18:03:49 +0200

    iteration 15 half done

--- src/components/signup/SignupPage.js
+++ src/components/signup/SignupPage.js
@@ -1,17 +1,61 @@
 import React, { Component } from "react";
 
+
 class SignupPage extends Component {
   
   state = {
     email: '',
     password: '',
-    nationality: '',
+    nationality: 'en',
+  }
+
+  handleInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  greetings = () => {
+    const { nationality } = this.state;
+    if (nationality === 'en') {
+      return 'Hello';
+    } else if (nationality === 'de') {
+      return 'Hallo';
+    } else {
+      return 'Bonjour';
+    }
   }
   
   render () {
+    const { email, password } = this.state;
     return (
-      <div className="">
-
+      <div className="form">
+        <form>
+          <div>
+            <label>Email</label>
+            <div>
+              <input type="text" name="email" placeholder="Email" value={email} onChange={this.handleInput}/>
+            </div>
+          </div>
+          <div>
+            <label>Pasword</label>
+            <div>
+              <input type="text" name="password" placeholder="Pasword" value={password} onChange={this.handleInput}/>
+            </div>
+          </div>
+          <div>
+            <label>Nationality</label><br></br>
+            <select name="nationality" onChange={this.handleInput}>
+              <option value="en">England</option>
+              <option value="de">Germany</option>
+              <option value="fr">France</option>
+            </select>
+          </div>
+          <input className="button is-normal is-success" type="submit" value="Sign Up" />
+        </form>
+        <p>{this.greetings()}</p>
+        <p>Your email address is: {this.state.email}</p>
+        <p></p>
       </div>
     )
   }
