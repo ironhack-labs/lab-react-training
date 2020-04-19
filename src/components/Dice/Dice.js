f9b860a7e24aa9b8b0c21aa0b37d06f29731b87d
Jaime <jaimemoravallejo@gmail.com>
2020-04-19 19:29:40 +0200

    Fix conflicts

--- src/components/Dice/Dice.js
+++ src/components/Dice/Dice.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class Dice extends Component {
   state = {
     value: 0,
-<<<<<<< HEAD
     img: '/img/dice-empty.png',
   }
 
@@ -45,39 +44,6 @@ class Dice extends Component {
     const { img } = this.state;
     return (
       <div><p>Click on the dice</p><img style={{'width': '100px', 'marginLeft': '50px'}} onClick={this.rollDice} src={img} alt="dice" /></div>
-=======
-  }
-
-  rollDice = () => {
-    let result = 0
-    setTimeout(() => {
-      result = Math.floor(Math.random() * (6 - 0)) + 0;
-    }, 1000);
-  }
-
-  changeDice = (result) => {
-    switch (result) {
-      case 1:
-        return '/img/dice1.png';
-      case 2:
-        return '/img/dice2.png';
-      case 3:
-        return '/img/dice3.png';
-      case 4:
-        return '/img/dice4.png';
-      case 5:
-        return '/img/dice5.png';
-      case 6:
-        return '/img/dice6png';
-      default:
-        return '/img/dice-empty.png';
-    }
-  }
-
-  render(){
-    return (
-      <div><img onClick={this.rollDice} src={this.changeDice} alt="dice" /></div>
->>>>>>> 162a313c2fb2da69a98af0b11a7fee2ddf36461d
     )
   }
 }
