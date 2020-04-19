c09d5da5117db44990b9fb41eb6287bb74912d3e
Alex <alex.fernandez.canovas@gmail.com>
2020-04-19 14:00:04 +0200

    iteration 7

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ import BoxColor from './components/boxcolor/BoxColor.js'
 import CreditCard from './components/creditCard/CreditCard.js'
 import './components/creditCard/CreditCard.css'
 import Rating from './components/rating/Rating.js';
+import DriverCard from './components/driverCard/DriverCard.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Rating stars="0" />
-      <Rating stars="1.49" />
-      <Rating stars="1.5" />
-      <Rating stars="3" />
-      <Rating stars="4" />
-      <Rating stars="5" />
         <h1>IdCard</h1>
         <IdCard
           lastName='Doe'
@@ -27,45 +22,43 @@ class App extends Component {
           picture="https://randomuser.me/api/portraits/men/44.jpg"
         />
 
-      <IdCard
-        lastName='Delores '
-        firstName='Obrien'
-        gender='female'
-        height={172}
-        birth={new Date("1988-05-11")}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      />
-      <div className="creditCards">
-        <CreditCard
-          type="Visa"
-          number="0123456789018845"
-          expirationMonth={3}
-          expirationYear={2021}
-          bank="BNP"
-          owner="Maxence Bouret"
-          bgColor="#11aa99"
-          color="white" />
-        <CreditCard
-          type="Master Card"
-          number="0123456789010995"
-          expirationMonth={3}
-          expirationYear={2021}
-          bank="N26"
-          owner="Maxence Bouret"
-          bgColor="#eeeeee"
-          color="#222222" />
-        <CreditCard
-          type="Visa"
-          number="0123456789016984"
-          expirationMonth={12}
-          expirationYear={2019}
-          bank="Name of the Bank"
-          owner="Firstname Lastname"
-          bgColor="#ddbb55"
-          color="white" />
-      </div>
-
-
+        <IdCard
+          lastName='Delores '
+          firstName='Obrien'
+          gender='female'
+          height={172}
+          birth={new Date("1988-05-11")}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+        <div className="creditCards">
+          <CreditCard
+            type="Visa"
+            number="0123456789018845"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="BNP"
+            owner="Maxence Bouret"
+            bgColor="#11aa99"
+            color="white" />
+          <CreditCard
+            type="Master Card"
+            number="0123456789010995"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="N26"
+            owner="Maxence Bouret"
+            bgColor="#eeeeee"
+            color="#222222" />
+          <CreditCard
+            type="Visa"
+            number="0123456789016984"
+            expirationMonth={12}
+            expirationYear={2019}
+            bank="Name of the Bank"
+            owner="Firstname Lastname"
+            bgColor="#ddbb55"
+            color="white" />
+        </div>
         <h1>Greetings</h1>
         
         <Greetings lang="de">Ludwig</Greetings>
@@ -76,6 +69,37 @@ class App extends Component {
 
         <BoxColor r={255} g={0} b={0} />
         <BoxColor r={128} g={255} b={0} />
+        
+
+        <div>
+        
+          <h1>Ratings</h1>
+          <Rating stars="0" />
+          <Rating stars="1.49" />
+          <Rating stars="1.5" />
+          <Rating stars="3" />
+          <Rating stars="4" />
+          <Rating stars="5" />
+          
+          <DriverCard
+            name="Travis Kalanick"
+            rating={4.2}
+            img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+            car={{
+              model: "Toyota Corolla Altis",
+              licensePlate: "CO42DE"
+            }} />
+          <DriverCard
+            name="Dara Khosrowshahi"
+            rating={4.9}
+            img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+            car={{
+              model: "Audi A3",
+              licensePlate: "BE33ER"
+            }} />
+
+        </div>
+
 
       </div>
     );
---
+++ src/components/driverCard/DriverCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import '../driverCard/DriverCard.css'
+import Rating from '../rating/Rating';
+
+class DriverCard extends Component {
+
+  render(){
+
+    const {name, rating, img, car} = this.props;
+    return(
+      <div className="driverCard">  
+          <div>
+            <img className="photo-driver-card" src={img} />
+          </div>
+          <div className="info-driver-card">
+            <p><strong>{name}</strong></p>
+            <p><Rating stars={rating} className="rating-driver-card" /></p>
+            <p className="car-info">{car.model} - {car.licensePlate}</p>
+          </div>    
+      </div>
+    )
+
+  }
+
+}
+
+
+export default DriverCard
