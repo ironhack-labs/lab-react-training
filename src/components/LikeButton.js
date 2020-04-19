414f8ad689185c16fe80b413276f188b0b365eb3
Edu Carmona <dudu.carmona@gmail.com>
2020-04-19 17:53:35 +0200

    Iteration 8: Component: LikeButton

---
+++ src/components/LikeButton.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './LikeButton.css';
+
+class LikeButton extends Component {
+    state = {
+        counter: 0,
+        styleColor: {
+            backgroundColor: 'purple'
+        }
+    };
+
+    incrementLike = () => {
+        const colors = ['purple','blue','green','yellow','orange','red'];
+        const colorIndex = (this.state.counter + 1) % colors.length;
+
+        this.setState({
+            counter: this.state.counter + 1,
+            styleColor: {
+                backgroundColor: colors[colorIndex]
+            }
+        });
+    }
+
+    render() {
+        const { counter, styleColor } = this.state;
+
+        return (
+            <button style={styleColor} onClick={this.incrementLike}>{counter} Likes</button>
+        );
+    }
+}
+
+export default LikeButton;
