7ffa62d0f599085ad6adcf0dc81399f25cdf9d26
Edu Carmona <dudu.carmona@gmail.com>
2020-04-20 19:48:19 +0200

    Iteration 14: List and Keys | FaceBook | Advanced

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -3,25 +3,62 @@ import profiles from '../data/berlin.json';
 import './FaceBook.css';
 
 class FaceBook extends Component {
+    state = {
+        countrySelected: ''
+    }
+
+    getCountriesArray = () => {
+        const countriesArray = ['All'];
+
+        for (let i = 0; i < profiles.length; i++) {
+            if (profiles[i].country === 'Catalonia') {
+                profiles[i].country = 'Spain';
+            }
+
+            if (!countriesArray.includes(profiles[i].country)) {
+                countriesArray.push(profiles[i].country);
+            }
+        }
+
+        return countriesArray.sort();
+    };
+    
+    selectProfiles = (country) => {
+        console.log(country);
+
+        this.setState({
+            countrySelected: country
+        });
+    }
+
     render() {
-        console.log(profiles);
+        const arrayButtons = this.getCountriesArray();
 
         return (
-            <ul className='FaceBook'>
-                {
-                    profiles.map((profile, index) => (
-                        <li key={index} className='FaceBook-profile'>
-                            <img src={profile.img} alt='' />
-                            <div className='FaceBook-profile-data'>
-                                <div><b>First name: </b>{profile.firstName}</div>
-                                <div><b>Last name: </b>{profile.lastName}</div>
-                                <div><b>Country: </b>{profile.country}</div>
-                                <div><b>Type: </b>{profile.isStudent ? 'Student' : 'Teacher'}</div>
-                            </div>
-                        </li>
-                    ))
-                }
-            </ul>
+            <div className='FaceBook'>
+                <div className='FaceBook-country-buttons'>
+                    {
+                        arrayButtons.map((button, index) => (
+                            <button style={this.state.countrySelected === button ? {backgroundColor: '#A3D2E2'} : {}} key={index} className='FaceBook-country-button' onClick={() => {this.selectProfiles(button)}}>{button}</button>
+                        ))
+                    }
+                </div>
+                <ul className='FaceBook-list'>
+                    {
+                        profiles.map((profile, index) => (                
+                            <li key={index} className='FaceBook-profile' style={this.state.countrySelected === profile.country ? {backgroundColor: '#A3D2E2'} : {}} >
+                                <img src={profile.img} alt='' />                                
+                                <div className='FaceBook-profile-data' >
+                                    <div><b>First name: </b>{profile.firstName}</div>
+                                    <div><b>Last name: </b>{profile.lastName}</div>
+                                    <div><b>Country: </b>{profile.country}</div>
+                                    <div><b>Type: </b>{profile.isStudent ? 'Student' : 'Teacher'}</div>
+                                </div>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         );
     }
 }
