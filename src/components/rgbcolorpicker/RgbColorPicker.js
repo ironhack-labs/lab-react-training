d0abfeea41d3d801d7b1267e00d9b3f663bb77dc
Javier Varela <jvarela@strands.com>
2020-04-20 12:13:53 +0200

    colorpicker done

--- src/components/rgbcolorpicker/RgbColorPicker.js
+++ src/components/rgbcolorpicker/RgbColorPicker.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react';
 import SingleColorPicker from './SingleColorPicker';
 
 
+
 class RgbColorPicker extends Component {
     
     state = {
-        rValue: 0,
-        gValue: 0,
-        bValue: 0
+        red: 0,
+        green: 0,
+        blue: 0
+    }
+
+    handleInputChange = (e) => {
+        this.setState(
+            {[e.target.name] : e.target.value}
+        )
     }
 
     render() {
+        const {red, green, blue} = this.state;
+        const colorBoxStyle= {backgroundColor: `rgb(${red}, ${green}, ${blue})`, width: "100px", height: "100px"};
         return (
             <div>
-                <SingleColorPicker />
-                <SingleColorPicker />
-                <SingleColorPicker />
+                <SingleColorPicker color="red" value={red} onChange={this.handleInputChange}/>
+                <SingleColorPicker color="green" value={green} onChange={this.handleInputChange}/>
+                <SingleColorPicker color="blue" value={blue} onChange={this.handleInputChange}/>
+                <div className="IdCard">
+                    <div className="left" style={colorBoxStyle}></div>
+                    <div className="right">
+                        RGB({red}, {green}, {blue})
+                    </div>
+                </div>
             </div>
           
         );
