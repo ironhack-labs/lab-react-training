3c6f9d08050b37a57673df5447fe0d18ac39cebc
Pablolo <pablo_olondriz@hotmail.com>
2020-04-20 17:46:35 +0200

    iteration 16 done

--- src/components/RGBColorPicker.js
+++ src/components/RGBColorPicker.js
@@ -4,15 +4,27 @@ import SingleColorPicker from './SingleColorPicker';
 class RGBColorPicker extends Component {
   
   state = {
-    rValue: 0,
-    gValue: 0,
-    bValue: 0,
+    r: 255,
+    g: 150,
+    b: 0,
+  }
+
+  boxOnChange = (e, color) => {
+    this.setState({
+      [color]: e.target.value,
+    })
   }
 
   render () {
+    const { r, g, b } = this.state;
     return (
-      <div className="">
-      <SingleColorPicker />
+      <div className="rgb-container">
+      <SingleColorPicker color='r' value={r} onChange={this.boxOnChange}/>
+      <SingleColorPicker color='g' value={g} onChange={this.boxOnChange}/>
+      <SingleColorPicker color='b' value={b} onChange={this.boxOnChange}/>
+      <div className="box-result-container">
+        <div style={{'background-color': `rgb(${r},${g},${b})`}} className='box-singlecolor'></div> <p>rgb({r},{g},{b})</p>
+      </div>
       </div>
     )
   }
--- src/components/SingleColorPicker.js
+++ src/components/SingleColorPicker.js
@@ -2,11 +2,27 @@ import React, { Component } from "react";
 
 class SingleColorPicker extends Component {
   
+  boxColor = () => {
+    const {color} = this.props;
+    if (color === 'r') {
+      return 'box-singlecolor box-red';
+    } else if (color === 'g') {
+      return 'box-singlecolor box-green'
+    } else {
+      return 'box-singlecolor box-black';
+    }
+  }
+
+  valueChange = (e, color) => {
+    return this.props.onChange(e, color);
+  }
+
   render () {
-    const { color, value, onChange } = this.props;
+    const { color, value } = this.props;
     return (
-      <div className="">
-       <p>1</p>
+      <div className="singlecolor-container">
+       <div className={this.boxColor()}></div>
+       <p>{color}:</p><input className="input-singlecolor" value={value} type="number" onChange={(e) => this.valueChange(e, color)}/>
       </div>
     )
   }
