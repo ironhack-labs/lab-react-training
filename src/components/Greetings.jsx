0fd7fd7ac19e8decfa4be604da44f47bfec46cb6
Chelsey Grice <chelseygrice92@gmail.com>
2020-04-30 17:45:43 +0200

    commit

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const checkLang = (lang) => {
+  if (lang === "de") {
+    return "Hallo";
+  } else if (lang === "fr") {
+    return "Bonjour";
+  } else if (lang === "es") {
+    return "Hola";
+  } else if (lang === "en") {
+    return "Hello";
+  }
+};
+
+export default function Greetings(props) {
+
+  return (
+    <div>
+      <h2>{checkLang(props.lang)} {props.children} </h2>
+    </div>
+  );
+}
---
+++ src/components/Random.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  }
+
+export default function Random(props) {
+    return (
+        <div>
+        <h2>Random value between {props.min} and {props.max} => {getRandomIntInclusive(props.min, props.max)}</h2>
+
+        </div>
+    )
+} 
