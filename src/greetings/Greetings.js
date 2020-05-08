61016a93b705434387082a6ec9e7b0889b4f8ca7
Andre <andre.talpo@gmail.com>
2020-05-07 22:09:36 -0300

    Comecado

---
+++ src/greetings/Greetings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Greetings extends React.Component {
+    render() {
+        return(
+            <h1></h1>
+        );
+    }
+}
+
+export default Greetings;
---
+++ src/idCard/IdCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './IdCard.css';
+
+class IdCard extends React.Component {
+    render() {
+        return (
+            <div className="id-card">
+                <div>
+                    <img src={this.props.picture} />
+                </div>
+                <div>
+                    <p><span>First name: </span>{this.props.firstName}</p>
+                    <p><span>Last name: </span>{this.props.lastName}</p>
+                    <p><span>Gender: </span>{this.props.gender}</p>
+                    <p><span>Height: </span>{this.props.height}</p>
+                    <p><span>Birth: </span>{this.props.birth.toString()}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default IdCard;
