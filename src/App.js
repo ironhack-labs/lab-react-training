0aa7c049445c5a6e27749e30d9258a1c5b767221
Stefano <oddouu@gmail.com>
2020-05-31 01:41:57 +0100

    iteration 5 done

--- src/App.js
+++ src/App.js
@@ -4,77 +4,102 @@ import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random';
-import BoxColor from './components/BoxColor'
-
+import BoxColor from './components/BoxColor';
+import CreditCard from './components/CreditCard'
 
 class App extends Component {
   state = {
     userA: {
-      firstName: "Harper",
-      lastName: "Perez",
-      gender: "male",
+      firstName: 'Harper',
+      lastName: 'Perez',
+      gender: 'male',
       height: 178,
       birth: new Date('1992-07-14'),
-      picture:
-        "https://randomuser.me/api/portraits/men/44.jpg",
+      picture: 'https://randomuser.me/api/portraits/men/44.jpg',
     },
     userB: {
-      firstName: "Delores",
-      lastName: "Obrien",
-      gender: "female",
+      firstName: 'Delores',
+      lastName: 'Obrien',
+      gender: 'female',
       height: 172,
       birth: new Date('1988-05-11'),
-      picture:
-        "https://randomuser.me/api/portraits/women/44.jpg",
+      picture: 'https://randomuser.me/api/portraits/women/44.jpg',
     },
 
     color: {
       randomR: Math.floor(Math.random() * 255),
       randomG: Math.floor(Math.random() * 255),
-      randomB: Math.floor(Math.random() * 255)
-    }
+      randomB: Math.floor(Math.random() * 255),
+    },
   };
 
-
-  render () {
+  render() {
     return (
       <div className="App">
-        
-      <IdCard 
-        lastName={this.state.userA.lastName}
-        firstName={this.state.userA.firstName}
-        gender={this.state.userA.gender}
-        height={this.state.userA.height}
-        birth={this.state.userA.birth}
-        picture={this.state.userA.picture}
-      />
-      <IdCard 
-        lastName={this.state.userB.lastName}
-        firstName={this.state.userB.firstName}
-        gender={this.state.userB.gender}
-        height={this.state.userB.height}
-        birth={this.state.userB.birth}
-        picture={this.state.userB.picture}
-      />
+        <IdCard
+          lastName={this.state.userA.lastName}
+          firstName={this.state.userA.firstName}
+          gender={this.state.userA.gender}
+          height={this.state.userA.height}
+          birth={this.state.userA.birth}
+          picture={this.state.userA.picture}
+        />
+        <IdCard
+          lastName={this.state.userB.lastName}
+          firstName={this.state.userB.firstName}
+          gender={this.state.userB.gender}
+          height={this.state.userB.height}
+          birth={this.state.userB.birth}
+          picture={this.state.userB.picture}
+        />
 
-      <Greetings lang="de">Ludwig</Greetings>
-      <Greetings lang="en">Charles</Greetings>
-      <Greetings lang="es">Pablo</Greetings>
-      <Greetings lang="fr">François</Greetings>
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="en">Charles</Greetings>
+        <Greetings lang="es">Pablo</Greetings>
+        <Greetings lang="fr">François</Greetings>
 
-      <Random min={1} max={6} />
-      <Random min={1} max={100} />
+        <Random min={1} max={6} />
+        <Random min={1} max={100} />
 
         <BoxColor r={255} g={0} b={0} />
         <BoxColor r={128} g={255} b={0} />
 
-        <BoxColor r={this.state.color.randomR} g={this.state.color.randomG} b={this.state.color.randomB} />
-      
+        <BoxColor
+          r={this.state.color.randomR}
+          g={this.state.color.randomG}
+          b={this.state.color.randomB}
+        />
 
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white" />
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222" />
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white" />
       </div>
     );
   }
 }
-
 
 export default App;
---
+++ src/components/CreditCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './CreditCard.css';
+
+function CreditCard(props) {
+  let imageUrl;
+  switch (props.type) {
+    case 'Visa':
+      imageUrl =
+        'https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.app/img/visa.png';
+      break;
+    case 'Master Card':
+      imageUrl =
+        'https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.app/img/master-card.svg';
+      break;
+    default:
+      imageUrl = '';
+  }
+
+  let expirationMonth = String(props.expirationMonth);
+  if (expirationMonth.length < 2) {
+     expirationMonth = '0' + props.expirationMonth
+  }
+
+  let expirationYear = String(props.expirationYear).slice(-2);
+
+  const cardNumber = props.number.slice(-4)
+  
+
+  const style = {
+    backgroundColor: props.bgColor,
+    color: props.color,
+  };
+  return (
+    <div className="creditCard" style={style}>
+        <p className="type">
+      <img src={imageUrl} alt="" />
+        </p>
+          <p className="number">•••• •••• •••• {cardNumber}</p>
+      <p>
+        Expires {expirationMonth}/{expirationYear}
+        <span> {props.bank}</span>
+      </p>
+  <p>{props.owner}</p>
+    </div>
+  );
+}
+
+export default CreditCard;
