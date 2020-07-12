82aca1667410b7bb8c58142293e98c36ec3fe414
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-07-12 23:13:22 +0200

    Iteration 6

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import IdCard from './components/IdCard';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 
 function App() {
@@ -28,6 +29,39 @@ function App() {
     }
   ]
 
+  const creditCard = [
+    {
+      type: "Visa",
+      number: "0123456789018845",
+      expirationMonth: 3,
+      expirationYear: 2021,
+      bank:"BNP",
+      owner: "Maxence Bouret",
+      bgColor:"#11aa99",
+      color: "white"
+    },
+    {
+      type: "Master Card",
+      number: "0123456789010995",
+      expirationMonth: 3,
+      expirationYear: 2021,
+      bank:"N26",
+      owner: "Maxence Bouret",
+      bgColor: "#eeeeee",
+      color: "#222222"
+    },
+    {
+      type: "Visa",
+      number: "0123456789016984",
+      expirationMonth: 12,
+      expirationYear: 2019,
+      bank: "Name of the Bank",
+      owner: "Firstname Lastname",
+      bgColor: "#ddbb55",
+      color: "white"
+    }
+  ]
+
   const cards = card.map(thisCard => {
     return (
       <IdCard lastname={thisCard.lastName}
@@ -36,6 +70,20 @@ function App() {
               height={thisCard.height} 
               birth={thisCard.birth.toLocaleDateString()}
               picture={thisCard.picture}
+       />
+    )
+  })
+
+  const creditCards = creditCard.map(thisCC => {
+    return (
+      <CreditCard type={thisCC.type}
+                  number={thisCC.number}
+                  expirationMonth={thisCC.expirationMonth}
+                  expirationYear={thisCC.expirationYear}
+                  bank={thisCC.bank}
+                  owner={thisCC.owner}
+                  bgColor={thisCC.bgColor}
+                  color={thisCC.color}
        />
     )
   })
@@ -56,34 +104,15 @@ function App() {
       <BoxColor r={128} g={255} b={0} />
       <h1>CreditCard</h1>
       <div className="credit-cards-container">
-        <CreditCard
-          type="Visa"
-          number="0123456789018845"
-          expirationMonth={3}
-          expirationYear={2021}
-          bank="BNP"
-          owner="Maxence Bouret"
-          bgColor="#11aa99"
-          color="white" />
-        <CreditCard
-          type="Master Card"
-          number="0123456789010995"
-          expirationMonth={3}
-          expirationYear={2021}
-          bank="N26"
-          owner="Maxence Bouret"
-          bgColor="#eeeeee"
-          color="#222222" />
-        <CreditCard
-          type="Visa"
-          number="0123456789016984"
-          expirationMonth={12}
-          expirationYear={2019}
-          bank="Name of the Bank"
-          owner="Firstname Lastname"
-          bgColor="#ddbb55"
-          color="white" />
+        {creditCards}
       </div>
+      <h1>Rating</h1>
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>
     </div>
   );
 }
---
+++ src/components/Rating.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Rating(props) {
+
+    const pourcentage = Math.round((props.children /5 * 100) / 5) * 5 
+    
+
+    return (
+        <div className="stars-container">
+            <div className="stars-outer">
+            <div className="stars-inner" style={{width:pourcentage}}></div>
+            </div>
+        </div>
+    )
+}
+
+export default Rating
