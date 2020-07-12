1d41a6c6872d9f72ce8dce224ec3cac3090f1e90
Alex <alex@Alexs-MacBook-Pro.local>
2020-07-12 12:38:55 +0200

    iteration 5

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Greetings from './components/Greetings';
 import IdCard from './components/IdCard';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
+import CreditCard from './components/CreditCard'
 
-
+//Iteration 1
 const idCards = [
   {
     lastName:'Doe',
@@ -37,9 +38,14 @@ const idCardsCollection = idCards.map(card =>
   />
 )
 
+const bgVisa = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Visa_2014_logo_detail.svg/200px-Visa_2014_logo_detail.svg.png'
+const bgMC = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Mastercard_2019_logo.svg/320px-Mastercard_2019_logo.svg.png'
+
 function App() {
   return (
     <div className="App">
+
+    {/* Iteration 1 */}
       <div className="Greetings">
         <Greetings lang="pt">
           Lourenço
@@ -51,13 +57,53 @@ function App() {
           John
         </Greetings>
       </div>
+
+    {/* Iteration 2 */}
       {idCardsCollection}
-      <Random 
-        min='0'
-        max='255'
-      />
-      <BoxColor r={255} g={0} b={0}>#ff0000</BoxColor>;
-      <BoxColor r={128} g={255} b={0}>#80ff00</BoxColor>;
+
+    {/* Iteration 3 */}
+      <Random min={1} max={6}/>
+      <Random min={1} max={100}/>
+
+    {/* Iteration 4 */}
+      <BoxColor r={255} g={0} b={0}>#ff0000</BoxColor>
+      <BoxColor r={128} g={255} b={0}>#80ff00</BoxColor>
+
+    {/* Iteration 5 */}
+ 
+    <CreditCard
+      type= {bgVisa}
+      number="0123456789018845"
+      expirationMonth={3}
+      expirationYear={2021}
+      bank="BNP"
+      owner="Maxence Bouret"
+      bgColor="#11aa99"
+      color="white" 
+    />
+
+    <CreditCard
+      type={bgMC}
+      number="0123456789010995"
+      expirationMonth={3}
+      expirationYear={2021}
+      bank="N26"
+      owner="Maxence Bouret"
+      bgColor="#eeeeee"
+      color="#222222" 
+    />
+
+    <CreditCard
+      type={bgVisa}
+      number="0123456789016984"
+      expirationMonth={12}
+      expirationYear={2019}
+      bank="Name of the Bank"
+      owner="Firstname Lastname"
+      bgColor="#ddbb55"
+      color="white" 
+    />
+
     </div>
   );
 }
---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function CreditCard(props) {
+
+const numbers = [...props.number]
+
+const lastNumbers = numbers.splice((numbers.length/4) * 3, (numbers.length/4) * 4)
+const firstNumbers = numbers.splice(0, (numbers.length/4) * 3)
+
+console.log(firstNumbers)
+    return (
+        
+        <div 
+            className="CreditCard"
+            style={{backgroundColor: `${props.bgColor}`,
+                    color: `${props.color}` }}
+        >
+            <div className="CreditCard__type">
+                <img src={props.type} alt="img"/>
+            </div>
+            <div className="CreditCard__number">
+                <label className="number-secret"><span></span></label>
+
+                <label>{lastNumbers}</label>
+            </div>
+            <div className="CreditCard__expire-cont">
+                <label>Expires {props.expirationMonth}/{props.expirationYear}</label>
+                <label>{props.bank}</label>
+            </div>
+            <div className="CreditCard__owner">{props.owner}</div>
+        </div>
+    )
+}
