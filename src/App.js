e286bc52d432318db0925f3dc001ead92560bc15
David de Lamo <d.lamo.valverde@gmail.com>
2020-07-12 13:55:01 +0200

    iteration 12

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
 import Dice from './components/Dice';
 import Carousel from './components/Carousel';
+import NumbersTable from './components/NumbersTable';
 
 function App() {
   return (
@@ -91,6 +92,7 @@ function App() {
           'https://randomuser.me/api/portraits/men/2.jpg'
         ]}
       />
+      <NumbersTable limit={24} />
     </div>
   );
 }
---
+++ src/components/NumbersTable.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NumbersTable = (props) => {
+    const getNumbers = ({limit}) => {
+        let numbers = [];
+        for (let i = 1; i <= limit; i++) {
+            numbers.push(<p key={i} className='number'>{i}</p>)
+        }
+        return numbers
+    }
+    return (
+        <div className='numbers-table'>
+           {getNumbers(props)} 
+        </div>
+    )
+}
+
+export default NumbersTable
