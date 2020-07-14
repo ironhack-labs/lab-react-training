9b18dedad149c8a03dbb6c97f6bc488fe7d83969
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-07-14 14:27:45 +0200

    Iteration 14 - refactoring: sacar todo del render y crear metodos en la clase

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button(props) {
-    return (
-        <button style={props.style} onClick={props.updateCountryFunction}>{props.country}</button>
-    )
-}
-
-export default Button
--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,7 +1,6 @@
 import profilesImported from '../data/berlin.json';
 import React, { Component } from 'react'
 import IdCard from './IdCard';
-import Button from './Button';
 
 const newProfiles = [...JSON.parse(JSON.stringify(profilesImported))]
 
@@ -12,25 +11,7 @@ export class FaceBook extends Component {
             selectedCountry: ""
         }
         this.profiles = [...newProfiles]
-        this.countriesArray = [...new Set([...this.profiles].map(profile => profile.country))] // Create an array of unique countries found in 
-                                                                                               // the profiles array
-        // this.countryButtons = [...this.countriesArray].map((country,index) =>{
-
-        //     const componentStyle = () => country === this.state.selectedCountry ? {backgroundColor: "blue"} : {backgroundColor: "white"}
-        //     return <Button key={index} style={componentStyle()} updateCountryFunction={() => this.updateSelectedCountry({country})} country={country} />
-        // })
-        // this.faceBookProfiles = [...this.profiles].map((profile, index) => {
-        //     const isAStudent = profile.isStudent
-        //     return (
-        //         <IdCard key={index}
-        //                 picture={profile.img}
-        //                 firstName={profile.firstName} 
-        //                 lastName={profile.lastName}
-        //                 country={profile.country}
-        //                 type={isAStudent ? "Student" : "Teacher"} 
-        //          />
-        //     )
-        // })
+        this.countriesArray = [...new Set([...this.profiles].map(profile => profile.country))]
         this.componentStyle = country => country === this.state.selectedCountry ? {backgroundColor: "#a3d2e2"} : {backgroundColor: "white"}
     }
 
@@ -40,29 +21,37 @@ export class FaceBook extends Component {
             this.setState({selectedCountry: country.country})
     }
 
+    getCountryButtons(arrOfCountries) {
+        const countryButtons = [...arrOfCountries].map((country,index) =>{
+            return <button key={index} style={this.componentStyle(country)} onClick={() => this.updateSelectedCountry({country})}>{country}</button>
+        })
+        return countryButtons
+    }
+
+    getFaceBookProfiles(arrOfProfiles) {
+        const faceBookProfiles = [...arrOfProfiles].map((profile, index) => {
+            const isAStudent = profile.isStudent
+            return (
+                <IdCard key={index}
+                        picture={profile.img}
+                        firstName={profile.firstName} 
+                        lastName={profile.lastName}
+                        country={profile.country}
+                        type={isAStudent ? "Student" : "Teacher"} 
+                        style={this.componentStyle(profile.country)}
+                />
+            )
+        })
+        return faceBookProfiles
+    }
+
     render() {
         return (
             <div>
                 <div className="country-buttons">
-                    {/* {this.countryButtons} ======> doesnt work because "mounting only", not "update" */} 
-                    {this.countryButtons = [...this.countriesArray].map((country,index) =>{
-                    return <Button key={index} style={this.componentStyle(country)} updateCountryFunction={() => this.updateSelectedCountry({country})} country={country} />
-                    })}
+                    {this.getCountryButtons(this.countriesArray)}
                 </div>
-                {/* {this.faceBookProfiles} */}
-                {this.faceBookProfiles = [...this.profiles].map((profile, index) => {
-                    const isAStudent = profile.isStudent
-                    return (
-                        <IdCard key={index}
-                                picture={profile.img}
-                                firstName={profile.firstName} 
-                                lastName={profile.lastName}
-                                country={profile.country}
-                                type={isAStudent ? "Student" : "Teacher"} 
-                                style={this.componentStyle(profile.country)}
-                        />
-                    )
-                })}
+                {this.getFaceBookProfiles(this.profiles)}
             </div>
         )
     }
