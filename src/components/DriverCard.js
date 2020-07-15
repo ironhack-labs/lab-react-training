d86159792408779241e464190c07003d876e400b
Juli Ramon <juli@nexumlabs.com>
2020-07-15 12:25:20 +0200

    Iteration 7 - Done

---
+++ src/components/DriverCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Rating from './Rating';
+
+const DriverCard = (props) => {
+  return (
+    <div className="card">
+      <div className="col">
+        <div
+          className="profilePicture"
+          style={{ backgroundImage: `url(${props.profilePicture})` }}
+        ></div>
+      </div>
+      <div className="col">
+        <ul>
+          <li className="name">{props.name}</li>
+          <li>
+            <Rating>{props.rating}</Rating>
+          </li>
+          <li>
+            {props.car.model} - {props.car.licensePlate}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default DriverCard;
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import SolidStar from './SolidStar';
+import BorderedStar from './BorderedStar';
 
 const Rating = (props) => {
-  const solidStar = <img src="./img/solidStar.svg" alt="Solid Star" />;
-  const borderedStar = <img src="./img/borderedStar.svg" alt="Bordered Star" />;
   const rate = Math.round(props.children);
   let starsArr = [];
   for (let i = 0; i < 5; i++) {
-    rate < i + 1 ? starsArr.push(borderedStar) : starsArr.push(solidStar);
+    rate < i + 1
+      ? starsArr.push(<BorderedStar />)
+      : starsArr.push(<SolidStar />);
   }
   return <div className="stars-row">{starsArr}</div>;
 };
