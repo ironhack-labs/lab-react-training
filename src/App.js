6e3f0013b28d15e64d8ace30d27324fad412dd67
irensvm <irens.vm@gmail.com>
2020-07-15 22:43:35 +0200

    it1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greetings from './components/Greetings';
+import IdCard from './components/IdCard';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>IdCard</h1>
+    <IdCard
+    lastName='Delores '
+    firstName='Obrien'
+    gender='female'
+    height='172'
+    birth="1988-05-11"
+    picture="https://randomuser.me/api/portraits/women/44.jpg"/>
+    <IdCard
+    lastName='Doe'
+    firstName='John'
+    gender='male'
+    height='178'
+    birth="1992-07-14"
+    picture="https://randomuser.me/api/portraits/men/44.jpg"/>
+  
+    </div>
+    <div>
+    <h1>Greetings</h1>
+    <Greetings lang="de">Ludwig</Greetings>
+    <Greetings lang="fr">François</Greetings>
+    </div>
+      
+      
     </div>
   );
 }
