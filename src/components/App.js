9fc280a846400e653d06561457ba782600e3b72f
Robson <robsonoliveira@gmail.com>
2020-07-21 16:36:41 -0300

    second iteration done

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import IdCard from './IdCard';
+import Greetings from './Greetings';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <IdCard  
+          <h1>IdCard</h1> 
+          <IdCard  
           lastName='Robson'
           firstName='Santos'
           gender='male'
           height={181}
           birth={new Date("1981-09-04")}
           picture="https://scontent.fcgh37-1.fna.fbcdn.net/v/t1.0-0/p206x206/74575651_2597664863589997_2029651297543651328_o.jpg?_nc_cat=100&_nc_sid=da31f3&_nc_ohc=a23hl9QxqYoAX--pDZW&_nc_ht=scontent.fcgh37-1.fna&_nc_tp=6&oh=442a4df3ee8977b9cdcb6ce31b8f77c2&oe=5F3D5DE3"/>
-          src/App.js and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        
+        <h1>Greetings</h1>
+        {/* Use the Greetings component and definie props.lang="de" and props.children="Ludwig" */}
+        <Greetings lang="en">Ironhacker</Greetings> 
+        </div>
+  )
 }
 
 export default App;
---
+++ src/components/Greetings.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Greetings (props){
+
+  function renderGreeting (lang) {
+      const greetings = {
+        de: 'Hallo',
+        en: 'Hello',
+        fr: 'Bonjour',
+        es: 'Hola',
+      }
+      return greetings[lang];
+    };
+    
+    return (
+    <div className="box">
+{`${renderGreeting(props.lang)} ${props.children}`}
+</div>)
+}
+
+export default Greetings;
