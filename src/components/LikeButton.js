04273a7f267fce912d7294dd5e6f5e958eb39c31
Elise Djn <elisedjn@protonmail.com>
2020-08-18 21:04:20 +0200

    change on like button

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,18 +1,34 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-const LikeButton = () => {
-    const [count, setCount] = useState(0)
-    let colors = ['purple','blue','green','yellow','orange','red']
-    let index;
-    count > 5? index=(count-6) : index=count
-    let buttonStyle = {
-        backgroundColor: colors[index]
+class LikeButton extends React.Component {
+    constructor(props){
+        super(props) 
+        this.state = {
+            index: 0,
+            count: 0,
+            buttonStyle: {backgroundColor: "purple"}
+        }
     }
-    return (
-        <button onClick={() => setCount(count+1)} style={buttonStyle}>{count} {
-            count > 1 ? "Likes": "Like"
-        }</button>
-    )
+
+    handleClick = () => {
+        let colors = ['purple','blue','green','yellow','orange','red']
+        let newIndex;
+        this.state.index < colors.length-1? newIndex = this.state.index + 1 : newIndex = 0;
+        let newColor = colors[newIndex]
+        this.setState({
+            count: this.state.count + 1,
+            index: newIndex,
+            buttonStyle: {backgroundColor: newColor}
+        })
+    }
+    
+    render() {
+        return (
+            <button onClick={this.handleClick} style={this.state.buttonStyle}>{this.state.count} {
+                this.state.count > 1 ? "Likes": "Like"
+            }</button>
+        )
+    }
 }
 
 export default LikeButton
