de260768879c4ca2879e1012fa2b89dc8f0e32e3
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-09-08 13:44:05 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import IdCard from './Components/IdCard';
 import Greetings from './Components/Greetings';
+import Random from './Components/Random';
 
 function App() {
   return (
@@ -21,7 +22,8 @@ function App() {
         <h1>Greetings</h1>
         <Greetings lang="en">Alex</Greetings>
         <Greetings lang="fr">Jacques</Greetings>
-     
+        <h1>Random</h1>
+        <Random min={1} max={20}/>
     </div>
   );
 }
