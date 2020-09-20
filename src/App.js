0fe26ea57506d59118c4ae62ca91c9142b3688eb
“Debora <debora.crescenzo@gmail.com>
2020-09-20 11:31:05 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    IdCardA: {
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      height: 1.78,
+      birth: new Date('1992-07-14'),
+      picture: 'https://randomuser.me/api/portraits/men/44.jpg',
+    },
+    IdCardB: {
+      firstName: 'Obrien',
+      lastName: 'Delores',
+      gender: 'female',
+      height: 1.72,
+      birth: new Date('1988-05-11'),
+      picture: 'https://randomuser.me/api/portraits/women/44.jpg',
+    },
+  };
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+        <div className="IdCards box">
+          <h1>IdCard</h1>
+          <IdCard
+            firstName={this.state.IdCardA.firstName}
+            lastName={this.state.IdCardA.lastName}
+            gender={this.state.IdCardA.gender}
+            height={this.state.IdCardA.height}
+            birth={this.state.IdCardA.birth}
+            picture={this.state.IdCardA.picture}
+          />
+          <IdCard
+            firstName={this.state.IdCardB.firstName}
+            lastName={this.state.IdCardB.lastName}
+            gender={this.state.IdCardB.gender}
+            height={this.state.IdCardB.height}
+            birth={this.state.IdCardB.birth}
+            picture={this.state.IdCardB.picture}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/IdCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function IdCard(props) {
+  return (
+    <div className="IdCard">
+      <span>
+        <img src={props.picture} alt={props.firstName} />
+      </span>
+      <div className="details">
+        <p>
+          <strong> First name:</strong> {props.firstName}
+        </p>
+        <p>
+          <strong> Last name:</strong> {props.lastName}
+        </p>
+        <p>
+          <strong> Gender:</strong> {props.gender}
+        </p>
+        <p>
+          <strong> Height</strong> {props.height}m
+        </p>
+        <p>
+          <strong> Birth:</strong>{' '}
+          {new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
+            props.birth
+          )}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default IdCard;
