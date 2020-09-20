aa0e351cefbc4252e3e67c5bcf04ea19950372ef
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-09-20 13:24:26 +0200

    iteration 7 complete

--- src/DriverCard/DriverCard.js
+++ src/DriverCard/DriverCard.js
@@ -2,7 +2,28 @@ import React from 'react';
 import './DriverCard.css';
 
 const DriverCard = (props) => {
-  return <div className="driver-card"></div>;
+  let resRating = '';
+  for (let i = 0; i < 5; i++) {
+    if (i < Math.round(props.rating)) resRating += '★';
+    else resRating += '☆';
+  }
+
+  return (
+    <div className="driver-card">
+      <div className="driver-img-div">
+        <img src={props.img} />
+      </div>
+      <div className="driver-info-div">
+        <p> {props.name} </p>
+        <p> {resRating} </p>
+        <div>
+          <p>
+            {props.car.model} <span> {props.car.licensePlate} </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DriverCard;
