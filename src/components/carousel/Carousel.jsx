b4335a27fca77694609bc684d79c1ca41b6fd626
Juan Romero <jrg89@hotmail.es>
2020-09-21 21:02:38 +0200

    iteration 11

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -9,24 +9,33 @@ export default class Carousel extends React.Component {
   }
 
   rightMove() {
-      if ({ img: this.props.imgs[0] }) {
+      if (this.state.img === this.props.imgs[0] ) {
         this.setState({ img: this.props.imgs[1] })
-      } else if ({ img: this.props.imgs[1] }) {
+      } else if (this.state.img ===  this.props.imgs[1] ) {
         this.setState({ img: this.props.imgs[2] })
-      } else if (({ img: this.props.imgs[2] })) {
+      } else if (this.state.img === this.props.imgs[2] ) {
         this.setState({ img: this.props.imgs[3] })
-      } else if ({ img: this.props.imgs[3] }) {
+      } else {
         this.setState({ img: this.props.imgs[0] })
-      }
-    ;
+      };
   }
 
-
+  leftMove() {
+    if (this.state.img === this.props.imgs[0] ) {
+      this.setState({ img: this.props.imgs[3] })
+    } else if (this.state.img ===  this.props.imgs[3] ) {
+      this.setState({ img: this.props.imgs[2] })
+    } else if (this.state.img === this.props.imgs[2] ) {
+      this.setState({ img: this.props.imgs[1] })
+    } else {
+      this.setState({ img: this.props.imgs[0] })
+    };
+}
 
   render() {
     return (
       <div>
-        <button onClick={() => this.getLike()}> Left</button>
+        <button onClick={() => this.leftMove()}> Left</button>
         <img src={this.state.img} style={{width: '300px'}}/>
         <button onClick={() => this.rightMove()}> Right</button>
       </div>
