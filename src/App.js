38716bf0fe041f997b5a58b1be6be7522ed42315
Hamlet <hayrapetyan.hamlet@gmail.com>
2020-09-22 12:38:53 +0200

    Iteration 4 on the way

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import IdCard from './components/IdCard'
 import Greetings from './components/Greetings'
 import Random from './components/Random'
+import BoxColor from './components/BoxColor'
+
 
 
 
@@ -37,6 +39,10 @@ function App() {
       <h1>Random</h1>
       <Random min={1} max={10}/>
       <Random min={20} max={100}/>
+
+      <h1>BoxColor</h1>
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
   </Fragment>
   );
 }
---
+++ src/components/BoxColor.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class BoxColor extends Component {
+    render() {
+        let r,g,b = this.props
+        return (
+            <div className="BoxColor box">
+                
+              </div>
+        )
+    }
+}
