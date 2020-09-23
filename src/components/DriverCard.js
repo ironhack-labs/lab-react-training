13567133c31b5ea60a05df99332a7aa5e2dbb250
Marly <marlenak90@gmail.com>
2020-09-23 12:54:16 +0200

    Iteration 7 finished

--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import Rating from './Rating.js';
+
 
 export default class DriverCard extends Component {
     render() {
         return (
-            <div>
+            <div style="background-color: #455EB5, color: white">
                 <div>
-                    <img src="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"></img>
+                    <img src={this.props.img} />
                 </div>
                 <div>
                     <h3>{this.props.name}</h3>
-                    <p>{this.props.rating}</p>
+                    <div><Rating children={this.props.rating} /></div>
                     <p>{this.props.car.model} - {this.props.car.licensePlate}</p>
                 </div>
             </div>
