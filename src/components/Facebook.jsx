06d25c03849e6710053a78093f2eec4a250f501b
Jaime Baro <jaimebaroval@gmail.com>
2020-09-23 19:20:49 +0200

    Iteration 13 ok

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import profiles from '../data/berlin.json';
 import FacebookCard from './FacebookCard'
 
-const firstName = profiles
-
-// }
+const profile = profiles
 
 class Facebook extends Component {
     render() {
         return (
             <ul>
-                {firstName.map(s => (<FacebookCard firstName={s.firstName} lastName={s.lastName} country={s.country} img={s.img} isStudent={s.isStudent}>
+                {profile.map(s => (<FacebookCard firstName={s.firstName} lastName={s.lastName} country={s.country} img={s.img} isStudent={s.isStudent}>
                 </FacebookCard>))}
             </ul>
         );
