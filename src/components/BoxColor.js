84cd5cbfb1da7152b86570b4cd24b7565f61dd76
Marly <marlenak90@gmail.com>
2020-09-23 12:17:22 +0200

    transformation from decimal to hexadecimal included

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -6,9 +6,21 @@ export default class BoxColor extends Component {
             "background-color": `rgb(${this.props.r}, ${this.props.g}, ${this.props.b})`,
             "border-color": "black"
         }
+        function hexLetter(num) {
+            let hexa = ["A", "B", "C", "D", "E", "F"];
+            (num > 9) ? num = hexa[(num - 10)] : num;
+            return num;
+        }
+
+        function hexFormat(item) {
+            str = hexLetter(Math.floor(item / 16)) + hexLetter(item % 16);
+            return str;
+        }
+
         return (
             <div style={color}>
-                rgb({this.props.r}, {this.props.g}, {this.props.b})
+                <p>rgb({this.props.r}, {this.props.g}, {this.props.b})</p>
+                <p>#{hexFormat(this.props.r)}{hexFormat(this.props.g)}{hexFormat(this.props.b)}</p>
             </div >
         )
     }
