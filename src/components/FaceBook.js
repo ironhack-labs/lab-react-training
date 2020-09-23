76ad9a56a318751e9f35b9816f6f86742afa9b7a
Olesya Steinmetz <newsabome@icloud.com>
2020-09-23 13:22:19 +0200

    WDFT-Berlin-Olesya

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -3,14 +3,37 @@ import profiles from '../data/berlin.json';
 
 export default class FaceBook extends Component {
     state = {
-        profiles: []
+        profiles: profiles
     }
+
+handleClick = (countryToFilter) => {
+    console.log(countryToFilter, "country")
+    let newProfiles = [...this.state.profiles].map(profile => {
+        if (profile.country == countryToFilter){
+            profile.back = "lightblue";
+            return profile
+        } else {
+            profile.back = "white";
+            return profile
+        }
+    })
+    console.log(newProfiles)
+    this.setState({
+        profiles: newProfiles
+    })
+}
 
     render() {
         return (
-        <div>{profiles.map(profile => {
+        <div>
+            {profiles.map(profile => {
+                return(
+                <button onClick={()=>this.handleClick(profile.country)}>{profile.country}</button>
+                )
+            })}
+            {profiles.map(profile => {
             return (
-                <div style={{width: '500px', height: '400px', border: '1px solid black'}}>
+                <div style={{width: '500px', height: '400px', border: '1px solid black', backgroundColor: `${profile.back}`}}>
                         <img src={profile.img} style={{width: '200px'}}/>
                         <p>First name:{profile.firstName}</p>
                         <p>Last name: {profile.lastName}</p>
