09ea65d08b7fbb08503e98c019276719a5ce7a18
Irma Blanco <irblancofdz@gmail.com>
2020-09-25 20:42:21 +0200

    feat: Iteration 14 without extras

--- src/components/facebook/Facebook.jsx
+++ src/components/facebook/Facebook.jsx
@@ -5,53 +5,37 @@ import profiles from '../../data/berlin.json';
 export default class FaceBook extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: false };
-    this.cardsList = this.cardsList;
+    this.state = {
+      country: '',
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.repeatedCountries = profiles.map((profile) => profile.country);
+    this.countries = this.repeatedCountries.filter(
+      (country, i) => this.repeatedCountries.indexOf(country) === i
+    );
   }
 
   handleClick(e) {
-    const selectedCountry = e.target.innerText;
-
-    console.log('selected Button', selectedCountry);
-
-    this.cardsList.forEach((card) => {
-      if (card.props.pais === selectedCountry) {
-        this.setState((state) => ({
-          isToggleOn: !state.isToggleOn,
-        }));
-      }
+    this.setState({
+      country: e.target.innerText,
     });
-  }
-
-  getBgColor() {
-    return this.state.isToggleOn ? '#66D3FA' : 'white';
   }
 
   render() {
-    let repeatedCountries = [];
-    profiles.forEach((profile) => {
-      repeatedCountries.push(profile.country);
-    });
-
-    let countries = repeatedCountries.filter(
-      (country, i) => repeatedCountries.indexOf(country) === i
-    );
-
-    const countriesList = countries.map((country, key) => (
+    const countriesList = this.countries.map((country, key) => (
       <button key={key} onClick={(e) => this.handleClick(e)}>
         {country}
       </button>
     ));
 
-    this.cardsList = profiles.map((profile, key) => (
+    const cardsList = profiles.map((profile, key) => (
       <div
-        className="Card"
-        pais={profile.country}
+        onClick={(e) => this.handleClick(e)}
+        className={`Card ${
+          this.state.country === profile.country ? 'card-blue' : 'card-white'
+        }`}
+        country={profile.country}
         key={key}
-        style={{
-          backgroundColor: this.getBgColor(),
-        }}
       >
         <div className="UserImage">
           <img className="Image" src={profile.img} />
@@ -83,7 +67,7 @@ export default class FaceBook extends React.Component {
       <div>
         <div>{countriesList}</div>
         <br />
-        <div>{this.cardsList}</div>
+        <div>{cardsList}</div>
       </div>
     );
   }
