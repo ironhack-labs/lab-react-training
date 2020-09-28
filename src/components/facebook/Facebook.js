15f3d1920b989735a1bf8dc7467d13297a968b13
Matthew Howard <mattalanhoward@gmail.com>
2020-09-28 21:55:04 +0200

    Half of Iteration 14

--- src/components/facebook/Facebook.js
+++ src/components/facebook/Facebook.js
@@ -4,34 +4,81 @@ import profiles from '../../data/berlin.json';
 export default class FaceBook extends Component {
   state = {
     profiles,
+    buttonColor: '',
+    isActive: false,
+    activeCountry: [],
+    country: '',
   };
 
   handleOnClick = (event) => {
-    //prevents rerender of whole page
-    event.preventDefault();
-    const { firstName, lastName } = this.state;
+    this.filterCountry();
+    let btnColor;
+    this.state.isActive ? (btnColor = '') : (btnColor = 'blue');
+    this.setState(
+      {
+        country: event.target.innerHTML,
+        buttonColor: btnColor,
+        isActive: !this.state.isActive,
+        activeCountry: btnColor,
+      },
+      () =>
+        console.log(
+          this.state.buttonColor,
+          this.state.isActive,
+          this.state.country,
+          this.state.activeCountry
+        )
+    );
+  };
 
-    //create new array that copies this.state.profiles(json) and adds new object to the beginning of that newProfilesArray
-    const newProfilesArray = [
-      { firstName: firstName, lastName: lastName },
-      ...this.state.profiles,
-    ];
-    //set state of profiles to newProfilesArray
-    //clear form (setState back to "")
-    this.setState({
-      profiles: newProfilesArray,
-      firstName: '',
-      lastName: '',
-    });
-    console.log(this.state.profiles);
+  //this is not yet working.
+  filterCountry = () => {
+    let filteredCountry = [];
+    for (let i = 0; i < 28; i++) {
+      if (this.state.country === this.state.profiles.country) {
+        filteredCountry.push(this.state.profiles);
+      }
+    }
+    console.log(filteredCountry);
   };
 
   render() {
     return (
       <div className="facebook-container">
+        <button
+          onClick={this.handleOnClick}
+          style={{
+            backgroundColor: `${this.state.buttonColor}`,
+          }}
+        >
+          USA
+        </button>
+        <button onClick={this.handleOnClick}>England</button>
+        <button onClick={this.handleOnClick}>Malaysia</button>
+        <button>Germany</button>
+        <button>Sweden</button>
+        <button>Nigeria</button>
+        <button>Italy</button>
+        <button>Scotland</button>
+        <button>Kazakhstan</button>
+        <button>Russia</button>
+        <button>Catalonia</button>
+        <button>France</button>
+        <button>Israel</button>
+        <button>Brazil</button>
+        <button>Taiwan</button>
+        <button>Turkey</button>
+        <button>Norway</button>
+
         <div>
-          {this.state.profiles.map((element) => (
-            <div className="profile-container">
+          {this.state.profiles.map((element, index) => (
+            <div
+              style={{
+                backgroundColor: `${this.state.buttonColor}`,
+              }}
+              key={index}
+              className="profile-container"
+            >
               <div>
                 <img id="idcard-img" src={element.img} alt="Head Shot" />
               </div>
