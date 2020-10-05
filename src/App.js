1b72362fbcaa338ecb89694da52ff2ea9bafde6c
pheno <nicolas.enault@gmail.com>
2020-10-05 16:58:22 +0200

    iteration 1&2 done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,60 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Greetings from "./components/Greetings";
+
+function IdCard(props) {
+  return (
+    <div className="Card">
+      <img src={props.picture} alt="picture" />
+      <p>First name: {props.firstName}</p>
+      <p>Last name: {props.lastName}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Height: {props.height}</p>
+      <p>Birth: {props.birth}</p>
+    </div>
+  );
+}
+
+
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    gender: 'male',
+    height: 1.78,
+    birth: new Date('1992-07-14').toString(),
+    picture: 'https://randomuser.me/api/portraits/men/44.jpg',
+  },
+  {
+    firstName: 'Obrien',
+    lastName: 'Delores',
+    gender: 'female',
+    height: 1.72,
+    birth: new Date('1993-05-11').toString(),
+    picture: 'https://randomuser.me/api/portraits/women/8.jpg',
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>IdCard</h2>
+        {people.map((person) => (
+          <IdCard
+            picture={person.picture}
+            firstName={person.firstName}
+            lastName={person.lastName}
+            gender={person.gender}
+            height={person.height}
+            birth={person.birth}
+          />
+        ))}
+
+        <h2>Greetings</h2>
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
       </header>
     </div>
   );
