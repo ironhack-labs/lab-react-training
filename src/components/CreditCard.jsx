4aaeb31c8a8e5c16fef18bdd34f7b1b36934b409
Rico <rasim.rico@gmail.com>
2020-10-05 21:32:56 +0200

    iteration 5

---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import '../App.css';
+// import visa from './img/visa.png'
+
+const CreditCard = (props) => {
+  const divStyle = {
+    backgroundColor: `${props.bgColor}`,
+  };
+
+  let strNum = `${props.number}`;
+  let countNum = '';
+
+  for (let i = 0; i < strNum.length; i++) {
+    countNum += '•';
+  }
+  let cardNum = countNum + `${strNum.slice(-4)}`;
+
+  let cardNumber = [...cardNum]
+    .map((num, i) => (i % 4 === 0 ? ' ' + ' ' + num : num))
+    .join('')
+    .trim();
+
+  return (
+    <div className="creditCard" style={divStyle}>
+      <div className="logo">
+        <img
+          src={(() => {
+            switch (props.type) {
+              case 'Visa':
+                return '/img/visa.png';
+              case 'Master Card':
+                return '/img/master-card.svg';
+              default:
+            }
+          })()}
+          alt="card"
+        />
+      </div>
+      <div
+        className="cardNumber"
+        style={{
+          color: (() => {
+            switch (props.type) {
+              case 'Visa':
+                return 'white';
+              case 'Master Card':
+                return 'black';
+              default:
+            }
+          })(),
+        }}
+      >
+        {cardNumber}
+      </div>
+      <div
+        style={{
+          color: (() => {
+            switch (props.type) {
+              case 'Visa':
+                return 'white';
+              case 'Master Card':
+                return 'black';
+              default:
+            }
+          })(),
+        }}
+      >
+        <span className="details">
+          Expires {props.expirationMonth}/{props.expirationYear}
+        </span>
+        &nbsp; &nbsp; <span>{props.bank}</span>
+        <br />
+        {props.owner}
+      </div>
+    </div>
+  );
+};
+
+export default CreditCard;
