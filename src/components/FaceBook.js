dd5b7a421c1bac3365d193377cf333ec36e60d2d
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-10-05 21:40:39 -0400

    iteration 14: working on selecting country btn

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react'
 import IdCard from './IdCard'
+// import CountryBtn from './CountryBtn'
 import profiles from '../data/berlin.json'
 
 export default class FaceBook extends Component {
     state = {
-        countries: []
+        countriesSelected: ["usa"]
     }
+    
+    // const countryArray = [];
 
-    componentDidMount = () => {
-        const countryArray = [];
+        // profiles.forEach(profile => {
+        //     !countryArray.includes(profile.country) && countryArray.push(profile.country);
+        // })
 
-        profiles.forEach(profile => {
-            !countryArray.includes(profile.country) && countryArray.push(profile.country);
-        })
+        // this.setState((preState) => ({
+        //     countriesBtns: countryArray
+        // }))
 
-        this.setState((preState) => ({
-            countries: countryArray
-        }))
+    getCountries = () => [...new Set(profiles.map(profile => profile.country))];
+
+    countriesBtnToggle = (country) => {
+        // this.setState((preState) => ({
+        //     countriesSelected: !preState.countriesSelected.includes(country) && preState.countriesSelected.push(country)
+        // }))
     }
 
     render() {
+        const btnStyle = {
+            backgroundColor: "lightblue"
+        }
         return (
             <div>
-                {this.state.countries.map((country, index) => 
-                    <button key={index}>{country}</button>
+                {this.getCountries()?.map((country, index) => 
+                    <button key={index} onClick={() => this.countriesBtnToggle(country)} name={country}>{country}</button>
                 )}
                 {profiles.map((profile, index) =>
                     <IdCard
