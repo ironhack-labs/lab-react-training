15c54ea9fa804cdf8e3cec321c0931cb40cfa541
kevin4052 <kevin.hernandez001@gmail.com>
2020-10-07 11:55:44 -0400

    done

--- src/components/RGBColorPicker.js
+++ src/components/RGBColorPicker.js
@@ -3,26 +3,51 @@ import SingleColorPicker from './SingleColorPicker'
 
 export default class RGBColorPicker extends Component {
     state = {
-        rValue: 0,
-        gValue: 0,
-        bValue: 0,
+        rValue: 150,
+        gValue: 150,
+        bValue: 150,
     }
 
-    colorChange = (name, value) => {
-        this.setState(() => ({
+    colorChange = async (name, value) => {
+        await this.setState(() => ({
             [name]: value
         }));
-
-        console.log(name, this.state[name])
     }
 
     render() {
+        const {rValue, gValue, bValue} = this.state;
+        const colorPreview = {
+            backgroundColor: `rgb(${rValue}, ${gValue}, ${bValue})`,
+            width: '50px',
+            height: '50px'
+        };
+        const pickerDiv = {
+            display: 'flex',
+            alignItems: 'center'
+        };
+
         return (
             <div>
-                <SingleColorPicker name="rValue" color='R' value={this.state.rValue} onChange={this.colorChange}/>
-                <SingleColorPicker name="gValue" color='G' value={this.state.gValue} onChange={this.colorChange}/>
-                <SingleColorPicker name="bValue" color='B' value={this.state.bValue} onChange={this.colorChange}/>
-                
+                <SingleColorPicker 
+                    name="rValue" 
+                    color='R' 
+                    value={this.state.rValue} 
+                    onChange={this.colorChange}/>
+                <SingleColorPicker 
+                    name="gValue" 
+                    color='G' 
+                    value={this.state.gValue} 
+                    onChange={this.colorChange}/>
+                <SingleColorPicker 
+                    name="bValue" 
+                    color='B' 
+                    value={this.state.bValue} 
+                    onChange={this.colorChange}/>
+
+                <div style={pickerDiv}>
+                    <div style={colorPreview}></div>
+                    <p>{`rgb(${rValue}, ${gValue}, ${bValue})`}</p>
+                </div>
             </div>
         )
     }
--- src/components/SingleColorPicker.js
+++ src/components/SingleColorPicker.js
@@ -1,27 +1,28 @@
 import React from 'react'
 
-export default function SingleColorPicker({name, color, value, onChange}) {
+const SingleColorPicker = ({name, color, value, onChange}) => {
     const colorPreview = {
         backgroundColor: name === "rValue" ? `rgb(${value}, 0, 0)` : name === "gValue" ? `rgb(0, ${value}, 0)` : `rgb(0, 0, ${value})`,
         width: '50px',
         height: '50px'
-    }
+    };
     const pickerDiv = {
         display: "flex",
-    }
+    };
 
-    const changeColor = (event) => {
-        console.log("singlePicker", name, value)
-        console.log({event})
+    const changeColor = (e) => {
+        const { value } = e.target;
         onChange(name, value);
-    }
+    };
 
     return (
         <div style={pickerDiv}>
             <div style={colorPreview}></div>
             <label>{`${color}: `}
-                <input type="number" min="0" max="255" value={value} onClick={() => changeColor()} />
+                <input type="number" min="0" max="255" value={value} onChange={changeColor} />
             </label>
         </div>
-    )
+    );
 }
+
+export default SingleColorPicker;
