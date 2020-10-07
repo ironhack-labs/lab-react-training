3fa8a3e31a9c83ae9849cf8a6ace187b416b3dc7
Lisa Poncet <poncet.lisa@gmail.com>
2020-10-07 12:58:39 +0200

    iteration facebook almost done: can't set state to empty between btn clicks

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import profiles from '../data/berlin.json';
 
-const Facebook = (props) => {
-  return (
-    <div>
-      {profiles.map((student, index) => (
-        <div className="profile" key={index}>
-          <img src={student.img} alt="" />
-          <div>
-            <div>First name: {student.firstName}</div>
-            <div>Last name:{student.lastName}</div>
-            <div>Country:{student.country}</div>
-            <div>Type: {student.isStudent ? 'Student' : 'Teacher'}</div>
-          </div>
+class Facebook extends Component {
+  state = {
+    countrySelected: '',
+  };
+
+  handleClick = (event, country) => {
+    this.setState({
+      countrySelected: country,
+    });
+    console.log(this.state);
+  };
+
+  render() {
+    let allCountries = profiles.map((student) => student.country);
+    let countries = [...new Set(allCountries)];
+
+    return (
+      <div>
+        <div className="countries-btn">
+          {countries.map((country) => (
+            <button
+              style={{
+                backgroundColor:
+                  this.state.countrySelected === country ? 'lightblue' : 'none',
+              }}
+              key={country}
+              onClick={(event) => this.handleClick(event, country)}
+            >
+              {country}
+            </button>
+          ))}
         </div>
-      ))}
-    </div>
-  );
-};
+
+        {profiles.map((student, i) => (
+          <div
+            style={{
+              backgroundColor:
+                this.state.countrySelected === student.country
+                  ? 'lightblue'
+                  : 'none',
+            }}
+            className="profile"
+            key={i}
+          >
+            <img src={student.img} alt="" />
+            <div>
+              <div>First name: {student.firstName}</div>
+              <div>Last name:{student.lastName}</div>
+              <div>Country:{student.country}</div>
+              <div>Type: {student.isStudent ? 'Student' : 'Teacher'}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Facebook;
