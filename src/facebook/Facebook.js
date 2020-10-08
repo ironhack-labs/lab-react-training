baf2808339a8d1d5e890bdeed11f619eff52d3ee
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-10-07 20:28:10 -0400

    Complete iteration 14

--- src/facebook/Facebook.js
+++ src/facebook/Facebook.js
@@ -3,21 +3,51 @@ import profiles from '../data/berlin.json';
 
 
 export default class Facebook extends Component {
+
+    //get the unique countries
+    countries = [...new Set(profiles.map(profile => profile.country))]
+
+    state = {
+        selected: null
+    }
+
+    handleButtonClick(country) {
+        this.setState(prevState => ({
+            selected: country
+        }))
+    }
+
     render() {
         return (
             <div>
+                {/* buttons */}
+                {this.countries.map(country => (
+                    <button
+                        key={country}
+                        style={{
+                            backgroundColor: this.state.selected === country ? 'lightblue' : 'white'
+                        }}
+                        onClick={() => this.handleButtonClick(country)} >
+                        {country}
+                    </button>
+                ))}
+
+                {/* profiles */}
                 {profiles.map((profile, i) => {
                     return (
-                    <div key={i}>
-                        <div><img width="100" src={profile.img}/></div>
-                        <div>{profile.firstName}</div>
-                        <div>{profile.lastName}</div>
-                        <div>{profile.country}</div>
-                        <div>{profile.isStudent? 'Student' : 'Teacher'}</div>
-                    </div>
+                        <div key={i} style={{
+                            backgroundColor: profile.country === this.state.selected ? 'lightblue' : 'white'
+                        }} >
+                            <div><img width="100" src={profile.img} /></div>
+                            <div>{profile.firstName}</div>
+                            <div>{profile.lastName}</div>
+                            <div>{profile.country}</div>
+                            <div>{profile.isStudent ? 'Student' : 'Teacher'}</div>
+                        </div>
                     )
                 })}
             </div>
         )
     }
 }
+
