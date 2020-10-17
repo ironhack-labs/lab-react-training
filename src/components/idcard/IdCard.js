4e5d5185cd54cb8d9f22d88e6b0ac388c064b4ad
anna dodo <anna.dorenskaya@gmail.com>
2020-10-17 15:49:34 +0200

    1st iteration

---
+++ src/components/idcard/IdCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function IdCard(props) {
+    return (
+        <div>
+            <div>First name: {props.firstName}</div>
+            <div>Last name: {props.lastName}</div>
+            <div>Gender: {props.gender}</div>
+            <div>Height: {props.height}</div>
+            <div>Birth: {props.birth}</div>
+            <div>
+                <img src={props.picture} alt=""/>
+            </div>
+        </div>
+    )
+}
