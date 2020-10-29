a9a3d326abd5fdbdab9777d41055a7f9dfdfeae1
Leonardo Mezzanotti <leomezza@gmail.com>
2020-10-29 15:41:46 -0300

    Iteration 1 done

---
+++ src/components/IdCard/IdCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './IdCard.css';
+
+const genderEnum = {
+  male: "male",
+  female: "female",
+};
+
+const IdCard = ({ lastName, firstName, gender, height, birth, picture }) => {
+
+  const birthCard = date => date.toString().split(' ', 4).join(' ');
+
+  const genderCard = gender => genderEnum[gender] ? gender : 'unknown';
+
+  const heightCard = height => height / 100;
+
+  return (
+    <div className="card-container">
+      <img src={picture} alt={`${firstName} ${lastName}`} />
+
+      <div>
+        <p><strong>First name: </strong>{firstName}</p>
+        <p><strong>Last name: </strong>{lastName}</p>
+        <p><strong>Gender: </strong>{genderCard(gender)}</p>
+        <p><strong>Height: </strong>{heightCard(height)}m</p>
+        <p><strong>Birth: </strong>{birthCard(birth)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default IdCard;
