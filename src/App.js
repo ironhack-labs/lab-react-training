d3e7c7945951d2afea3ed1782e38d255d2b9b40a
Leonardo Mezzanotti <leomezza@gmail.com>
2020-10-29 17:30:01 -0300

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import IdCard from './components/IdCard/IdCard';
 import Greetings from './components/Greetings/Greetings'
 import Random from './components/Random/Random'
+import BoxColor from './components/BoxColor/BoxColor'
 
 function App() {
   return (
@@ -34,6 +35,9 @@ function App() {
 
       <Random min={1} max={6} />
       <Random min={1} max={100} />
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
---
+++ src/components/BoxColor/BoxColor.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './BoxColor.css';
+
+const BoxColor = ({ r, g, b }) => {
+
+  const hexColor = (r, g, b) => {
+    const rHex = ("0" + r.toString(16)).slice(-2);
+    const gHex = ("0" + g.toString(16)).slice(-2);
+    const bHex = ("0" + b.toString(16)).slice(-2);
+    return `#${rHex}${gHex}${bHex}`
+  };
+
+  const textColor = (r, g, b) => {
+    if ((r * 0.299 + g * 0.587 + b * 0.114) > 186) {
+      return 'black';
+    } else {
+      return 'white';
+    }
+  };
+
+  const boxColor = {
+    backgroundColor: hexColor(r, g, b),
+    color: textColor(r, g, b),
+  }
+
+  return (
+    <div style={boxColor} className="boxcolor-container">
+      <h2>rgb({r},{g},{b})</h2>
+      <h2>{hexColor(r, g, b)}</h2>
+    </div>
+  );
+};
+
+export default BoxColor;
