1bb7ded552e6847b17e2f3490bcef3849724ebd2
Irene A <ihatepineapple@gmail.com>
2020-11-24 16:21:55 +0100

    Iteration 7

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Random from './components/random.js'
 import BoxColor from './components/boxColor.js'
 import CreditCard from './components/creditCard.js'
 import Rating from './components/rating.js'
-
+import DriverCard from './components/driverCard.js'
 
 function App() {
   return (
@@ -76,6 +76,7 @@ function App() {
       </div>
 
       <h1>Rating</h1>
+      <div className="stars-container">
       <Rating>0</Rating>
       <Rating>1.49</Rating>
       <Rating>1.5</Rating>
@@ -83,8 +84,25 @@ function App() {
       <Rating>4</Rating>
       <Rating>5</Rating>
 
+      </div>
 
       <h1>Driver Card</h1>
+      <DriverCard
+        name="Travis Kalanick"
+        rating={4.2}
+        img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+        car={{
+          model: "Toyota Corolla Altis",
+          licensePlate: "CO42DE"
+        }} />
+      <DriverCard
+        name="Dara Khosrowshahi"
+        rating={4.9}
+        img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+        car={{
+          model: "Audi A3",
+          licensePlate: "BE33ER"
+        }} />
 
     </div>
 
---
+++ src/components/driverCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Rating from './rating.js'
+
+const DriverCard = (props) => {
+    return (
+        <div className="driver-card">
+            <div className="img-container">
+                <img src={props.img}/>
+            </div>
+            <div className="info-driver">
+                <h3>{props.name}</h3>
+                <p><Rating>{props.rating}</Rating></p>
+                <p>{props.car.model}{props.car.licensePlate}</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default DriverCard;
