c34d6b183c9ce2c6b1dba361137555164640c607
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-24 15:28:48 -0600

    cards

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const CreditCard = (props) => {
   return (
-    <div
-      className="creditCard"
-      style={{ backgroundColor: `${props.bgColor}`, color: `${props.color}` }}
-    >
-      <p>{props.type}</p>
-      <p>{props.number}</p>
+    <div className="cards">
+      <div
+        className="creditCard"
+        style={{ backgroundColor: `${props.bgColor}`, color: `${props.color}` }}
+      >
+        <p>{props.type}</p>
+        <p>{props.number}</p>
 
-      <p>
-        {props.expirationMonth}/{props.expirationYear} {props.bank}
-      </p>
-      <p>{props.owner}</p>
+        <p>
+          {props.expirationMonth}/{props.expirationYear} {props.bank}
+        </p>
+        <p>{props.owner}</p>
+      </div>
     </div>
   );
 };
