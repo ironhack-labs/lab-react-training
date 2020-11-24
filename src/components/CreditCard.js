10d543b7c83d723784e2df5c4547a78664e998aa
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-24 15:19:48 -0600

    cards

---
+++ src/components/CreditCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CreditCard = (props) => {
+  return (
+    <div
+      className="creditCard"
+      style={{ backgroundColor: `${props.bgColor}`, color: `${props.color}` }}
+    >
+      <p>{props.type}</p>
+      <p>{props.number}</p>
+
+      <p>
+        {props.expirationMonth}/{props.expirationYear} {props.bank}
+      </p>
+      <p>{props.owner}</p>
+    </div>
+  );
+};
+
+export default CreditCard;
