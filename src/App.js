59e825f24c5caa91d8a146a4f2342165993a658d
Abbie <abbie@summers.gg>
2020-11-25 20:11:08 +0100

    Iteration 7 complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Random from './components/random.js'
 import BoxColor from './components/boxColor.js'
 import CreditCard from './components/creditCard.js'
 import Rating from './components/rating.js'
+import DriverCard from './components/driverCard.js'
 
 function App() {
   return (
@@ -72,13 +73,35 @@ function App() {
         color="white" />
 
       <h1>Rating</h1>
-      <Rating>0</Rating>
-      <Rating>1.49</Rating>
-      <Rating>1.5</Rating>
-      <Rating>3</Rating>
-      <Rating>4</Rating>
-      <Rating>5</Rating>
+      <div classname="star-container">
+        <Rating>0</Rating>
+        <Rating>1.49</Rating>
+        <Rating>1.5</Rating>
+        <Rating>3</Rating>
+        <Rating>4</Rating>
+        <Rating>5</Rating>
+      </div>
+
+      <h1>Driver Card</h1>
+      <DriverCard
+        name="Travis Kalanick"
+        rating={4.2}
+        img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+        car={{
+          model: "Toyota Corolla Altis",
+          licensePlate: "CO42DE"
+        }} />
+      <DriverCard
+        name="Dara Khosrowshahi"
+        rating={4.9}
+        img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+        car={{
+          model: "Audi A3",
+          licensePlate: "BE33ER"
+        }} />
+
     </div>
+
   );
 }
 export default App;
