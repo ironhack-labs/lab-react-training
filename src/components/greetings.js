2bf90d6772a9c4f1d6313698716c0ba83b8a3275
Abbie <abbie@summers.gg>
2020-11-25 15:39:02 +0100

    iteration 2 complete

---
+++ src/components/greetings.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Greetings = (props) => {
+    function message(lang) {
+        switch (lang) {
+            case 'de':
+                return 'Hallo, ';
+            break;
+            case 'fr':
+                return 'Bonjour, ';
+            break;
+            case 'es':
+                return 'Hola, ';
+            break;
+            default:
+                return 'Hello, ';
+            break;
+        }
+    }
+
+    return (
+        <div className="greetings-container">
+            <p>{message(props.lang)}{props.children}</p>
+        </div>
+    )
+};
+
+export default Greetings;
