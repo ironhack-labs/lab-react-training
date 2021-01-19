408f13ac1bed26316406e654f18a346f8d4bc39a
Daina Slezaite <daina.slezaite@gmail.com>
2021-01-19 16:14:10 +0100

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 import {IdCard} from './components/IdCard/IdCard';
+import {Greetings} from './components/Greetings/Greetings';
 
 function App() {
   return (
     <React.Fragment>
       <h1>Id Cards</h1>
-      <IdCard image="https://randomuser.me/api/portraits/men/44.jpg" firstName="John" lastName="Doe" gender="male" height={178 + 'm'} birthday={ new Date("1992-07-14").toString().slice(0, 15) } />
-      <IdCard image="https://randomuser.me/api/portraits/women/44.jpg" firstName="Daina" lastName="Slezaite" gender="female" height={172 + 'm'} birthday={ new Date("1996-06-13").toString().slice(0, 15) } />
+        <IdCard image="https://randomuser.me/api/portraits/men/44.jpg" firstName="John" lastName="Doe" gender="male" height={178 + 'm'} birthday={ new Date("1992-07-14").toString().slice(0, 15) } />
+        <IdCard image="https://randomuser.me/api/portraits/women/44.jpg" firstName="Daina" lastName="Slezaite" gender="female" height={172 + 'm'} birthday={ new Date("1996-06-13").toString().slice(0, 15) } />
+      <h1>Greetings</h1>
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">Michelle</Greetings>
     </React.Fragment>
+
   );
 }
 
---
+++ src/components/Greetings/Greetings.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Greetings.css';
+
+function Greetings(props) {
+    let hello = '';
+    if(props.lang == 'en') {
+        hello = 'Hello';
+    }
+    if(props.lang == 'de') {
+        hello = 'Hallo';
+    }
+    if(props.lang == 'es') {
+        hello = 'Hola';
+    }
+    if(props.lang == 'fr') {
+        hello = 'Bonjour';
+    }
+
+    return (
+        <div>
+            <p className="greetings-box" lang={props.lang}>{hello} {props.children}</p>
+        </div>
+    )
+}
+
+export {Greetings};
