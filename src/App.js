7602491602e16a8e1488b58bb242ad3b05d8b3c1
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-19 14:06:04 +0100

    complete iteration 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './idcard/IdCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IdCard</h1>
+        <IdCard picture="https://randomuser.me/api/portraits/men/44.jpg" firstName="John" lastName="Doe" gender="male" height={178} birth={new Date("1992-7-14")} />
+        <IdCard picture="https://randomuser.me/api/portraits/women/44.jpg" firstName="Deloris" lastName="Obrien" gender="female" height={172} birth={new Date("1988-5-11")} />
     </div>
   );
 }
