e62b89c41d6d1990d8c1183c18352b6977a85536
ZuzanaAK <kubatovazuzana@gmail.com>
2021-01-19 10:18:02 +0100

    iteration 1 done

---
+++ src/components/IdCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../components/IdCard.css'
+
+class IdCard extends React.Component {
+    render() {
+        return(
+            <div className="IdCard">
+                <img class="profileImg" src={this.props.picture} alt={`${this.props.firstName}`}/>
+                <div className ="profileInfo">
+                    <p><b>Last Name: </b>{this.props.lastName}</p>
+                    <p><b>First Name: </b>{this.props.firstName}</p>
+                    <p><b>Gender: </b>{this.props.gender}</p>
+                    <p><b>Height: </b>{this.props.height}</p>
+                    <p><b>Birth: </b>{this.props.birth}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default IdCard;
