f0a19f2f33c52f14ed2c95b601f7a36d4de2a7c3
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-22 10:52:04 +0100

    complete iteration 4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './Box.css';
 import IdCard from './idcard/IdCard';
 import Greetings from './greetings/Greetings';
 import Random from './random/Random';
+import BoxColor from './boxcolor/BoxColor';
 
 function App() {
   return (
@@ -19,6 +20,10 @@ function App() {
       <h1>Random</h1>
       <Random min={1} max={6}/>
       <Random min={1} max={100} />
+
+      <h1>Box Color</h1>
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
---
+++ src/boxcolor/BoxColor.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './BoxColor.css';
+
+const BoxColor = props => {
+    const boxStyle = {
+        backgroundColor: `rgb(${props.r}, ${props.g}, ${props.b})`,
+        border: "1px solid black",
+        display: "flex",
+        height: "100px",
+        width: "99%",
+        marginTop: "10px",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+
+    return(
+        <div style={boxStyle}>
+            <p>rgb({props.r},{props.g},{props.b})</p>
+        </div>
+    )
+}
+
+export default BoxColor;
