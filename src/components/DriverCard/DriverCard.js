42a1c2ed1b61af23b7dd3674933332dde9245a8c
Daina Slezaite <daina.slezaite@gmail.com>
2021-01-25 16:14:25 +0100

    Driver card styling added

--- src/components/DriverCard/DriverCard.js
+++ src/components/DriverCard/DriverCard.js
@@ -5,11 +5,9 @@ import {Rating} from '../Rating/Rating';
 function DriverCard(props) {
     return(
         <div id="driver-card">
-            <div>
-                <img className='driver-img' src={props.img} />
-            </div>
-            <div>
-                <p>{props.name}</p>
+            <img className='driver-img' src={props.img} />
+            <div className='driver-card-text'>
+                <p><strong>{props.name}</strong></p>
                 <Rating>{props.rating}</Rating>
                 <p>{props.car.model} - {props.car.licensePlate}</p>
             </div>
