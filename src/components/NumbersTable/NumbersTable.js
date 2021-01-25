13464803631e12f9d99613fefc8eac35c6c2c1d2
Daina Slezaite <daina.slezaite@gmail.com>
2021-01-26 00:04:47 +0100

    Iteration 12 done

---
+++ src/components/NumbersTable/NumbersTable.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './NumbersTable.css';
+
+class NumbersTable extends Component {
+    render() {
+        const numbers = [];
+        let number = 0;
+
+        do {
+            number++
+            numbers.push(number);
+        }
+        while(numbers.length < this.props.limit)
+
+
+        const listNumbers = numbers.map(number => {
+            return number % 2 === 0 ? <li className='grid-item'>{number}</li> : <li className='grid-item red'>{number}</li> 
+        })
+
+        return (
+            <div className='grid-container'>
+                {listNumbers}
+            </div>
+        )
+    }
+}
+
+export {NumbersTable};
