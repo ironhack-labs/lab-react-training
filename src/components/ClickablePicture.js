9b402a2328e366f4cf2870744f5658d93e57a0c9
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-26 12:49:38 +0100

    complete iteration 9

---
+++ src/components/ClickablePicture.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ClickablePicture extends React.Component {
+    state = {
+        clicked: false
+    }
+
+    toggleImg() {
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    }
+
+    render() {
+        return (
+            <img src={!this.state.clicked ? this.props.img : this.props.imgClicked} onClick={() => this.toggleImg()} />
+        )
+    }
+}
+
+export default ClickablePicture;
