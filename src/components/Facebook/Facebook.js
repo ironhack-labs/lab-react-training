32ea8e01def3e539d4064e8161f1eef6a62af4c7
Daina Slezaite <daina.slezaite@gmail.com>
2021-01-26 16:23:33 +0100

    Iteration 14 done

--- src/components/Facebook/Facebook.js
+++ src/components/Facebook/Facebook.js
@@ -4,12 +4,29 @@ import './Facebook.css';
 
 class Facebook extends React.Component {
 
-    state = { profiles }
+    state = { 
+        profiles: profiles,
+        selectedProfiles: []
+    }
+
+    handleClick = e => {
+        if(e.target.innerHTML !== 'All') {
+            const sortedProfiles = this.state.profiles.filter(profile => profile.country === e.target.innerHTML);
+        
+            this.setState({
+                selectedProfiles: sortedProfiles
+            })
+        } else {
+            this.setState({
+                selectedProfiles: []
+            })
+        }
+    }
 
     render() {
         const newProfiles = this.state.profiles.map(profile => {
             return (
-                <div key={profile.firstName} className='facebook-profile'>
+                <div style={{backgroundColor: this.state.selectedProfiles.includes(profile) ? 'lightblue' : 'white'}} key={profile.firstName} className='facebook-profile'>
                     <img src={profile.img} alt='Facebook profile' />
                     <div className='profile-text'>
                         <p><strong>First name:</strong> {profile.firstName}</p>
@@ -18,13 +35,24 @@ class Facebook extends React.Component {
                         {profile.isStudent ? <p><strong>Type:</strong> Student</p> : <p><strong>Type:</strong> Teacher</p>} 
                     </div>
                 </div>
-
             ) 
+        })
+
+        const countryArr = [...new Set(this.state.profiles.map(profile => {
+            return profile.country;
+        }))];
+
+        const countryButtons = countryArr.map(country => {
+            return (
+                <button onClick={this.handleClick} className='country-button' key={country}>{country}</button>
+            )
         })
 
         return (
             <div>
-            {newProfiles} 
+                <button onClick={this.handleClick} className='country-button'>All</button>
+                {countryButtons}
+                {newProfiles} 
             </div>
         )
     }
