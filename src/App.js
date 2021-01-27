4ed6e087b9b28cbbaa2dd015b48af9a4d505a682
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-27 12:22:37 +0100

    complete iteration 15 without bonus or styling

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Dice from './components/Dice';
 import Carousel from './components/Carousel';
 import NumbersTable from './components/NumbersTable';
 import FaceBook from './components/FaceBook';
+import SignupPage from './components/SignupPage';
 
 function App() {
   return (
@@ -113,6 +114,10 @@ function App() {
 
       <h1>FaceBook</h1>
       <FaceBook />
+
+      <h1>SignupPage</h1>
+      <SignupPage />
+
     </div>
   );
 }
---
+++ src/components/SignupPage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class SignupPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            nationality: '',
+            submitted: false
+        }
+    }
+
+    handleChange = (e) => {
+        const propertyName = e.target.name
+
+        this.setState({[propertyName]: e.target.value})
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+    }
+
+    render() {
+        let greeting = ''
+        switch (this.state.nationality){
+            case "en":
+                greeting = 'Hello';
+                break;
+            case "de":
+                greeting = 'Hallo';
+                break;
+            case "fr":
+                greeting = 'Bonjour';
+                break;
+            default:
+                greeting = 'Hi';
+        }
+
+        return (
+            <div>
+                <form>
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="Your email" value={this.state.email} onChange={this.handleChange}/>
+
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="****" value={this.state.password} onChange={this.handleChange}/>
+
+                    <label>Nationality</label>
+                    <select name="nationality" value={this.state.nationality} onChange={this.handleChange}>
+                        <option value="en">English</option>
+                        <option value="de">German</option>
+                        <option value="fr">French</option>
+                    </select>
+                    <button onClick={this.handleSubmit}>Submit</button>
+                </form>
+                
+                <hr />
+                <p>{greeting}</p>
+                <p>Your email address is: {this.state.email}</p>
+            </div>
+        )
+    }
+}
+
+export default SignupPage;
