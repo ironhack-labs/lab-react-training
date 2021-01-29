b0f40fee8eaf0aadb3fc7fb8a033370db9cc18f9
Thrashskin <jmontegonz@gmail.com>
2021-01-29 14:30:00 +0100

    Iteration 10 complete

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Rating from './components/Rating';
 import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClikcablePicture';
+import Dice from './components/Dice';
 
 
 function App() {
@@ -89,6 +90,13 @@ function App() {
         img='/img/persons/maxence.png'
         imgClicked='/img/persons/maxence-glasses.png'
       />
+
+      <br/>
+      <br/>
+      <br/>
+      <h1>Dice</h1>
+
+      <Dice />
 
     </div>
   );
---
+++ src/components/Dice.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Dice.css'
+
+class Dice extends React.Component {
+    constructor(props){
+        super(props);
+        this.getRandomPic = this.getRandomPic.bind(this)
+        this.setRandomPic = this.setRandomPic.bind(this)
+        this.state = {
+            img: `${this.getRandomPic()}`
+        }
+    }
+    
+    getRandomPic() {
+        let randN = Math.floor(Math.random() * 6 + 1)        
+        return `./img/dice${randN}.png`
+    }
+
+    setRandomPic() {
+
+        let empty = './img/dice-empty.png'
+
+        setTimeout((() => {
+            this.setState({
+                img: empty
+            })
+        }),1)
+
+        setTimeout((() => {
+            this.setState({
+                img: this.getRandomPic()
+            })
+        }),1000)
+    }
+
+   
+
+    //'./img/dice-empty.png'
+
+    render() {
+        return(
+            <div className="img-container">
+                <img src={this.state.img} className="dice" onClick={this.setRandomPic}/>
+            </div>
+        );
+    }
+}
+
+export default Dice;
