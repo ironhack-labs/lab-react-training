c4689f856e39fcf6ed39fd1149460d821ce4a859
shtesh <vshtefanuka@gmail.com>
2021-02-16 18:09:52 +0100

    Carousel component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
 import Dice from './components/Dice';
+import Carousel from './components/Carousel';
 import Test from './components/Test/Test';
 import './components/CreditCard.css';
 import './components/DriverCard.css';
@@ -115,6 +116,16 @@ function App() {
 
       <h1>Iteration 10</h1>
       <Dice />
+
+      <h1>Iteration 11</h1>
+      <Carousel
+        imgs={[
+          'https://randomuser.me/api/portraits/women/1.jpg',
+          'https://randomuser.me/api/portraits/men/1.jpg',
+          'https://randomuser.me/api/portraits/women/2.jpg',
+          'https://randomuser.me/api/portraits/men/2.jpg'
+        ]}
+      />
     </div>
   );
 }
