636fa999468cf3558cb9bcb0fe6b65f1ff914494
honghainguyen777 <n.h.hai2015@gmail.com>
2021-02-22 16:38:15 +0100

    iteration 1

---
+++ src/components/IdCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const IdCard = ({lastName, firstName, gender, height, birth, picture}) => {
+    return (
+        <div className="IdCard border">
+            <img src={picture} alt=""/>
+            <div className="card-content">
+                <strong>First name</strong>: {firstName}
+                <br />
+                <strong>Last name</strong>: {lastName}
+                <br />
+                <strong>Gender</strong>: {gender}
+                <br />
+                <strong>Height</strong>: {height}m
+                <br />
+                <strong>Birth</strong>: {birth.getDate()}/{birth.getMonth()}/{birth.getFullYear()}
+            </div>
+        </div>
+    );
+}
+
+export default IdCard;
