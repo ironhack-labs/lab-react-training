026e4f31fa0847b3960d2e437adcb157edbc0c73
Rob TC <roberto.taboada@gmail.com>
2021-02-23 20:55:27 -0600

    Hasta la iteración 11

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Rating from './components/Rating'
 import DriverCard from './components/DriverCard'
 import LikeButton from './components/LikeButton'
 import ClickablePicture from './components/ClickablePicture'
+import Dice from './components/Dice'
+import Carousel from './components/Carousel'
 
 function App() {
   return (
@@ -106,6 +108,17 @@ function App() {
 <ClickablePicture
   img='/img/persons/maxence.png'
   imgClicked='/img/persons/maxence-glasses.png'
+/>
+
+<Dice />
+
+<Carousel
+  imgs={[
+    'https://randomuser.me/api/portraits/women/1.jpg',
+    'https://randomuser.me/api/portraits/men/1.jpg',
+    'https://randomuser.me/api/portraits/women/2.jpg',
+    'https://randomuser.me/api/portraits/men/2.jpg'
+  ]}
 />
 
     </div>
---
+++ src/components/Carousel.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+export default function Carousel(props) {
+    
+    const [index, setIndex] = useState(0)
+    
+    const [images, setImages] = useState(props.imgs[index])
+    
+    function changeR() {
+        setIndex(index+1)
+        if (index >= 3){
+            setIndex(index-3)
+        }
+        setImages(props.imgs[index])
+        console.log(index)
+    }
+    function changeL(){
+        setIndex(index-1)
+        if (index <= 0){
+            setIndex(props.imgs.length-1)
+        }
+        setImages(props.imgs[index])
+        console.log(index)
+    }
+    return (
+        <div>
+            <img src={images} />
+            <button onClick={()=>changeR()}>Right</button>
+            <button onClick={()=>changeL()}>Left</button>
+        </div>
+    )
+}
