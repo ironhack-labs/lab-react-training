0ae83aa01366827ed1be514c1c32f8e42f68a2e4
EduLp99 <edulp99@gmail.com>
2021-02-26 10:35:07 -0300

    done

---
+++ src/components/BoxColor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BoxColor(props) {
+    const color = props.hex
+      ? props.hex
+      : `rgb(${props.r}, ${props.g}, ${props.b})`;
+    return (
+      <div className={props.class} style={{ backgroundColor: color }}>
+        {props.children}
+      </div>
+    );
+  }
+
+export default BoxColor;
---
+++ src/components/CreditCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import BoxColor from './BoxColor';
+
+function CreditCard(props) {
+    return (
+        <div className="card-size m-2">
+        <BoxColor hex={props.bgColor} class="rounded-3">
+          <div className="card-body" style={{ color: props.color }}>
+            <header className="d-flex flex-row-reverse">
+              <img
+                className="img-size mb-1"
+                src={props.type === 'Visa'}
+                alt="Credit-card logo"
+              />
+            </header>
+            <h2>•••• •••• •••• {props.number.substr(-4)}</h2>
+            <div className="row">
+              <div className="text-truncate">
+                <small>
+                  Expires: {props.expirationMonth}/{props.expirationYear}
+                </small>
+                <span className="ms-5">
+                  <small>{props.bank}</small>
+                </span>
+              </div>
+              <div>
+                <small>{props.owner}</small>
+              </div>
+            </div>
+          </div>
+        </BoxColor>
+      </div>
+    )
+}
+
+export default CreditCard;
