980d41fb7589fb9603de95ca660ed62b061d0e6e
asierbayon <bayonasier@gmail.com>
2021-02-27 14:57:11 +0100

    feat: iteration 6 refactorized

--- src/components/rating/Rating.js
+++ src/components/rating/Rating.js
@@ -1,22 +1,9 @@
 const Rating = ({children}) => {
-    const rating = Math.floor(children);
-    let filledStars = '';
-    let emptyStars = '';
-    function fillStarsFunc() {
-        for (let i = 0; i < rating; i++ ) {
-            filledStars += '★';
-        }
-    }
-    function emptyStarsFunc() {
-        for (let i = 0; i < 5- rating; i++ ) {
-            filledStars += '☆';
-        }
-    }
-    fillStarsFunc();
-    emptyStarsFunc();
+    const rating = Math.round(children);
+    const emptyStars = 5 - rating;
     return (
-       <div className="d-flex">
-           <span>{filledStars}</span><span>{emptyStars}</span>
+       <div>
+           {'★'.repeat(rating)}{'☆'.repeat(emptyStars)}
        </div>
     );
 }
