dbf221063bc1435d9ce4c70bb4b53e9c0bb8180c
fmmyriam <benhmyriam@gmail.com>
2021-03-16 10:31:55 +0100

    did the missing part so that the rating import works

--- src/Components/DriverCard.jsx
+++ src/Components/DriverCard.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-// import Rating from './../Components/Rating';
+import Rating from './../Components/Rating';
 // import "../styles/DriverCard.css";
 
 const DriverCard = (props) => {
-    function stars(note) {
+    // function stars(note) {
        
-       let rating = Math.round(note);
-       let text = ''; 
-       for (let i = 0; i < rating; i++) {
-           text+='★';
-       }
-       while (text.length < 5) {
-        text+='☆';
-       }
-    return text;
-    }
+    //    let rating = Math.round(note);
+    //    let text = ''; 
+    //    for (let i = 0; i < rating; i++) {
+    //        text+='★';
+    //    }
+    //    while (text.length < 5) {
+    //     text+='☆';
+    //    }
+    // return text;
+    // }
 
 
 
@@ -27,7 +27,10 @@ const DriverCard = (props) => {
                         <img src={props.img} alt="picture" style={{width: 150}}/><br />
                 
                         {props.name} <br />
-                        {stars(props.rating)} <br />
+                        {/* {stars(props.rating)} <br /> */}
+                        <Rating>
+                            {props.rating}
+                        </Rating><br />
                         {props.car.model} <br />
                         {props.car.licensePlate} <br />
                     </span>
