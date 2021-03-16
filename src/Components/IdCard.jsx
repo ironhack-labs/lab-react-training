93c1c8d1e3a18659413c92567c02fa35c0ee1932
Mélusine Rey <zelumine@live.fr>
2021-03-16 09:47:18 +0100

    Fix date in IdCard

--- src/Components/IdCard.jsx
+++ src/Components/IdCard.jsx
@@ -10,7 +10,7 @@ function IdCard(props) {
         <p><b>Last name:</b> {props.lastName}</p>
         <p><b>Gender:</b> {props.gender}</p>
         <p><b>Height:</b> {props.height}</p>
-        <p><b>Birth:</b> {props.birth}</p>
+        <p><b>Birth:</b> {props.birth.toDateString()}</p>
       </div>
     </div>
   )
