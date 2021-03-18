a31067407d280314603179a26ff091cfd6eb526c
Ángela Herrador <angelaherrador1991@gmail.com>
2021-03-18 13:32:08 +0100

    It 2 done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import IdCard from './components/IdCard';
-
-// const IdCards = [
-//   {
-//     lastName: "Herrero",
-//     firstName: "Pedro",
-//     gender: "male",
-//     height: 182,
-//     birth: new Date ("1992-07-14"),
-//     picture: "https://randomuser.me/api/portraits/men/44.jpg"
-//   },
-//   {
-//     lastName: "Díaz",
-//     firstName: "Ana",
-//     gender: "feminine",
-//     height: 170,
-//     birth: new Date ("1990-07-14"),
-//     picture: "https://randomuser.me/api/portraits/women/44.jpg"
-//   }
-// ]
+import Greetings from './components/Greetings';
 
 function App() {
   return (
     <div className="App">
-          <IdCard
-            lastName='Doe'
-            firstName='John'
-            gender='male'
-            height='178'
-            birth={new Date("1992-07-14")}
-            picture="https://randomuser.me/api/portraits/men/44.jpg"
-          />
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height='178'
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
 
-          <IdCard
-            lastName='Delores '
-            firstName='Obrien'
-            gender='female'
-            height='172'
-            birth={new Date("1988-05-11")}
-            picture="https://randomuser.me/api/portraits/women/44.jpg"
-          />
+      <IdCard
+        lastName='Delores '
+        firstName='Obrien'
+        gender='female'
+        height='172'
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+      <Greetings lang="es">Paco</Greetings>
+      <Greetings lang="en">Tom</Greetings>
+      
     </div>
   );
 }
---
+++ src/components/Greetings.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../assets/css/Greetings.css'
+
+const Greetings = ({lang, children}) => {
+    let greetings = ""
+     
+    if (lang == "de") {
+        greetings = "Hallo"
+    } else if (lang == "en") {
+        greetings = "Hello"
+    } else if (lang == "es") {
+        greetings= "Hola"
+    } else if (lang == "fr") {
+        greetings = "Bonjour"
+    }
+
+    return (
+        <div className="Greetings">
+            {greetings} {children}
+        </div>
+    )
+}
+
+export default Greetings
