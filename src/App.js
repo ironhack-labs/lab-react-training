cf0a449f64e967178701a9326cea785365c4c3b5
Alicia Parra <alicia@redparra.com>
2021-03-18 19:51:32 +0100

    Do iteration 4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import IdCard from './components/IdCard'
 import Greetings from './components/Greetings'
 import Random from './components/Random'
+import BoxColor from './components/BoxColor'
 
 function App() {
   return (
@@ -36,8 +37,9 @@ function App() {
       <Random min={1} max={100}/>
 
       <h1>Iteration 4</h1>
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
 
-      
     </div>
   );
 }
