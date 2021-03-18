b436b1fbb1f56a58165457e9dc0c3b061d6ef9c4
rgarsan <rgarsan84@gmail.com>
2021-03-18 11:56:10 +0100

    iteration-1

---
+++ src/components/IdCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './assets/css/IdCard.css'
+
+
+
+
+const IdCard =({lastName,firstName,gender,height,birth,picture})=>{
+   
+    return(
+      <div className="IdCard">
+            <img src={ picture }></img>
+            <div className="Data">
+                <p><strong>First name</strong>: { firstName }</p>
+                <p><strong>Last name</strong>: { lastName }</p>
+                <p><strong>Gender</strong>: { gender }</p>
+                <p><strong>Height</strong>: { height }</p>
+                <p><strong>Birth</strong>: { birth }</p>
+            </div>
+        </div>
+    )
+}
+
+export default IdCard
