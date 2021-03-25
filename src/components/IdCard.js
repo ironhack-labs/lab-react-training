1ed1fc2086ee9c2dbd00dc8476bfb9886916d9b1
Pedro Frias <plfriasf@gmail.com>
2021-03-25 13:06:51 -0700

    Iteration 1 | Component: IdCard done

---
+++ src/components/IdCard.js
@@ -0,0 +1,42 @@
+import '../assets/css/IdCard.css';
+import moment from 'moment';
+
+const IdCard = ({ lastName, firstName, gender, height, birth, picture }) => {
+  return (
+    <div className="IdCard">
+      <div className="card mb-3" style={{ 'max-width': '800px' }}>
+        <div className="row g-0">
+          <div className="col-md-4 d-flex p-1">
+            <img className="img-fluid" src={picture} alt={picture} />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body text-start">
+              <p>
+                <b>First Name: </b>
+                {firstName}
+              </p>
+              <p>
+                <b>Last Name: </b>
+                {lastName}
+              </p>
+              <p>
+                <b>Gender: </b>
+                {gender}
+              </p>
+              <p>
+                <b>Height: </b>
+                {height}
+              </p>
+              <p>
+                <b>Birth: </b>
+                {moment(birth).format('dddd, MMMM Do YYYY')}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdCard;
