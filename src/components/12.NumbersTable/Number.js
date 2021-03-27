62de9af02c71ca781f69b50a144861f328422cb0
Ines de Palma <ipalmamasaveu@gmail.com>
2021-03-27 18:10:44 +0100

    It12

---
+++ src/components/12.NumbersTable/Number.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Numbers.scss'
+
+const Number = ({i}) => {
+    const styling = (i) => {
+        if (i % 2 === 0) {
+            return '#f542dd'
+        } else {
+            return '#ffffff'
+        }
+    }
+    return (
+        <div className="Number" style={{backgroundColor: styling(i)}}>
+            {i}
+        </div>
+    )
+}
+
+export default Number
--- src/components/12.NumbersTable/NumbersTable.js
+++ src/components/12.NumbersTable/NumbersTable.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Number from './Number'
 import './Numbers.scss'
 
 const NumbersTable = ({ limit }) => {
+    
+    const iteration = (l) => {
+        const nums = []
+        for (let i = 1; i<= l; i ++) {
+           nums.push(i)
+        }
+        return nums.map((n) => <Number i={n}/>)
+    }
     return (
         <div className="NumbersTable">
-            
+            {iteration(limit)}
         </div>
     )
 }
