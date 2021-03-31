638899fe65931d4dd116356b4a67be6f65529a44
joselrdg <josesietepicos@gmail.com>
2021-03-31 21:57:34 +0200

    iteration15

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../assets/css/Greetings.css'
 
-
 const Greetings = ({ lang, children }) => {
     const language = () => {
         switch (lang) {
--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import '../assets/css/SignupPage.css'
+import Greetings from './Greetings'
+
 export default class SignupPage extends Component {
     state = {
         email: 'Email',
         password: 'Password',
-        nationality: 'Nationality'
+        nationality: 'Nationality',
+        emailColor: null,
+        passwordColor: false
     }
 
     handleSubmit = () => {
@@ -12,30 +16,53 @@ export default class SignupPage extends Component {
     }
 
     handleChange = (e) => {
+        let eColor = false
+        let pColor = false
+        if (/^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i
+            .test(this.state.email)) {
+            eColor = true;
+        }
+        if (/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(this.state.password)) {
+            pColor = true;
+        } else {
+            console.log("La dirección de email es incorrecta.");
+        }
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
+            emailColor: eColor,
+            passwordColor: pColor
         })
     }
 
-    // (?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
 
     render() {
         return (
             <div className='SignupPage'>
                 <form className='SignupPageForm' onSubmit={this.handleSubmit}>
-                    <label htmlFor="email">Email</label><br />
-                    <input type="email" onChange={this.handleChange} name="email" value={this.state.email} /><br />
-                    <label htmlFor="password">Password</label><br />
-                    <input type="password" onChange={this.handleChange} name="password" value={this.state.password} /><br />
+                    <label htmlFor="email" style={{ color: this.state.emailColor ? 'green' : 'red' }}>Email</label><br />
+                    <input type="email" style={{
+                        borderColor: this.state.emailColor ? 'green' : 'red',
+                        color: this.state.emailColor ? 'green' : 'red'
+                    }} onChange={this.handleChange} name="email" value={this.state.email} />
+                    <br />
+                    <i style={{ color: this.state.emailColor ? 'green' : 'red' }}>
+                        {this.state.emailColor ? ' You typed a valid email' : 'The email address entered is incorrect'}
+                    </i><br />
+                    <label htmlFor="password" style={{ color: this.state.passwordColor ? 'green' : 'red' }}>Password</label><br />
+                    <input type="password" style={{
+                        borderColor: this.state.passwordColor ? 'green' : 'red',
+                        color: this.state.passwordColor ? 'green' : 'red'
+                    }} onChange={this.handleChange} name="password" value={this.state.password} /><br />
                     <label htmlFor="nationality">Nationality</label><br />
                     <select name="nationality" onChange={this.handleChange} value={this.state.nationality}>
-                        <option value="Germany">Germany</option>
-                        <option value="France">France</option>
+                        <option value="de">Germany</option>
+                        <option value="fr">France</option>
                         <option value="Scotland">Scotland</option>
                         <option value="Italy">Italy</option>
                     </select><br />
                     <button type="submit">Submit</button>
                 </form>
+                <Greetings lang={this.state.nationality}>Your email address is: {this.state.emailColor && this.state.email}</Greetings>
             </div>
         )
     }
