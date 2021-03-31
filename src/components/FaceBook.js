e64d5ae464a2772fd260a96b2e8b8cb7c1428bf8
Laura <pedroscampoy@gmail.com>
2021-03-31 12:13:02 +0200

    start facebook header

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react';
-import profiles from './data/berlin.json';
+import profiles from '../data/berlin.json';
 
 class Facebook extends Component {
   state = {
     profiles: [...profiles],
-    city: '',
+    country: '',
   };
 
-  onClick = () => {
-    this.setState({ toggle: !this.state.toggle });
+  onClickHandler = (country) => {
+    this.setState({ country });
   };
 
   render() {
+    const allCountries = profiles.map((profile) => profile.country);
+
+    const uniqueCountries = new Set(allCountries);
+
+    const displayedCountries = ['All', ...uniqueCountries].map((country) => {
+      const divStyle = {
+        border: '1px solid black',
+        backgroundColor: this.state.country === country ? 'cyan' : 'white',
+        width: 'auto',
+        height: '50px',
+        display: 'inline-block',
+        textAlign: 'center',
+      };
+      return (
+        <div
+          className="country-element"
+          style={divStyle}
+          key={country}
+          onClick={() => this.onClickHandler(country)}
+        >
+          {country}
+        </div>
+      );
+    });
+
     const imgPath = this.state.toggle ? this.props.imgClicked : this.props.img;
 
     return (
       <div className="Facebook">
-        <img
-          style={{ maxWidth: '100px' }}
-          src={imgPath}
-          alt="Clickable pic"
-          onClick={this.onClick}
-        />
+        <div className="CitiesList">{displayedCountries}</div>
+        <div className="CitiesList">{displayedCountries}</div>
       </div>
     );
   }
