e34505f89f55d5060ee7ee718f6ba42c73f7eec3
Laura <pedroscampoy@gmail.com>
2021-03-31 20:20:46 +0200

    Iteration 15

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -14,6 +14,8 @@ const SignupPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [nationality, setNationality] = useState('German');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const [collapse, setCollapse] = useState(false);
 
   const dictionary = {
@@ -22,13 +24,35 @@ const SignupPage = () => {
     German: 'Hallo',
   };
 
+  const emailValidate = emailError ? 'valid' : 'invalid';
+  const emailValidateMessage = emailError
+    ? 'You typed a valid email'
+    : 'You typed an invalid email';
+
+  const passwordValidate = passwordError ? 'valid' : 'invalid';
+  const passwordValidateMessage = passwordError
+    ? 'You typed a valid password'
+    : 'Your password is too weak';
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+  const validatePassword = (password) => {
+    const mediumRegex = new RegExp(
+      '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'
+    );
+    return mediumRegex.test(password);
+  };
+
   const handleEmail = (e) => {
     setEmail(e.target.value);
-    console.log(email);
+    setEmailError(validateEmail(e.target.value));
   };
 
   const handlePassword = (e) => {
     setPassword(e.target.value);
+    setPasswordError(validatePassword(e.target.value));
   };
 
   const handleNationality = (e) => {
@@ -51,9 +75,12 @@ const SignupPage = () => {
             id="email"
             value={email}
             onChange={handleEmail}
-            valid
+            invalid={!emailError}
+            valid={emailError}
           />
-          <FormFeedback valid>You typed a valid email</FormFeedback>
+          <FormFeedback invalid={!emailError} valid={emailError}>
+            {emailValidateMessage}
+          </FormFeedback>
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
@@ -63,9 +90,12 @@ const SignupPage = () => {
             id="password"
             value={password}
             onChange={handlePassword}
-            invalid
+            invalid={!passwordError}
+            valid={passwordError}
           />
-          <FormFeedback>Your password is too weak</FormFeedback>
+          <FormFeedback invalid={!passwordError} valid={passwordError}>
+            {passwordValidateMessage}
+          </FormFeedback>
         </FormGroup>
         <FormGroup>
           <Label for="nationality">Nationality</Label>
