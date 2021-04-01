a7349273bfad3ab92b7b08ee86f55c6d7a16e8e9
karuvsentor <karuvsentor@gmail.com>
2021-04-01 22:33:12 +0200

    It 11 done

--- src/App.js
+++ src/App.js
@@ -135,12 +135,12 @@ function App() {
 
       <h1 className="title"><strong>Carousel</strong></h1>
 
-      < Carousel
+      <Carousel
         imgs={[
           'https://randomuser.me/api/portraits/women/1.jpg',
           'https://randomuser.me/api/portraits/men/1.jpg',
           'https: / /randomuser.me/api/portraits/women/2.jpg ',
-          ' https://randomuser.me/api/portraits/men/2.jpg '
+          'https://randomuser.me/api/portraits/men/2.jpg'
         ]}
       />
 
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,15 +1,26 @@
-import React, {Component} from 'react'  
+import React, { Component } from 'react'
 import '../assets/css/Carousel.css'
 
 class Carousel extends Component {
     state = {
+        counter: 0
+    }
 
+    left = () => {
+        this.setState({ counter: (this.state.counter - 1 + 4) % 4 })
+    }
+    right = () => {
+        this.setState({ counter: (this.state.counter + 1) % 4 })
     }
 
 
-    render(){
+    render() {
         return (
-            <div className="carousel"></div>
+            <div className="carousel">
+                <button onClick={this.left}>Left</button>
+                <img src={this.props.imgs[this.state.counter]} alt="" />
+                <button onClick={this.right}>Right</button>
+            </div>
         )
     }
 }
