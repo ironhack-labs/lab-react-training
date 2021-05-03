c51c3bf32454e6faad52f154a0155f4e08954b26
alescofet <alex.escofet96@gmail.com>
2021-05-03 18:12:06 +0200

    Iteration 7 done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Greetings from './components/Greetings.js'
 import Random from './components/Random.js'
 import BoxColor from './components/BoxColor.js'
 import Rating from './components/Rating.js'
-
+import DriverCard from './components/DriverCard.js'
 
 function App() {
   return (
@@ -46,6 +46,23 @@ function App() {
         <Rating>3</Rating>
         <Rating>4</Rating>
         <Rating>5</Rating>
+
+        <DriverCard
+          name="Travis Kalanick"
+          rating={4.2}
+          img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+          car={{
+            model: "Toyota Corolla Altis",
+            licensePlate: "CO42DE"
+          }} />
+        <DriverCard
+          name="Dara Khosrowshahi"
+          rating={4.9}
+          img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+          car={{
+            model: "Audi A3",
+            licensePlate: "BE33ER"
+          }} />
 
 
     </div>
--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 class BoxColor extends React.Component {
     render(){
+        const rgbToHex = function (rgb) {
+          let hex = Number(rgb).toString(16);
+          if (hex.length < 2) {
+            hex = '0' + hex;
+          }
+          return hex;
+        };
         const {r,g,b} = this.props
         const divStyle = {
         backgroundColor: `rgb(${r},${g},${b})`,
         width:`100vw`}
         return (
-            <div style={divStyle}>{`rgb(${r},${g},${b})`}</div>
+            <div style={divStyle}> <p>{`rgb(${r},${g},${b})`}</p><p>{rgbToHex(r)+rgbToHex(g)+rgbToHex(b)}</p></div>
       );
     }
   }
