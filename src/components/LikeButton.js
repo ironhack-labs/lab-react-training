0d9994b9b258415b58dd2a9c57e1a6bfd0a8d1e1
alescofet <alex.escofet96@gmail.com>
2021-05-04 17:18:10 +0200

    Bonus done

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
 class LikeButton extends React.Component {
+  state = {
+    name: `Alex`,
+    counter: 0,
+    colorCounter: 0,
+    Color: ['purple', 'blue', 'green', 'yellow', 'orange', 'red']
+  };
 
-    state = {
-        name: `Alex`,
-        counter: 0
-    }
+  colorCounterUp() {
+    this.setState(this.state.colorCounter < 5 ? {colorCounter: this.state.colorCounter + 1 , counter: this.state.counter + 1 } : {colorCounter: 0, counter: this.state.counter + 1 })
+  }
 
-   
   render() {
+    const LikeBtnDiv = styled.button`
+      background-color: ${this.state.Color[this.state.colorCounter]};
+    `;
+
     return (
-      <div className="LikeButton">
-        <button onClick={()=>this.setState({counter: (this.state.counter+1)})}>{this.state.counter} Likes</button>
+      <div>
+        <LikeBtnDiv
+          onClick={
+            (() => this.colorCounterUp())
+          }
+        >
+          {this.state.counter} Likes
+        </LikeBtnDiv>
       </div>
     );
   }
