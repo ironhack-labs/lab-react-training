1109222c6d43885ac952281013586fb3eea1436d
Marcia Faria <mfndnc@gmail.com>
2021-05-04 17:57:21 +0200

    facebook round 2

--- src/component/FaceBook.js
+++ src/component/FaceBook.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import profiles from '../data/berlin.json';
+const countries = [...new Set(profiles.map((item) => item.country))];
 
-const App = (props) => {
-  let list = profiles.map((profile, i) => {
+class App extends React.Component {
+  createList = (profile, i) => {
     const classFix = 'IdCard box ';
     const classtext = classFix + (i % 2 ? 'boxblue' : '');
 
@@ -18,8 +19,57 @@ const App = (props) => {
         </div>
       </div>
     );
-  });
-  return <div className="faceBook">{list}</div>;
-};
+  };
+  createButton = (country, i) => (
+    <button onClick={() => this.clickHandler(country)} key={'faceclick' + i}>
+      {country}
+    </button>
+  );
+
+  state = {
+    list: profiles.map(this.createList),
+    buttons: countries.map(this.createButton),
+  };
+
+  clickHandler = (info) => {
+    this.setState((state, props) => {
+      if (typeof info === 'string') {
+        return {
+          list: profiles
+            .filter((profile) => profile.country === info)
+            .map(this.createList),
+          buttons: countries.map((country, i) => (
+            <button
+              onClick={() => this.clickHandler(country)}
+              key={'faceclick' + i}
+              className={info === country ? 'butactive' : ''}
+            >
+              {country}
+            </button>
+          )),
+        };
+      } else {
+        return {
+          list: profiles.map(this.createList),
+          buttons: countries.map(this.createButton),
+        };
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="faceBookAdvanced">
+        <div className="faceBookButtons">
+          <button key="faceclickall" onClick={this.clickHandler}>
+            ALL
+          </button>
+          {this.state.buttons}
+        </div>
+        <div className="faceBook">{this.state.list}</div>
+      </div>
+    );
+  }
+}
 
 export default App;
