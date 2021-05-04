fac6a613f4b6106dfe1cff784b7018a9ae295678
Marcia Faria <mfndnc@gmail.com>
2021-05-04 16:59:41 +0200

    NumbersTable alternative

--- src/component/NumbersTable.js
+++ src/component/NumbersTable.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const App = (props) => {
-  let list = Array.from({ length: props.limit }, (_, i) => {
+  const list = Array.from({ length: props.limit }, (_, i) => {
+    /*
     const divStyle = {
       backgroundColor: i % 2 ? 'red' : 'white',
     };
+    */
+    // style={divStyle}
+    const cssStyle = i % 2 ? 'boxred' : 'boxwhite';
     return (
-      <li key={'li' + i} style={divStyle}>
+      <li key={'li' + i} className={cssStyle}>
         {i + 1}
       </li>
     );
