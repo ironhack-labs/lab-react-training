0c06df942231c596e4dcbc68bb44c7691910f0cc
Zoe Zeitler <zlz97@hotmail.com>
2021-05-04 11:28:08 +0200

    4 May 11:28

--- src/App.js
+++ src/App.js
@@ -25,3 +25,43 @@ function App() {
 
 export default App;
 
+
+function IdCard(lastName, firstName, gender, height, birth, picture) {
+  return (
+  <img src=picture/>
+  <div>
+    First name: {firstName}
+    Last name: {lastName}
+    Gender: {gender}
+    Height: {height}
+    Birth: {birth}
+  </div>; )
+
+}
+
+
+function Greetings(lang, children) {
+  if (lang == "de") {
+    return <div>Hallo {children}</div>
+  }
+
+  if (lang == "en") {
+    return <div>Hello {children}</div>
+  }
+
+  if (lang == "es") {
+    return <div>Hola {children}</div>
+  }
+
+  if (lang == "fr") {
+    return <div>Salut {children}</div>
+  }
+
+}
+
+
+function Random(min, max) {
+  let value = Math.floor(Math.random() * max)
+
+  return <div>Random value between {min} and {max} =>  </div>
+}
