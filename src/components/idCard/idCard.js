0540f7b87462b396c3d23602fcfcc2048fd784be
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-05-05 20:42:23 -0300

    add iteration 1

---
+++ src/components/idCard/idCard.js
@@ -0,0 +1,24 @@
+import './idCard.css'
+
+const IdCard = (props) => {
+    const getHeigth = height => height / 100 + 'm';
+
+    const getBirthDate = birthDate => birthDate.toDateString();
+
+    return (
+        <div className="card">
+            <div className="profile-pic">
+                <img src={props.picture} alt={props.firstName + ' profile picture'}/>
+            </div>
+            <div className="content">
+                <p><b>First name</b>: {props.firstName}</p>
+                <p><b>Last name</b>: {props.lastName}</p>
+                <p><b>Gender</b>: {props.gender}</p>
+                <p><b>Height</b>: {getHeigth(props.height)}</p>
+                <p><b>Birth</b>: {getBirthDate(props.birth)}</p>
+            </div>
+        </div>
+    )
+}
+
+export default IdCard
