aadca6a20903f1bfd817482e2eaab653abad487a
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-05-11 22:36:31 +0200

    9th done

--- src/components/ClickablePicture.js
+++ src/components/ClickablePicture.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import '../App.css';
 
-function ClickablePicture() {
-  return <div></div>;
+function ClickablePicture(props) {
+  const { img, imgClicked } = props;
+  const [image, setImage] = React.useState({ img });
+
+  function changeTheImage() {
+    let currentImage;
+    image === img ? (currentImage = imgClicked) : (currentImage = img);
+    setImage(currentImage);
+    console.log(currentImage);
+  }
+
+  return (
+    <div>
+      <img src={image} alt="Some guy" onClick={changeTheImage}></img>
+    </div>
+  );
 }
 
 export default ClickablePicture;
