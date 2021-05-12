7f591e754c8ca4a9755ccf5be5f3541411dbbaf2
Aleksandra Porada <aleksandra.porada92@gmail.com>
2021-05-12 21:04:28 +0200

    Part of uteration 12th done – stuck with the colors changing

--- src/components/NumbersTable.js
+++ src/components/NumbersTable.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import '../App.css';
 
-function NumbersTable() {
-  return <div></div>;
+function NumbersTable(props) {
+  const { limit } = props;
+
+  let arrOfNumbers = [];
+  for (let i = 1; i <= limit; i++) {
+    arrOfNumbers.push(i);
+  }
+
+  let color = 'green';
+
+  const style = {
+    backgroundColor: color,
+    borderColor: 'black',
+    borderStyle: 'solid',
+    height: 50,
+    width: 50,
+    fontSize: 20,
+    padding: 0,
+    margin: 0,
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {arrOfNumbers.map((number) => {
+              return <th style={style}>{number}</th>;
+            })}
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 }
 
 export default NumbersTable;
