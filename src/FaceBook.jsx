a4d84985e00619d62f1951959f219ed18b42e19d
Tomsylva <tomsylvanz@gmail.com>
2021-05-14 16:11:13 +0200

    iteration 14 complete

--- src/FaceBook.jsx
+++ src/FaceBook.jsx
@@ -3,21 +3,43 @@ import './FaceBook.css';
 import profiles from './data/berlin.json';
 
 function FaceBook() {
+  const [countrySelect, setCountrySelect] = React.useState('null');
   let countryArr = [];
+
+  function selectCountry(event) {
+    setCountrySelect(event.target.value);
+  }
+
   profiles.map((profile) => {
     if (countryArr.indexOf(profile.country) === -1) {
       countryArr.push(profile.country);
     }
   });
+
   return (
     <div>
       {countryArr.map((el) => {
-        return <button className="FaceBook-button">{el}</button>;
+        return (
+          <button
+            className="FaceBook-button"
+            value={el}
+            onClick={selectCountry}
+          >
+            {el}
+          </button>
+        );
       })}
       {profiles.map((person) => {
         const { firstName, lastName, img, isStudent, country } = person;
         return (
-          <div className="FaceBook">
+          <div
+            className="FaceBook"
+            style={
+              countrySelect === country
+                ? { backgroundColor: 'lightBlue' }
+                : { backgroundColor: 'white' }
+            }
+          >
             <img src={img} alt={firstName} />
             <div className="FaceBook-text">
               <p>
