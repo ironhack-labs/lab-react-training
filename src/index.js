11112fe1d611c8884d3b368f68bd3d7e973b0ea9
Mauricio Munoz <mauripedroza89@gmail.com>
2021-06-19 02:28:37 -0500

    almost done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import UIkit from 'uikit';
+import "uikit/dist/css/uikit.min.css";
+import Icons from 'uikit/dist/js/uikit-icons';
+
+UIkit.use(Icons);
+
 
 ReactDOM.render(
   <React.StrictMode>
