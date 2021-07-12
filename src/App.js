0e27c7c23c6e13b2f9743551a35c990d26808ec6
Irina <ifalko@protonmail.com>
2021-07-12 17:26:51 +0200

    First iteration done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person
+        lastName="Doe"
+        firstName="John"
+        gender="male"
+        height={1.78}
+        birth={new Date('1992-07-14')}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <Person
+        lastName="Delores "
+        firstName="Obrien"
+        gender="female"
+        height={1.72}
+        birth={new Date('1988-05-11')}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
     </div>
   );
 }
