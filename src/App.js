558a8c67631de52e40929fb852ffab76ff9350e8
doris hutter <doris.hutter@gmx.de>
2021-07-13 10:47:39 +0200

    first Iteration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard';
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+      <h1>IdCard</h1>
+    
+      <IdCard 
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+        firstName="John"
+        lastName="Doe"
+        gender="male"
+        height={1.78}
+        birth={`${new Date("1992-07-14")}`}
+      />
+      <IdCard 
+        picture="https://randomuser.me/api/portraits/women/8.jpg"
+        firstName="Obrien"
+        lastName="Delores"
+        gender="female"
+        height={1.72}
+        birth={`${new Date("1988-05-11")}`} 
+      />
+</div>
   );
+    
 }
 
 export default App;
---
+++ src/components/IdCard.js
@@ -0,0 +1,52 @@
+/* function IdCard ({firstName, lastName, gender, height, birth, picture}) {
+    return (
+        <div className="IdCard box">
+            <div>
+                <img  src={picture} alt={firstName}/>
+            </div>
+            <div>
+                <p>
+                    <span>First name:</span> {firstName}
+                    <br/>
+                    <span>Last name:</span> {lastName} 
+                    <br/>
+                    <span>Gender:</span> {gender}
+                    <br/>
+                    <span>Height:</span> {height}m
+                    <br/>
+                    <span>Birth:</span> {birth}
+                    <br/>
+                </p>
+            </div>
+        </div> 
+
+
+    )
+}
+ */
+
+function IdCard (props){
+    return(
+        
+        <div className="card bio-card">
+
+                <div>
+                    <img  src={props.picture} alt={props.firstName}/>
+                </div>
+
+                <div>
+                    <span>First name:</span> {props.firstName}
+                    <br/>
+                    <span>Last name:</span> {props.lastName} 
+                    <br/>
+                    <span>Gender:</span> {props.gender}
+                    <br/>
+                    <span>Height:</span> {props.height}m
+                    <br/>
+                    <span>Birth:</span> {props.birth}
+                    <br/>
+                </div>
+        </div>
+    )
+}
+export default IdCard
