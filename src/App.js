95ddc64cf10feb9d8049dd804161da6f5821ad22
Flavio-Pestana <flaviopestan@gmail.com>
2021-07-17 22:16:46 -0300

    iteration5

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import IdCard from './components/IdCard.js';
 import Greetings from './components/Greetings.js';
 import Random from './components/Random.js';
 import BoxColor from './components/BoxColor.js'
+import CreditCard from './components/CreditCard.js';
 import { directive } from '@babel/types';
 
 function App() {
@@ -45,6 +46,39 @@ function App() {
         <h1>BoxColor</h1>
         <BoxColor r={255} g={0} b={0} />
         <BoxColor r={128} g={255} b={0} />
+      </div>
+
+      <div className='credit-card-div'>
+        <h1>CreditCard</h1>
+        <div className='credit-cards'>
+          <CreditCard
+            type="Visa"
+            number="0123456789018845"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="BNP"
+            owner="Maxence Bouret"
+            bgColor="#11aa99"
+            color="white" />
+          <CreditCard
+            type="Master Card"
+            number="0123456789010995"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="N26"
+            owner="Maxence Bouret"
+            bgColor="#eeeeee"
+            color="#222222" />
+          <CreditCard
+            type="Visa"
+            number="0123456789016984"
+            expirationMonth={12}
+            expirationYear={2019}
+            bank="Name of the Bank"
+            owner="Firstname Lastname"
+            bgColor="#ddbb55"
+            color="white" />
+      </div>
       </div>
 
 
