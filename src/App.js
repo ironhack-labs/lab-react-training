a6ddffaee1db0a2b4268b16119fc0a2e70afcd7e
helenetrommelen <helenetrommelen@gmail.com>
2021-09-02 14:25:58 +0200

    added functionality of iteration 7

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Random from './Random'
 import BoxColor from './BoxColor'
 import CreditCard from './CreditCard'
 import Rating from './Rating'
+import DriverCard from './DriverCard'
 
 
 
@@ -95,6 +96,26 @@ export default class App extends React.Component {
           <Rating>3</Rating>
           <Rating>4</Rating>
           <Rating>5</Rating>
+          <br />
+
+          <h2>Iteration 7: DriverCard</h2><br />
+
+          <DriverCard
+            name="Travis Kalanick"
+            rating={4.2}
+            img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+            car={{
+              model: "Toyota Corolla Altis",
+              licensePlate: "CO42DE"
+            }} />
+          <DriverCard
+            name="Dara Khosrowshahi"
+            rating={4.9}
+            img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+            car={{
+              model: "Audi A3",
+              licensePlate: "BE33ER"
+            }} />
 
 
           <h2>Iteration 8</h2>
--- src/DriverCard.js
+++ src/DriverCard.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import Rating from './Rating'
 
 class DriverCard extends Component {
 
     constructor(props) {
         super(props);
     }
-    
+
     render() {
         return (
             <div>
-                
+               <div><img src={this.props.img} alt="profile picture" /></div>
+               <div>
+                   <p>{this.props.name}</p>
+                   <Rating>1.49</Rating>
+                   <p>{this.props.car.model} - {this.props.car.licensePlate}</p>
+               </div> 
             </div>
         );
     }
