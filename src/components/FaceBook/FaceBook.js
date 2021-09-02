e3e48f2d23d1ec04c1302e6063d66ddd24fe67ae
Marcos Salvo <marcossalvo95@gmail.com>
2021-09-02 12:22:09 +0200

    Iteration 14 done

--- src/components/FaceBook/FaceBook.js
+++ src/components/FaceBook/FaceBook.js
@@ -6,24 +6,49 @@ import { id } from "postcss-selector-parser";
 class FaceBook extends Component {
     constructor (props) {
         super(props)
+
+        this.state = {
+            clickedCountry: ""
+        }
+    }
+
+    onHighlight = (event) => {
+        let countryClicked = event.target.innerHTML
+        this.setState({clickedCountry: countryClicked})
     }
 
     render () {
+        let profileMap = profiles.map(profile => [profile.country, profile])
+        var profileMapArr = new Map(profileMap)
+        let uniques = [...profileMapArr.values()]
+        let countries = uniques.map(profile => profile.country)
+
         return (
-            <div>
-                {profiles.map((profile) => {
-                    return (
-                        <div className="flex">
-                            <img src={profile.img}></img>
-                            <div className="flex-start">
-                                <p><b>FirstName</b>: {profile.firstName}</p>
-                                <p><b>LastName</b>: {profile.lastName}</p>
-                                <p><b>Country</b>: {profile.country}</p>
-                                <p><b>Type</b>: {profile.isStudent === true ? "Student" : "Teacher"}</p>
+            <div className="FaceBook">
+                <div>
+                    {countries.map((country) => {
+                        return (
+                            <button className="btn btn-info" onClick={this.onHighlight} style={{backgroundColor: country==this.state.clickedCountry ? "#caa6f7" : "transparent"}}>
+                                {country}
+                            </button>
+                        )
+                    })}
+                </div>
+                <div className="cards">
+                    {profiles.map((profile) => {
+                        return (
+                            <div key={`${profile.fistName}-${profile.lastName}`} style={{backgroundColor: profile.country==this.state.clickedCountry ? "#caa6f7" : "transparent"}} className="flex">
+                                <img src={profile.img}></img>
+                                <div className="flex-start">
+                                    <p><b>FirstName</b>: {profile.firstName}</p>
+                                    <p><b>LastName</b>: {profile.lastName}</p>
+                                    <p><b>Country</b>: {profile.country}</p>
+                                    <p><b>Type</b>: {profile.isStudent === true ? "Student" : "Teacher"}</p>
+                                </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
         )
     }
