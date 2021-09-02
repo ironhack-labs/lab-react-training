d3ef1ecbea61910ecd38a6a9c2623b8dfc49e5b6
helenetrommelen <helenetrommelen@gmail.com>
2021-09-02 18:38:29 +0200

    fixed functionality of iteration10

--- src/Dice.js
+++ src/Dice.js
@@ -1,35 +1,88 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-class Dice extends Component {
-
-    constructor(props) {
-        super(props);
+export default class Dice extends Component {
+    state = {
+        counter: 0,
     }
 
     rollDice = () => {
-        let diceNr = Math.floor(Math.random()* (6 - 0) + 0);
-        const diceOptions = [
-            "./img/dice1.png",
-            "./img/dice2.png",
-            "./img/dice3.png",
-            "./img/dice4.png",
-            "./img/dice5.png",
-            "./img/dice6.png",];
-        return diceOptions[diceNr];
+        const randomNum = (min, max) => {
+            return Math.round(Math.random() * (max - min) + min);
+        }
+        this.setState((state) => {
+            return {
+                counter: 0
+            }
+        })
+        setTimeout(() => {
+            this.setState((state) => {
+                return {
+                    counter: randomNum(1, 6)
+                }
+            })
+        }, 1000)
     }
-
+    diceImg = {
+        0: '/img/dice-empty.png',
+        1: '/img/dice1.png',
+        2: '/img/dice2.png',
+        3: '/img/dice3.png',
+        4: '/img/dice4.png',
+        5: '/img/dice5.png',
+        6: '/img/dice6.png',
+    }
     render() {
         return (
             <div class="dice">
-                <p>{this.rollDice()}</p>
-                <img src={this.rollDice()} alt="dice image" class="diceImg"/>
-                
+            <img onClick={this.rollDice} src={this.diceImg[this.state.counter]} alt="dice" class="diceImg"/>
             </div>
-        );
+        )
     }
 }
 
-export default Dice;
+// import React, { Component } from 'react';
+
+// class Dice extends Component {
+//     state = {
+//         diceNr: 1
+//     }
+
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     clickHandler = (props) => {
+//         this.setState((state, props) => ({
+//             diceNr: state.diceNr * this.rollDice()
+//         }))
+//     }
+
+//     rollDice = () => {
+//         let diceNr = Math.floor(Math.random()* (6 - 0) + 0);
+//         return diceNr;
+//     }
+
+//     render() {
+
+//         const diceOptions = [
+//             "./img/dice1.png",
+//             "./img/dice2.png",
+//             "./img/dice3.png",
+//             "./img/dice4.png",
+//             "./img/dice5.png",
+//             "./img/dice6.png",];
+
+
+//         return (
+//             <div class="dice">
+//                 <img src={diceOptions[this.state.diceNr]} alt="dice image" class="diceImg" onClick={this.clickHandler()}/>
+                
+//             </div>
+//         );
+//     }
+// }
+
+// export default Dice;
 
 //on click a random number between 1-6 sould be selected
 //
