19fe069788227346862b0a85798621197479ce4d
Paloma Borro <palomabs93@gmail.com>
2021-09-03 14:57:27 +0200

    iteraciones 11 y 14

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import DriverCard from "./components/drivercard/DriverCard";
 import LikeButton from "./components/likebutton/LikeButton";
 import ClickablePicture from "./components/clickablepicture/ClickablePicture";
 import Dice from "./components/dice/Dice";
-// import Carousel from "./components/carousel/Carousel";
+import Carousel from "./components/carousel/Carousel";
 import NumbersTable from "./components/numberstable/NumbersTable";
 import Facebook from "./components/facebook/Facebook";
 
@@ -138,7 +138,7 @@ export default function App() {
         <ClickablePicture {...clickablePicture} />
       <h1>Dice</h1>
         <Dice />
-      {/* <h1>Carousel</h1>
+      <h1>Carousel</h1>
         <Carousel>
           imgs={[
             'https://randomuser.me/api/portraits/women/1.jpg',
@@ -146,7 +146,7 @@ export default function App() {
             'https://randomuser.me/api/portraits/women/2.jpg',
             'https://randomuser.me/api/portraits/men/2.jpg'
           ]}
-        </Carousel> */}
+        </Carousel>
       <h1>Numbers Table</h1>
         <NumbersTable limit={12} />
       <h1>Facebook</h1>
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,36 +1,30 @@
-// import React from 'react';
-// import "./Carousel.css";
+import React from 'react';
+import "./Carousel.css";
 
-// export default class Carousel extends React.Component {
-//       state = {
-//           index: 0,
-//       }
+export default class Carousel extends React.Component {
+      state = {
+          index: 0,
+      }
     
-//       nextImg = () => {
-//         this.setState({
-//             index:
-//                 this.state.index === 0
-//                   ? this.props.imgs.length - 1
-//                   : this.state.index - 1
-//             });
-//       }
+      nextImg = () => {
+                this.state.index === 0
+                  ? this.setState({index: 3})
+                  : this.setState({index: this.state.index - 1})
+      }
 
-//       prevImg = () => {
-//         this.setState({
-//             index:
-//               this.state.index === this.props.imgs.length - 1
-//                 ? 0
-//                 : this.state.index + 1,
-//           });
-//       }
+      prevImg = () => {
+              this.state.index === 3
+                ? this.setState({index: 0})
+                : this.setState({index: this.state.index + 1})
+      }
 
-//       render() {
-//         return (
-//             <div>
-//                 <button onClick={this.prevImg}>Left</button>
-//                 <img src={this.props.imgs(this.state.index)} alt="Carousel imgage" />
-//                 <button onClick={this.nextImg}>Right</button>
-//             </div>
-//         );
-//       }
-// }
+      render() {
+        return (
+            <div className='Carousel'>
+                <button onClick={this.nextImg}>Left</button>
+                <img src={this.props.imgs[this.state.index]} alt="Carousel imgage" />
+                <button onClick={this.prevImg}>Right</button>
+            </div>
+        );
+      }
+}
