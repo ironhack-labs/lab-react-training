c0f3b548013c72b4137e033dd77ea501e06ee9ee
Elizabeth Colastra <elizabethcolastra@hotmail.es>
2021-09-05 23:50:35 +0200

    Iteration 1

---
+++ src/components/IdCard/IdCard.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./IdCard.css"
+
+export default class IdCard extends React.Component{
+    render() {
+        const { lastName, firstName, gender, height, birth, picture } = this.props
+        return (
+            <div class="IdCard">
+
+            </div>
+         
+
+        )
+ }
+}
