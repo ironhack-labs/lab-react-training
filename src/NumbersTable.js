f9e05d89bc2c9eda2c09b9c3f04883e8ab121f3f
helenetrommelen <helenetrommelen@gmail.com>
2021-09-07 19:41:02 +0200

    first commit numberstable

---
+++ src/NumbersTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NumbersTable extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default NumbersTable;
