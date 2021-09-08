99ee785cb6f9da5669510db08d14da13235c9ec9
helenetrommelen <helenetrommelen@gmail.com>
2021-09-08 23:20:56 +0200

    first commit facebook

---
+++ src/Facebook.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Facebook extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Facebook;
