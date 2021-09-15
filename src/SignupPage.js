13449387ba54be1c3d2765fb011ecd70ca4773c5
helenetrommelen <helenetrommelen@gmail.com>
2021-09-15 23:50:43 +0200

    first commit signuppage

---
+++ src/SignupPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignupPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SignupPage;
