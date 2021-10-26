eea1fe7d9cef76816069b877ff8aeccb6fce4a54
Pablo Alonso Lorente <pablochux@me.com>
2021-10-26 19:42:27 +0200

    Adds Iteration 11

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,20 +4,22 @@ function Carousel(props) {
 
     // We create a state for the counter of the array elements (the images)
     // The dispatchAction sets the counter as a number between 0 and the 
-    const [counter, setcounter] = useState(0)
 
-    function rightImage(params) {
+    // Edge case 1 - It doesn't go back when you are in the first photo
+    const [counter, setCounter] = useState(4)
+
+    function rightImage() {
         setCounter((counter + 1) % props.imgs.length)
     }
 
-
-    function leftImage(params) {
+    function leftImage() {
         setCounter((counter - 1) % props.imgs.length)
     }
 
     return (
         <div>
             <img src={props.imgs[counter]} alt="" />
+            <br />
             <button onClick={leftImage}>Left</button>
             <button onClick={rightImage}>Right</button>
         </div>
