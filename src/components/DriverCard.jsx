4072f8b05e58300007636a4690452e73a74111f5
Pablo Alonso Lorente <pablochux@me.com>
2021-10-26 18:45:47 +0200

    Iteration 7 done

--- src/components/DriverCard.jsx
+++ src/components/DriverCard.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Rating from './Rating'
 
 function DriverCard(props) {
+    console.log(props);
     return (
         <div>
-            {/* <Rating>{props.rating}</Rating> */}
+            <img src={props.img} className="driverCard"/>
+            <h3> {props.name} </h3>
+            <Rating>{props.rating}</Rating>
+            <p> {props.car.model} - {props.car.licensePlate} </p>
         </div>
     )
 }
