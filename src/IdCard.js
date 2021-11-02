15dfc9cbeee3d11d0e9386d158b6304d30619377
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-02 18:26:51 +0100

    iteration 1

---
+++ src/IdCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export default function IdCard(props) {
+  return (
+    <section className="recipe">
+      <p>First name: {props.firstName}</p>
+      <p>Last name: {props.firstName}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Height: {props.height}</p>
+      <p>Birth: {props.birth}</p>
+      <img src={props.picture} alt="Member"/>
+    </section>
+  );
+}
+
