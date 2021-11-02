47fa3cf3b4d7b7d8840d3b5578453e38a5fd66fd
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-11-03 00:44:23 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -17,27 +17,43 @@ function IdCard(props) {
   );
 }
 
+function Greetings(props1, props2) {
+  return (
+    <div className="card">
+      <p>{props1.lang} {props2.name}</p>
+    </div>
+    
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <IdCard
-        lastName="Doe"
-        firstName="John"
-        gender="male"
-        height="178"
-        birth='1992-07-14'
-        picture="https://randomuser.me/api/portraits/men/44.jpg"
-      />
+      <section className="Iteration 1">
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height="178"
+          birth='1992-07-14'
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
 
-      <IdCard
-        lastName="Delores "
-        firstName="Obrien"
-        gender="female"
-        height="172"
-        birth='1988-05-11'
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      />
+        <IdCard
+          lastName="Delores "
+          firstName="Obrien"
+          gender="female"
+          height="172"
+          birth='1988-05-11'
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </section>
+      <section className="Iteration 2">
+        <Greetings lang="de" name="Ludwig"/>
+        <Greetings lang="fr" name="Francois"/>
+      </section>
     </div>
+    
   );
 }
 
