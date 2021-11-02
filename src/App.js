dc7316077285107869e1fa7dc8f135a1cf0fdb35
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-11-03 00:30:03 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function IdCard(props) {
+  return (
+    <div className="card">
+      <img src={props.picture} alt="user" />
+      <section className="infos">
+        <p><strong>First name:</strong> {props.firstName}</p>
+        <p><strong>Last name:</strong> {props.lastName}</p>
+        <p><strong>Gender:</strong> {props.gender}</p>
+        <p><strong>Height:</strong> {props.height}</p>
+        <p><strong>Birth:</strong> {props.birth}</p>
+      </section>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IdCard
+        lastName="Doe"
+        firstName="John"
+        gender="male"
+        height="178"
+        birth='1992-07-14'
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+
+      <IdCard
+        lastName="Delores "
+        firstName="Obrien"
+        gender="female"
+        height="172"
+        birth='1988-05-11'
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
     </div>
   );
 }
