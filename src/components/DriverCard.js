782168103f7ff8efecd313d1d669f1271cc26102
Dominika Lipka <dominika.lipka96@gmail.com>
2021-11-02 20:01:32 +0100

    Improved iteration 7 - rating displays as stars

--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Rating from './Rating';
 
 function DriverCard(props) {
     return (
-      <div className='driver-card-container'>
-        <div className='driver-card-image'>
+      <div className="driver-card-container">
+        <div className="driver-card-image">
           <img src={props.img} alt="driver" />
         </div>
-        <div className='driver-card-details'>
-            <h4>{props.name}</h4>
-            <h5>Rating: {props.rating.toString()}</h5>
-            <p>{props.car.model} - {props.car.licensePlate}</p>
+        <div className="driver-card-details">
+          <h4>{props.name}</h4>
+          <Rating>{props.rating}</Rating>
+          <p>
+            {props.car.model} - {props.car.licensePlate}
+          </p>
         </div>
       </div>
     );
