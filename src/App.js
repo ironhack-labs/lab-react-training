d3b051c14e0aada8deecb39c9e3f3fa8d36aaf64
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-03 08:13:20 +0100

    App.js file

--- src/App.js
+++ src/App.js
@@ -2,27 +2,67 @@ import React from 'react';
 import './App.css';
 import IdCard from './IdCard';
 import { LikeButton } from './LikeButton';
+import {CreditCard} from './CreditCard'
 
 function App() {
   return (
-    <div className="App">
-      <IdCard
-        lastName='Doe'
-        firstName='John'
-        gender='male'
-        height={178}
-        birth={new Date("1992-07-14")}
-        picture="https://randomuser.me/api/portraits/men/44.jpg"
-      />
+    <div>
+      <div>
+        <h1>Id Card</h1>
+        <IdCard
+          lastName='Doe'
+          firstName='John'
+          gender='male'
+          height={178}
+          birth={new Date("1992-07-14")}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
 
-      <IdCard
-        lastName='Delores '
-        firstName='Obrien'
-        gender='female'
-        height={172}
-        birth={new Date("1988-05-11")}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      />
+        <IdCard
+          lastName='Delores '
+          firstName='Obrien'
+          gender='female'
+          height={172}
+          birth={new Date("1988-05-11")}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+
+      <div className="credit-card">
+        <h1>Credit Card</h1>
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white" 
+         />
+
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222" 
+         />
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white" 
+          />
+
+      </div>
       <br/>
       <LikeButton/>
     </div>
