6741328b21ce2c50187cf466c4d1a6e66b86d784
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-12-01 00:04:38 +0100

    Iteration 5 completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import IdCard from './components/IdCard/IdCard.js';
 import Greetings from './components/Greetings/Greetings';
 import Random from './components/Random/Random';
 import BoxColor from './components/BoxColor/BoxColor';
+import CreditCard from './components/CreditCard/CreditCard';
 
 function App() {
   return (
@@ -35,6 +36,36 @@ function App() {
       <BoxColor r={255} g={0} b={0} />
       <BoxColor r={128} g={255} b={0} />
 
+      <CreditCard
+        type="Visa"
+        number="0123456789018845"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="BNP"
+        owner="Maxence Bouret"
+        bgColor="#11aa99"
+        color="white"
+      />
+      <CreditCard
+        type="Master Card"
+        number="0123456789010995"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="N26"
+        owner="Maxence Bouret"
+        bgColor="#eeeeee"
+        color="#222222"
+      />
+      <CreditCard
+        type="Visa"
+        number="0123456789016984"
+        expirationMonth={12}
+        expirationYear={2019}
+        bank="Name of the Bank"
+        owner="Firstname Lastname"
+        bgColor="#ddbb55"
+        color="white"
+      />
     </div>
   );
 }
---
+++ src/components/CreditCard/CreditCard.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './CreditCard.css';
+import visa from '../../assets/images/visa.png';
+import mastercard from '../../assets/images/master-card.svg';
+
+const CreditCard = (props) => {
+  const {
+    type,
+    number,
+    expirationMonth,
+    expirationYear,
+    bank,
+    owner,
+    bgColor,
+    color,
+  } = props;
+
+  let img = visa;
+
+  if (type === 'Visa') img = visa;
+  else img = mastercard;
+
+  const divStyle = {
+    color: `${color}`,
+    backgroundColor: `${bgColor}`,
+  };
+
+  //No supe como centrar las tarjetas probe con flexbox pero nada
+  //   const creditContainer ={
+  //     display: 'flex',
+  //     contentDirection: 'row',
+  //   }
+
+  function hiddenNumbers(card) {
+    let hideNumbers = [];
+    for (let i = 0; i < card.length; i++) {
+      if (i < card.length - 4) {
+        hideNumbers.push('•');
+      } else {
+        hideNumbers.push(card[i]);
+      }
+    }
+    return hideNumbers.join('');
+  }
+
+  return (
+    // <div style={creditContainer}>
+      <div style={divStyle} className="credit-card">
+        <img className="credit-img" src={img} alt="credit-img" />
+        <p className="credit-numb">{hiddenNumbers(number)}</p>
+        <p className="credit-details">
+          Expires {expirationMonth}/{expirationYear} <span>{bank}</span>
+        </p>
+        <p className="credit-details">{owner}</p>
+      </div>
+    // </div>
+  );
+};
+
+export default CreditCard;
