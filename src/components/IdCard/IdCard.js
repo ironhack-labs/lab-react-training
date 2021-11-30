ca8f852d5c7b2208977caa9cb45dc83163b62aa5
Guillem Ferrer <guillemfd@gmail.com>
2021-11-30 16:03:55 +0100

    Iteration 1 with CSS

--- src/components/IdCard/IdCard.js
+++ src/components/IdCard/IdCard.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import './IdCard.css';
 
 
 const IdCard = (props) => {
   const { lastName, firstName, gender, height, birth, picture} = props
     return (
-        <div className="idCard">
-            <p>Last name: {lastName}</p>
-            <p>Name: {firstName}</p>
-            <p>Gender: {gender}</p>
-            <p>Height: {height}</p>
-            <p>Birth: {birth.toDateString()}</p>
-            <img src={picture} alt={firstName + lastName}></img>
+        <div className="container">
+            <div>
+                <img className="IdPic" src={picture} alt={firstName + lastName}></img>
+            </div>
+            <div className="IdData">
+                <p><span>Last name: </span>{lastName}</p>
+                <p><span>Name: </span>{firstName}</p>
+                <p><span>Gender: </span>{gender}</p>
+                <p><span>Height: </span>{height}</p>
+                <p><span>Birth: </span>{birth.toDateString()}</p>
+            </div>
         </div>
     )
 }
