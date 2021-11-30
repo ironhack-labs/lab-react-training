a96e50f41d399e4f3fd68a08d5ce8a5660280aa4
Fede <f.gomezlara@outlook.es>
2021-11-30 15:49:07 +0100

    iteration 3

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,5 +1,14 @@
 const Random = (props) => {
+    const { min, max } = props
+    const randomValue = Math.floor(Math.random() * max) + min
 
+    return (
+        <div className="randomNumber">
+            <p>
+                Random value between {min} and {max} => {randomValue}
+            </p>
+        </div>
+    )
 }
 
 export default Random
