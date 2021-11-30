fdf9686a2c791c7ebf12a1a448994cc32f24cd8e
kostalexis89 <kostalexis.hosting@gmail.com>
2021-11-30 13:53:50 +0100

    Iteration 9 completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LikeButton from './components/LikeButton';
+import ClickablePicture from './components/ClickablePicture';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <LikeButton />
+        <ClickablePicture />
       </header>
     </div>
   );
---
+++ src/components/ClickablePicture.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {useState} from 'react'
+import imageOne from '../assets/images/maxence.png'
+import imageTwo from '../assets/images/maxence-glasses.png'
+
+
+export default function ClickablePicture() {
+    const setInitialValue = () => {
+        return imageOne
+    }
+    const [image, setImage] = useState(() => setInitialValue())
+    const imageClick = () => {
+        console.log('clicked image')
+        console.log(image)
+        if(image===imageOne){
+            setImage((image) => imageTwo)
+        } else {
+            setImage((image) => imageOne)
+        }
+        
+    }
+    return (
+        <div>
+        <h2>Iteration 9</h2>
+            <img src={image} style={{height: '200px'}} onClick={imageClick}/>
+        </div>
+    )
+}
