fa6229ffb9954bb4e42f52cc8693b7a88d581c6e
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-30 16:45:50 +0100

    Iteration 5

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import IdCard from './IdCard'
 import Greetings from './Greetings';
 import Random from './Random';
 import BoxColor from './BoxColor';
+import CreditCard from './CreditCard';
 import './App.css';
+
 
 function App() {
   return (
@@ -42,6 +44,38 @@ function App() {
         <h1>BoxColor</h1>
         <BoxColor r={255} g={0} b={0} />
         <BoxColor r={128} g={255} b={0} />
+      </div>
+      <div>
+        <h1>CreditCard</h1>
+        <div className="credit-cards">
+          <CreditCard
+            type="Visa"
+            number="0123456789018845"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="BNP"
+            owner="Maxence Bouret"
+            bgColor="#11aa99"
+            color="white" />
+          <CreditCard
+            type="Master Card"
+            number="0123456789010995"
+            expirationMonth={3}
+            expirationYear={2021}
+            bank="N26"
+            owner="Maxence Bouret"
+            bgColor="#eeeeee"
+            color="#222222" />
+          <CreditCard
+            type="Visa"
+            number="0123456789016984"
+            expirationMonth={12}
+            expirationYear={2019}
+            bank="Name of the Bank"
+            owner="Firstname Lastname"
+            bgColor="#ddbb55"
+            color="white" />
+        </div>
       </div>
     </div>
   );
