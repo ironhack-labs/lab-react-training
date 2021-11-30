d244b7ed9e9b60f36e6a09958c757ff53e4d9ce8
Tsaikkari <kirsi.trospe@gmail.com>
2021-11-30 13:35:07 +0100

    button bg color

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 
 export const LikeButton = () => {
   const [like, setLike] = useState(0)
+  const colors = ['purple','blue','green','yellow','orange','red']
+  const [backgroundColor, setBackgroundColor] = useState(colors[0])
 
   const handleLike = () => {
     setLike(like => like + 1)
+    const index = colors.indexOf(backgroundColor)
+    setBackgroundColor(backgroundColor => {
+      if (backgroundColor === 'red') {
+        backgroundColor = colors[0]
+      }
+      if (backgroundColor === colors[index]) {
+        backgroundColor = colors[index + 1]
+      }
+      return backgroundColor
+    })
   }
   return (
     <div>
-      <button onClick={handleLike}>{like} Likes</button>
+      <button style={{backgroundColor: `${backgroundColor}`}} onClick={handleLike}>{like} Likes</button>
     </div>
   )
 }
