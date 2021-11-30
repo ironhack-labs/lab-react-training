f8ea5946397add59c5d9d6eb1ce5916388c8cb03
Aarón López <n_aaron5@hotmail.com>
2021-11-30 16:01:42 +0100

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -5,37 +5,42 @@ import './App.css';
 import IdCard from './IdCard';
 import Greetings from './Greetings';
 import Random from './Random';
+import BoxColor from './BoxColor';
 
 function App() {
   return (
     <div className="App">
       <div>
-      <h1>IdCard</h1>
-        <IdCard
-          lastName='Doe'
-          firstName='John'
-          gender='male'
-          height={178}
-          birth={new Date("1992-07-14")}
-          picture="https://randomuser.me/api/portraits/men/44.jpg"
-        />
-        <IdCard
-          lastName='Delores '
-          firstName='Obrien'
-          gender='female'
-          height={172}
-          birth={new Date("1988-05-11")}
-          picture="https://randomuser.me/api/portraits/women/44.jpg"
-        />
+        <h1>IdCard</h1>
+          <IdCard
+            lastName='Doe'
+            firstName='John'
+            gender='male'
+            height={178}
+            birth={new Date("1992-07-14")}
+            picture="https://randomuser.me/api/portraits/men/44.jpg"
+          />
+          <IdCard
+            lastName='Delores '
+            firstName='Obrien'
+            gender='female'
+            height={172}
+            birth={new Date("1988-05-11")}
+            picture="https://randomuser.me/api/portraits/women/44.jpg"
+          />
       </div>
       <div>
-      <h1>Greetings</h1>
-        <Greetings lang="de">Ludwig</Greetings>
-        <Greetings lang="fr">François</Greetings>
+        <h1>Greetings</h1>
+          <Greetings lang="de">Ludwig</Greetings>
+          <Greetings lang="fr">François</Greetings>
       </div>
       <div>
         < Random  min = { 1 }  max = { 6 } / > 
         < Random  min = { 1 }  max = { 100 } / >
+      </div>
+      <div>
+        < BoxColor  r = { 255 }  g = { 0 }  b = { 0 }  / > 
+        < BoxColor  r = { 128 }  g = { 255 }  b = { 0 }  / >  
       </div>
     </div>
   );
---
+++ src/BoxColor.js
@@ -0,0 +1,18 @@
+
+import './BoxColor.css'
+
+const BoxColor = (props) => {
+
+    const { r, g, b } = props
+    const divStyle    = {
+          backgroundColor: `rgb(${r},${g},${b})`
+    }
+
+    return (
+        <div style={divStyle} className="boxColor" >
+            <p>rgb({r},{g},{b})</p>
+        </div>
+    )
+}
+
+export default BoxColor
