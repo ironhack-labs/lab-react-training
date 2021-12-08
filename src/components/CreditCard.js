77efcb5abfca4acee911d94b4301213de66cc286
SinuheJardinez <sinuhe.jh@gmail.com>
2021-12-08 12:30:05 -0600

    resuelto con imagen

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -3,9 +3,22 @@ const CreditCard = ({type,number, expirationMonth, expirationYear, bank, owner, 
     const cardColor ={
         background: `${bgColor}`
     }
+    let logoCard = ''
+        switch(type){
+            case 'Visa':
+                logoCard = 'https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.app/img/visa.png'
+             break;
+             case 'Master Card':
+                logoCard = 'https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.app/img/master-card.svg'
+             break;
+             default:
+                logoCard = 'https://5c6c2a0de07b084ce35373ee--determined-aryabhata-a8a833.netlify.app/img/master-card.svg'
+              break;
+            }
+
     return (
         <div className='CreditCard' style={cardColor}>
-            
+            <img src={logoCard} width='150' alt={type}/>
             <p>{type}</p>
             <p>{number}</p>
             <p>{expirationMonth}</p>
