1bf478fd07cc8fc1952ed2a83e8bfc809708c933
rodrigosamaniego <rodrigosamaniegopiza@gmail.com>
2021-12-08 00:06:43 -0600

    quinta iteración sin estilo

---
+++ src/components/Main/CreditCard.js
@@ -0,0 +1,24 @@
+//importaciones
+import React from "react";
+
+//CreditCard
+export default function CreditCard(props) {
+    
+    return(
+        <>
+        <article>
+            <ul>
+                <li>type {props.type}</li>
+                <li>number{props.number}</li>
+                <li>expirationMonth{props.expirationMonth}</li>
+                <li>expirationYear{props.expirationYear}</li>
+                <li>bank{props.bank}</li>
+                <li>owner{props.owner}</li>
+                <li>bgColor{props.bgColor}</li>
+                <li>color{props.color}</li>
+            </ul>
+        </article>
+        </>
+    )
+    
+}
