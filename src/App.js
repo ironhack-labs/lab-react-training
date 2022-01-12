45dd4efed63e3d4638ec9659aeaad06d1c28e0c6
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-01-12 16:54:16 -0400

    lab part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import IdCard from "./idcard/IdCard";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <IdCard lastName="Doe" firstName="John" gender="male" height={178} birth={new Date("1992-07-15")} />
+    </>
   );
-}
+};
 
 export default App;
