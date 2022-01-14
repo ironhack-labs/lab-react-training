77b0fdfe5e7eb5919667d148f3903b6ecb5573a2
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-01-14 03:36:07 +0100

    Iteration 4 - Done

---
+++ src/components/BoxColor.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const BoxColor = ({ r, g, b }) => {
+
+  function ColorToHex(color) {
+    var hexadecimal = color.toString(16);
+    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+  }
+
+  function ConvertRGBtoHex(r, g, b) {
+    return "#" + ColorToHex(r) + ColorToHex(g) + ColorToHex(b);
+  }
+
+  return (
+    <div className="border words columns" style={{backgroundColor: rgb(r, g, b)}}  >
+      <p className="centered">
+        rgb({r},{g},{b})
+      </p>
+      <p className="centered">
+      {ConvertRGBtoHex(r, g, b)}
+      </p>
+    </div>
+  )
+}
+
+export default BoxColor
