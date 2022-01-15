4989dccabf3d7ee0641b2ff5f53a1833d3ce9840
Lilli Reilly <lilli.reilly@gmail.com>
2022-01-15 13:53:30 +0100

    Iterations 1-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+const IDcard = ({lastName, firstName, gender, height, birth, picture }) => {
+  const birthDate = new Date(birth)
+  return(
+    <div>
+    <div className="idElements">
+    <img src={picture} alt="id"/>
+    <h4>First Name: {firstName}</h4>
+    <h4>Last Name: {lastName}</h4>
+    <h4>Gender: {gender}</h4>
+    <h4>Height: {height}</h4>
+    <h4>Birthday: {birthDate.toDateString()}</h4>
+    </div>
+    </div>
+  )
+}
+const Greetings =({lang, children}) => {
+  let greeting=''
+  if(lang === "de") {
+    greeting="Hallo";
+  } else if(lang === "fr") {
+    greeting="Bonjour"
+  } else if(lang === "en") {
+    greeting="Hello"
+  } else if(lang === "es") {
+    greeting="Hola"
+  }
+  
+  return(
+    <div className="Greetings">
+    <p>{greeting} {children}</p>
+    </div>
+    
+  )
+}
+const RandomNumber =({min, max}) => {
+  let numberRan = Math.floor(Math.random() *max) +min
+  return(
+    <div className="RandomNumber">
+      <p>Random value between {min} and {max} => {numberRan}</p>
+    </div>
+  )
+  
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="iDCard">IDcard</h1>
+      <IDcard
+        lastName="Reilly"
+        firstName="Lilli"
+        gender="female"
+        height="5'6"
+        birth={new Date("1996-09-19")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+      <IDcard
+      lastName="Doe"
+      firstName="John"
+      gender="male"
+      height="1.78m"
+      birth={new Date("1992-07-14")}
+      picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <Greetings lang="de" child="Ludwig">Lugwig</Greetings>
+      <Greetings lang="fr" child="François">François</Greetings>
+      <h1>Random Number</h1>
+      <RandomNumber min={1} max={6}/>
+      <RandomNumber min={1} max={100}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App IDcard/>
   </React.StrictMode>,
   document.getElementById('root')
 );
