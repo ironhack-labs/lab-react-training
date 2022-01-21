d0a0eaa9c8b911c27b4b3b9ae6de1dd7f1e6f720
Sergio <juliwood1980@gmail.com>
2022-01-21 11:05:41 -0300

    iteration 7

--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,12 +1,19 @@
 import React from "react";
 import '../styles/DriverCard.css';
+import Rating  from "./Rating";
 
-const DriverCard = ({name,rating,img,car}) => {
+const DriverCard = (props) => {
+    
     return (
         <div className="driver-card">
-            <span>itens aqui dentro</span>
+                <img src={props.img} alt='driver' className="img-driver"/>
+                <div className="div-text-perfil">
+                    <span>{props.name}</span>
+                    <Rating className='rating '>{props.rating}</Rating>   {/* "★★★☆☆"*/ }
+                    <div className="model-car">{props.car.model} - {props.car.licensePlate}</div>
+                </div>
         </div>
-    )
-}
+    );
+};
 
 export default DriverCard;
