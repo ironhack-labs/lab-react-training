cfb9207a4874faef9040ee147bbd5ff64a39283a
Brigi Horvath <brigi.horvath82@gmail.com>
2022-01-22 12:33:48 +0100

    Iteration 14 - click on img, and search

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -5,28 +5,42 @@ import { useState } from 'react';
 
 const Facebook = () => {
   const [selectedCountry, setSelectedCountry] = useState('');
-  const profilesArr = profiles.map((profile) => {
-    return (
-      <div
-        key={profile.img}
-        className={`${classes.container} ${
-          selectedCountry === profile.country ? classes.blue : ''
-        }`}
-      >
-        <img
-          src={profile.img}
-          alt={profile.name}
-          className={classes.profileImg}
-        />
-        <div className={classes.dataContainer}>
-          <p>First name: {profile.firstName}</p>
-          <p>Last name: {profile.lastName}</p>
-          <p>Country: {profile.country}</p>
-          <p>Type: {profile.isStudent ? 'Student' : 'Teacher'}</p>
+  const [showDetails, setShowDetails] = useState('');
+  const [searchState, setSearchState] = useState('');
+
+  const profilesArr = profiles
+    .filter(
+      (el) =>
+        el.firstName.toLowerCase().includes(searchState) ||
+        el.lastName.toLowerCase().includes(searchState)
+    )
+    .map((profile) => {
+      return (
+        <div
+          key={profile.id}
+          className={`${classes.container} ${
+            selectedCountry === profile.country ? classes.blue : ''
+          }`}
+        >
+          <img
+            src={profile.img}
+            alt={profile.name}
+            className={classes.profileImg}
+            onClick={() =>
+              setShowDetails((showDetails) => (showDetails = profile.id))
+            }
+          />
+          {showDetails === profile.id && (
+            <div className={classes.dataContainer}>
+              <p>First name: {profile.firstName}</p>
+              <p>Last name: {profile.lastName}</p>
+              <p>Country: {profile.country}</p>
+              <p>Type: {profile.isStudent ? 'Student' : 'Teacher'}</p>
+            </div>
+          )}
         </div>
-      </div>
-    );
-  });
+      );
+    });
   ///COUNTRY buttons
 
   const setCountryHandler = ({ target }) => {
@@ -45,8 +59,20 @@ const Facebook = () => {
     </button>
   ));
 
+  const searchChangeHandler = ({ target }) => {
+    setSearchState(target.value);
+  };
+
   return (
     <div>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search"
+        value={searchState}
+        onChange={searchChangeHandler}
+      />
       {countryButtons}
       {profilesArr}
     </div>
