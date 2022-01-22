6bbf995e01380cb209145eb15285e53a61384fa8
Brigi Horvath <brigi.horvath82@gmail.com>
2022-01-22 11:15:22 +0100

    Iteration 11

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
 import Dice from './components/Dice';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
@@ -107,6 +108,14 @@ function App() {
         imgClicked="/img/maxence-glasses.png"
       />
       <Dice />
+      <Carousel
+        images={[
+          'https://randomuser.me/api/portraits/women/1.jpg',
+          'https://randomuser.me/api/portraits/men/1.jpg',
+          'https://randomuser.me/api/portraits/women/2.jpg',
+          'https://randomuser.me/api/portraits/men/2.jpg',
+        ]}
+      />
     </div>
   );
 }
---
+++ src/components/Carousel.js
@@ -0,0 +1,32 @@
+import { Fragment, useState } from 'react';
+
+const Carousel = ({ images }) => {
+  const [imgNum, setImgNum] = useState(0);
+
+  const carouselClickHandler = ({ target }) => {
+    const max = images.length - 1;
+    if (imgNum === max && target.id === 'right') {
+      setImgNum(0);
+    } else if (imgNum === 0 && target.id === 'left') {
+      setImgNum(max);
+    } else if (target.id === 'left') {
+      setImgNum((imgNum) => imgNum - 1);
+    } else if (target.id === 'right') {
+      setImgNum((imgNum) => imgNum + 1);
+    }
+    console.log(target.id);
+  };
+  return (
+    <Fragment>
+      <button type="button" id="left" onClick={carouselClickHandler}>
+        Left
+      </button>
+      <img src={images[imgNum]} alt="carousel" />
+      <button type="button" id="right" onClick={carouselClickHandler}>
+        Right
+      </button>
+    </Fragment>
+  );
+};
+
+export default Carousel;
