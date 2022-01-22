939f3bb5c14942bb6ca9deeeb6185945733f0b2b
Brigi Horvath <brigi.horvath82@gmail.com>
2022-01-22 12:09:26 +0100

    Iteration 14

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -1,10 +1,18 @@
 import profiles from '../data/berlin.json';
 import classes from './Facebook.module.css';
 
+import { useState } from 'react';
+
 const Facebook = () => {
+  const [selectedCountry, setSelectedCountry] = useState('');
   const profilesArr = profiles.map((profile) => {
     return (
-      <div key={profile.img} className={classes.container}>
+      <div
+        key={profile.img}
+        className={`${classes.container} ${
+          selectedCountry === profile.country ? classes.blue : ''
+        }`}
+      >
         <img
           src={profile.img}
           alt={profile.name}
@@ -19,7 +27,30 @@ const Facebook = () => {
       </div>
     );
   });
-  return <div>{profilesArr}</div>;
+  ///COUNTRY buttons
+
+  const setCountryHandler = ({ target }) => {
+    setSelectedCountry(target.textContent);
+  };
+  let countries = new Set(profiles.map((profile) => profile.country));
+  countries = Array.from(countries);
+  const countryButtons = countries.map((el) => (
+    <button
+      type="button"
+      key={el}
+      onClick={setCountryHandler}
+      className={selectedCountry === el ? classes.blue : ''}
+    >
+      {el}
+    </button>
+  ));
+
+  return (
+    <div>
+      {countryButtons}
+      {profilesArr}
+    </div>
+  );
 };
 
 export default Facebook;
