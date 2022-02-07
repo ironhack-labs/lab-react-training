550bb27c782e6c8d2be46bc43954c5a8cc967249
Fran <fransanxenxo@gmail.com>
2022-02-07 19:09:40 +0100

    iteration 6 , 7 and 8

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,24 +1,39 @@
 import React from 'react'
 
 class LikeButton extends React.Component {
-   
+  
+    
 
     state = {
-        count: 0
-    }
+        count: 0,
+        clicks: 0
+    };
+
+    colors =  ['purple','blue','green','yellow','orange','red']
+                
+    
 
     likeClick = () => {
         this.setState({
-            count: this.state.count + 1
+            count: this.state.count + 1,
+            clicks: this.state.clicks < this.colors.length + 1 ? this.state.clicks - 1 : 0,
         })
     }    
+
+    
     
     render() {
+
+        let styles = {
+                color:'white',
+                background: this.colors[this.state.clicks],
+        }
         return (
+           
             <div>
-            <button className='btn btn-primary' onClick={this.likeClick}>{this.state.count}</button>
+            <button className='btn btn-primary' styles={styles} onClick={this.likeClick}>{this.state.count} Likes</button>
         </div>
-        )
+        );
     }
 
 }
@@ -26,5 +41,7 @@ class LikeButton extends React.Component {
 
 
 export default LikeButton
+
+
 
 
