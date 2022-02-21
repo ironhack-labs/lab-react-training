541f983e723fd53f073637998b81753cb2f9e2fc
VmMad <88061365+VmMad@users.noreply.github.com>
2022-02-21 21:24:00 +0100

    fixed

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -2,29 +2,12 @@ import "./Rating.css"
 const Rating = props => {
     const { children } = props
 
-    let stars = ""
     let childrenRounded = Math.round(children)
 
-    switch (childrenRounded) {
-        case 1:
-            stars = "★☆☆☆☆"
-            break
-        case 2:
-            stars = "★★☆☆☆"
-            break
-        case 3:
-            stars = "★★★☆☆"
-            break
-        case 4:
-            stars = "★★★★☆"
-            break
-        case 5:
-            stars = "★★★★★"
-            break
-    }
+    const arr = ["☆☆☆☆☆", "★☆☆☆☆", "★★☆☆☆", "★★★☆☆", "★★★★☆", "★★★★★"]
 
     return (
-        <span className="rating">{stars}</span>
+        <span className="rating">{arr[childrenRounded]}</span>
     )
 }
 export default Rating
