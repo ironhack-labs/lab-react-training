cdcb12e022e4f07e05fc6ee751128bfba255ab78
Leonardo <mr_kunkel@hotmail.com>
2022-02-22 17:37:50 -0600

    Iteration 4

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -1,6 +1,26 @@
 
-export default function BoxColor() {
+export default function BoxColor(props) {
+    function RGBToHex(r,g,b) {
+        r = r.toString(16);
+        g = g.toString(16);
+        b = b.toString(16);
+      
+        if (r.length === 1)
+          r = "0" + r;
+        if (g.length === 1)
+          g = "0" + g;
+        if (b.length === 1)
+          b = "0" + b;
+      
+        return "#" + r + g + b;
+    }
+
     return (
-        <div>BoxColor</div>
+        <div>
+            <div style={{backgroundColor: `rgb(${props.r}, ${props.g}, ${props.b})`}}>
+                <h2>rgb({props.r}, {props.g}, {props.b})</h2>
+                <h2>{RGBToHex(props.r, props.g, props.b)}</h2>
+            </div>
+        </div>
     )
 }
