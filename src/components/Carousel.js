36b84f446309dd539b8d0cf3b7d2be26b9cf73f1
Ghalia AlHasan <ghaliaalhasan12@gmail.com>
2022-02-23 00:58:01 +0200

    finished iteration 11

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,12 +1,29 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 function Carousel(props) {
-    // const [carousel, setCarousel] = useState(false)
+    const [carouselIndex, setCarousel] = useState(0)
 
+    // How can I refactor this?
+
+    const carouselRight = () => {
+        setCarousel(carouselIndex => {
+            if (carouselIndex === 3) return carouselIndex - 3
+            return carouselIndex + 1
+        })
+
+    }
+
+    const carouselLeft = () => {
+        setCarousel(carouselIndex => {
+            if (carouselIndex === 0) return carouselIndex + 3
+            return carouselIndex - 1
+        })
+
+    }
     return (
         <div>
-            <button>Left</button>
-            <img src={props.images[0]} alt='user carousel' />
-            <button>Right</button>
+            <button onClick={carouselLeft}>Left</button>
+            <img src={props.images[carouselIndex]} alt='user carousel' />
+            <button onClick={carouselRight}>Right</button>
         </div>
     )
 }
