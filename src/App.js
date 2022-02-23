cf10d3a52a9933e7c5711af84759cfaaf5bcbdb6
Alan Cruz <alanchis@hotmail.com>
2022-02-23 11:12:57 -0600

    iteracion 6

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 
 import './App.css';
 import BoxColor from './components/Main/BoxColor';
+import CreditCard from './components/Main/CreditCard';
 import Greetings from './components/Main/Greetings';
 import IdCard from './components/Main/IdCard';
 import Random from './components/Main/Random';
+import Rating from './components/Main/Rating';
 
 
 const idCards = [{
@@ -38,16 +40,50 @@ const r2 = Math.floor(Math.random() * (255 - 0) + 0)
 const g2 = Math.floor(Math.random() * (255 - 0) + 0)
 const b2 = Math.floor(Math.random() * (255 - 0) + 0)
 
+const creditCards = [{
+  type:"Visa",
+  number:"0123456789018845",
+  expirationMonth:3,
+  expirationYear:2021,
+  bank:"BNP",
+  owner:"Maxence Bouret",
+  bgColor:"#11aa99",
+  color:"white" 
+},{
+  type:"Master Card",
+  number:"0123456789010995",
+  expirationMonth:"3",
+  expirationYear:2021,
+  bank:"N26",
+  owner:"Maxence Bouret",
+  bgColor:"#eeeeee",
+  color:"#222222",
+},{
+  type:"Visa",
+  number:"0123456789016984",
+  expirationMonth:12,
+  expirationYear:2019,
+  bank:"Name of the Bank",
+  owner:"Firstname Lastname",
+  bgColor:"#ddbb55",
+  color:"white" 
+}]
+
+const rating = Math.round(Math.random()*5*10)/10
+
 
 function App() {
   return (
     <div className="App">
 
+    <h1>1. IdCard</h1>
     {
       idCards.map((idCard)=>{
         return <IdCard idInfo={idCard} />
       })
     }
+
+    <h1>2. Greetings</h1>
 
     {
       languages.map((language)=>{
@@ -57,9 +93,27 @@ function App() {
       })
     }
 
+    <h1>3. Random</h1>
+
     <Random min={min} max={max} />
+
+    <h1>4. Box Color</h1>
     <BoxColor r={r} g={g} b={b} />
     <BoxColor r={r2} g={g2} b={b2} />
+
+    <h1>5. Credit Card</h1>
+    <div className='cards'>
+    {
+      creditCards.map((creditCard)=>{
+        return <CreditCard creditCardInfo={creditCard} />
+      })
+    }
+    </div>
+
+    <h1>6. Rating</h1>
+    <Rating children={rating}/>
+
+
 
     </div>
   );
---
+++ src/components/Main/CreditCard.js
@@ -0,0 +1,36 @@
+// ./components/Main/CreditCard.js
+import React from 'react'
+
+export default function CreditCard(props) {
+    console.log("props:",props)
+
+    let cardLogo = "";
+
+    switch(props.creditCardInfo.type){
+        case "Master Card": cardLogo = "src/assets/images/master-card.svg"
+        break;
+        case "Visa": cardLogo = "src/assets/images/visa.png"
+        break;
+        default:
+        break;
+    }
+
+    console.log(cardLogo)
+
+    let lastDigits = props.creditCardInfo.number.split("").splice(12,4).join("")
+    console.log(lastDigits)
+
+    const divStyle = {
+        backgroundColor: `${props.creditCardInfo.bgColor}`
+      };
+
+  return (
+        <div className='creditCard' style={divStyle}>
+            <img src={cardLogo} alt="cardLogo"/>
+            <p>&bull; &bull; &bull; &bull;  &bull; &bull; &bull; &bull;  &bull; &bull; &bull; &bull;  {lastDigits}</p>
+            <span>Expires: {props.creditCardInfo.expirationMonth}/{props.creditCardInfo.expirationMonth}</span>
+            <span> {props.creditCardInfo.bank}</span>
+            <p>{props.creditCardInfo.owner}</p>
+        </div>
+  )
+}
