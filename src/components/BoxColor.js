99046be5aa8f72b3a668d2c6bb58effe7a1ab327
denisesenguel <denisesenguel@gmail.com>
2022-02-24 12:36:29 +0100

    added hex code to BoxColor

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -4,9 +4,19 @@ function BoxColor(props) {
     const {r, g, b} = props;
     const bgColor = "rgb(" + [r, g, b].join(",") + ")";
      
+    function convert(rgb) {
+
+        function hexCode(i) {
+            return ("0" + parseInt(i).toString(16)).slice(-2);
+        }
+        return "#" + hexCode(rgb[0]) + hexCode(rgb[1]) 
+                + hexCode(rgb[2]);
+    }
+
     return(
         <div className="BoxColor" style={{ backgroundColor: bgColor }}>
             <p>rgb({ r }, { g }, { b })</p>
+            <p>{ convert([r, g, b]) }</p>
         </div>
     )
 }
