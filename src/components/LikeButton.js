b5e6796b981f38a5c90c5f72d542c31435d63960
denisesenguel <denisesenguel@gmail.com>
2022-02-24 16:23:18 +0100

    change background of button on click

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -3,11 +3,23 @@ import './LikeButton.css';
 
 function LikeButton() {
 
+    const colorsArray = ['purple','blue','green','yellow','orange','red'];
+
     const [counter, setCounter] = useState(0);
+    const [bgColor, setBgColor] = useState(colorsArray[0])
+
+    const clickHandler = () => {
+
+        setBgColor((prev) => {
+            const index = colorsArray.indexOf(prev);
+            return (index == colorsArray.length - 1) ? colorsArray[0] : colorsArray[index + 1];
+        });
+        setCounter((prev) => prev + 1);
+    }
 
     return (
         <div className="LikeButton">
-            <button onClick={ () => setCounter((prev) => prev + 1) }> {counter} Likes </button>
+            <button onClick={ clickHandler } style={{backgroundColor: bgColor}}> {counter} Likes </button>
         </div>
     )
 }
