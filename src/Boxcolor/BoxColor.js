d8f61d9cc428f9c03d555448fd64d2863965d0cd
Nathalia Maia <nathaliamaiape@gmail.com>
2022-03-08 18:56:31 -0300

    changed the style of the iteration 4

--- src/Boxcolor/BoxColor.js
+++ src/Boxcolor/BoxColor.js
@@ -2,15 +2,19 @@
 import './BoxColor.css'
 
 function BoxColor({ r, g, b }) {
-  
+
+  const rConvert = r.toString(16).length === 1 ? '0' + r.toString(16) : r.toString(16);
+  const gConvert = g.toString(16).length === 1 ? '0' + g.toString(16) : g.toString(16);
+  const bConvert = b.toString(16).length === 1 ? '0' + b.toString(16) : b.toString(16);
 
   return (
-    <div className="card" style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
-      <h1>
-        rgb {r} {g} {b}
-      </h1>
+    <div className="card-color" style={{ backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
+      <p className='text'>
+        rgb ({r},{g},{b})
+        <br/>
+        #{rConvert}{gConvert}{bConvert}
+      </p>
     </div>
-
   );
 }
 
