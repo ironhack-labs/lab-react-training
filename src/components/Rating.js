b59e3941e3a44c4aa94fd4c51df3fab959f62f33
Camila Solér <msoler.camila@gmail.com>
2022-03-08 19:50:32 -0300

    lab done finaal

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,51 +1,9 @@
 function Rating(props) {
-  // let rating = '';
-  // let ratingRound = Math.round(props.children);
 
-  // switch (ratingRound) {
-  //   case 0:
-  //     rating = '☆☆☆☆☆';
-  //     break;
-  //   case 1:
-  //     rating = '★☆☆☆☆';
-  //     break;
-  //   case 2:
-  //     rating = '★★☆☆☆';
-  //     break;
-  //   case 3:
-  //     rating = '★★★☆☆';
-  //     break;
-  //   case 4:
-  //     rating = '★★★★☆';
-  //     break;
-  //   case 5:
-  //     rating = '★★★★★';
-  //     break;
-  //   default:
-  // }
-  // let arr = ['☆', '☆', '☆', '☆', '☆'];
+  let grade = Math.round(props.children);
+  let starRating = '★'.repeat(grade) + '☆'.repeat(5 - grade);
 
-  // let newArr = arr.map((currentElement) => {
-  //   if (Math.round(props.children) === arr.indexOf(currentElement) + 1) {
-  //     currentElement = '★';
-  //   }
-  //   return currentElement;
-  // });
-
-  let grade = '';
-  let gradeRounded = Math.round(props.children);
-  for (let i = 0; i <= 5; i++) {
-    if (i < gradeRounded) {
-      grade += '★';
-    } else {
-      grade += '☆';
-    }
-  }
-
-  // let grade = Math.round(props.children);
-  // let starRating = '★'.repeat(grade) + '☆'.repeat(5 - grade);
-
-  return <p className="rating">{grade}</p>;
+  return <p className="rating">{starRating}</p>;
 }
 
 // ☆★
