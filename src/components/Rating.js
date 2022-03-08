521e16442df29879abe206661b08a7ad388d459c
Camila Solér <msoler.camila@gmail.com>
2022-03-08 18:44:12 -0300

    lab done

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,48 +1,4 @@
 function Rating(props) {
-  // let rating = '';
-  // let ratingRound = Math.round(props.children);
-
-  // switch (rating) {
-  //   case ratingRound === 0:
-  //     rating = '☆☆☆☆☆';
-  //     break;
-  //   case ratingRound === 1:
-  //     rating = '★☆☆☆☆';
-  //     break;
-  //   case ratingRound === 2:
-  //     rating = '★★☆☆☆';
-  //     break;
-  //   case ratingRound === 3:
-  //     rating = '★★★☆☆';
-  //     break;
-  //   case ratingRound === 4:
-  //     rating = '★★★★☆';
-  //     break;
-  //   case ratingRound === 5:
-  //     rating = '★★★★★';
-  //     break;
-  //   default:
-  // }
-  //   let arr = ['☆', '☆', '☆', '☆', '☆'];
-
-  //   let newArr = arr.map((currentElement) => {
-  //     if (Math.round(props.children) === arr.indexOf(currentElement) + 1) {
-  //      (currentElement = '★');
-  //     }
-  //     return currentElement
-  //   }
-  //   );
-
-  // let grade = '';
-  // let gradeRounded = Math.round(props.children);
-  // for (let i = 0; i <= 5; i++) {
-  //   if (i < gradeRounded) {
-  //     grade = +'★';
-  //   } else {
-  //     grade = +'☆';
-  //   }
-  // }
-
   let grade = Math.round(props.children);
   let starRating = '★'.repeat(grade) + '☆'.repeat(5 - grade);
 
