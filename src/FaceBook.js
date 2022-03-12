6da17b3be8e806ddc4eae1c320ccf85813826ebc
Bruno Mortella <bmortella@gmail.com>
2022-03-12 12:27:38 -0300

    Iteration 14

--- src/FaceBook.js
+++ src/FaceBook.js
@@ -1,10 +1,63 @@
+import { useState } from 'react';
+import { Profiler } from 'react/cjs/react.production.min';
 import profiles from './data/berlin.json';
 
 function FaceBook() {
+  const [state, setState] = useState('All');
+
+  const countries = ['England', 'USA', 'Malaysia', 'Germany'];
+
+  function handleClick(event) {
+    setState(event.target.name);
+  }
+
   return (
     <div>
+      <ul className="filters">
+        <li key="All">
+          <button
+            name="All"
+            onClick={handleClick}
+            style={{ backgroundColor: state === 'All' ? 'aqua' : 'white' }}
+          >
+            All
+          </button>
+        </li>
+        {countries.map((country) => (
+          <li key={country}>
+            <button
+              name={country}
+              onClick={handleClick}
+              style={{ backgroundColor: state === country ? 'aqua' : 'white' }}
+            >
+              {country}
+            </button>
+          </li>
+        ))}
+        <li key="Other">
+          <button
+            name="Other"
+            onClick={handleClick}
+            style={{ backgroundColor: state === 'Other' ? 'aqua' : 'white' }}
+          >
+            ...
+          </button>
+        </li>
+      </ul>
+
       {profiles.map((profile) => (
-        <div className="id-card">
+        <div
+          className="id-card"
+          key={profile.lastName}
+          style={{
+            backgroundColor:
+              state === profile.country ||
+              state === 'All' ||
+              (state === 'Other' && !countries.includes(profile.country))
+                ? 'aqua'
+                : 'white',
+          }}
+        >
           <img src={profile.img}></img>
           <div>
             <b>First Name:</b> {profile.firstName}
