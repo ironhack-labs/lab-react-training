9f95b3f05478dac7d505ee454ca8c4252efaed91
Maria Gallego <mariagestionredes@gmail.com>
2022-03-19 14:06:51 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard'
 
 function App() {
+
+  const data = [
+{
+  lastName:'Doe',
+  firstName:'John',
+  gender:'male',
+  height: 178,
+  birth: new Date("1992-07-14"),
+  picture:"https://randomuser.me/api/portraits/men/44.jpg"
+},
+
+{
+  lastName:'Delores',
+  firstName:'Obrien',
+  gender:'female',
+  height: 172,
+  birth: new Date("1988-05-11"), //ni puta idea de como formatear esto. si uso '.format('MM-DD-YYYY')' peta
+  picture:"https://randomuser.me/api/portraits/women/44.jpg"
+}
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     {
+       data.map((props, index) => {
+         return (
+           <IdCard key={index} data={props} />
+         )
+       })
+     }
     </div>
   );
 }
---
+++ src/components/IdCard.js
@@ -0,0 +1,18 @@
+import './IdCard.css'
+
+function IdCard({ data }) {
+  return (
+    <div className="Card">
+      <img src={data.picture} className="card-img top" alt="pic" />
+      <div className="CardBody">
+        <p className="CardText">First name: {`${data.firstName}`}</p>
+        <p className="CardText">Last name: {`${data.lastName}`}</p>
+        <p className="CardText">Gender: {`${data.gender}`}</p>
+        <p className="CardText">Height: {`${data.height}`}</p>
+        <p className="CardText">Birth:</p>
+      </div>
+    </div>
+  );
+}
+
+export default IdCard;
