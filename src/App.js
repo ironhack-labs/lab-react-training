929ff619e41ef7d1a78298be16670168ec774034
Miguel <info.miguelbanos@gmail.com>
2022-03-19 14:17:42 +0100

    first iteration done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import IdCards from './components/IdCards/IdCards';
+import Data from './data/berlin.json'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IdCards lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <IdCards
+        lastName='Delores '
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth={new Date("1988-05-11")}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
     </div>
   );
 }
---
+++ src/components/IdCards/IdCards.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const IdCards = (props) => {
+    const parseDate = (date) => {
+        return date.getFullYear()
+    }
+    const parseHeight = (height)=> {
+        return height/100
+    }  
+    return (
+            <div className="card mb-3 mt-4">
+                <div className="row g-0 my-2 mx-2">
+                    <div className=" d-flex justify-content-start">
+                        <img src={props.picture} className="img-fluid rounded-start" alt="..." />
+                        <div className="card-body text-start">
+                            <h5 className="card-title">First Name: {props.firstName}</h5>
+                            <h5 className="card-title">Last Name: {props.lastName}</h5>
+                            <h5 className="card-title">Gender: {props.gender}</h5>
+                            <h5 className="card-title">Height: {parseHeight(props.height)}m</h5>
+                            <h5 className="card-title">Birth: {parseDate(props.birth)}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default IdCards;
+
