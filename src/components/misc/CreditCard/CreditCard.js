0bb771103fb21b79f0c0ece15cfb41734f5835ff
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-03-19 14:58:58 +0100

    Iteration 5 almost done and layout of the 6 and 7

---
+++ src/components/misc/CreditCard/CreditCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreditCard = (props) => {
+    return (
+        <div>
+
+        
+
+        </div>
+    );
+};
+
+export default CreditCard;
---
+++ src/components/misc/DriverCard/DriverCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DriverCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DriverCard;
