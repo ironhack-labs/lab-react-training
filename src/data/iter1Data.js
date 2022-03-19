8afda577792cb05c9cf63344be0842bbc7da76f9
David <davidblancoferrandez@gmail.com>
2022-03-19 14:07:44 +0100

    first commit

---
+++ src/data/iter1Data.js
@@ -0,0 +1,20 @@
+const iter1Data = [
+  {
+    lastName: 'Doe',
+    firstName: 'John',
+    gender: 'male',
+    height: 178,
+    birth: new Date('1992-07-14').toDateString(),
+    picture: 'https://randomuser.me/api/portraits/men/44.jpg',
+  },
+  {
+    lastName: 'Delores ',
+    firstName: 'Obrien',
+    gender: 'female',
+    height: 172,
+    birth: new Date('1988-05-11').toDateString(),
+    picture: 'https://randomuser.me/api/portraits/women/44.jpg',
+  },
+];
+
+export { iter1Data };
