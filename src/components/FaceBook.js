b45be0b2ac3cff8899efd224ee9e1ddc4fb6c3e8
Vitor Afonso <vittokm@hotmail.com>
2022-03-20 23:18:47 +0000

    iteration 14 done

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -5,13 +5,57 @@ import { useState } from 'react';
 export const FaceBook = () => {
 
     const [profiles, setProfiles] = useState(profilesData);
+   
+
+
+    const countryList = [...new Set(profiles.map(person => person.country))];
+    const countryListArr = countryList.map(item => {
+        return {name: item, color: ""};
+    });
+    
+    const [country, setCountry] = useState(countryListArr);
+
+    
+    const selectProfiles = (countryName) => {
+        let faceProfiles;
+        let btns;
+
+        btns = country.map(element => {
+            if (element.name === countryName){
+                element.color = "lightblue";
+                return element;
+            } else {
+                element.color = "";
+                return element;
+            }
+        });
+        
+        
+        faceProfiles = profiles.map(element => {
+            if (element.country === countryName){
+                element.color = "lightblue";
+                return element;
+            } else {
+                element.color = "";
+                return element;
+            }
+        });
+        setCountry(btns);
+        setProfiles(faceProfiles);
+    };
+
 
     return(
         <div className='FaceBook containers'>
+            <div className='listOfCountry'>
+                <button>All</button>
+                {country.map((country, index) => <button className={country.name} key={index.toString()} style={{backgroundColor: country.color}} onClick={() => selectProfiles(country.name)}>{country.name} </button>)}
+
+            </div>
             {profiles.map((person, index) => {
                 return(
 
-                    <div className='Id-card'>
+                    <div className={`Id-card facebook-profile ${person.country}`} key={index.toString()} style={{backgroundColor: person.color}}>
                         <img src={person.img} alt="Profile card" />
                         <div>
                             <span><b>First Name:</b> {person.firstName}</span> <br />
