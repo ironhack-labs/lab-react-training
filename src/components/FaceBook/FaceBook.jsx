394d3d5eb18bfb7b6fbe1d2b9efc4a2f77a0c51d
Pablo Mateos <pmateos805@gmail.com>
2022-03-23 00:45:32 +0100

    Iteration 14

--- src/components/FaceBook/FaceBook.jsx
+++ src/components/FaceBook/FaceBook.jsx
@@ -1,24 +1,75 @@
+import React, { Component } from 'react'
 import profiles from '../../data/berlin.json'
 import './FaceBook.css'
 
-const Facebook = () => {
-    return (
-        <div className='facebook-wrapper'>
-        { profiles.map((profile, index) => {
-            return(
-                <div key={index} className='facebook-card'>
-                    <img src={profile.img} alt="facebook" />
-                    <div className='facebook-card-body'>
-                        <p><strong>First Name:</strong> {profile.firstName}</p>
-                        <p><strong>Las Name:</strong> {profile.lastName}</p>
-                        <p><strong>Country:</strong> {profile.country}</p>
-                        <p><strong>Type:</strong> {profile.isStudent ? 'Student' :' Teacher'}</p>
+class Facebook extends Component {
+
+    state = {
+        selectedCountry: '',
+        countries: []
+    }
+
+    filteredCountries() {
+        const countries = []
+        profiles.forEach( profile => {
+            if (!countries.includes(profile.country)) {
+                countries.push(profile.country)
+            }
+        })
+        this.setState({
+            countries: countries
+        })
+    }
+
+    countryClicked(country){
+        this.setState({
+            selectedCountry: country
+        })
+    }
+
+    componentDidMount() {
+        this.filteredCountries()
+    }
+
+
+    render(){
+        return (
+            <div className='facebook-wrapper'>
+            
+            {this.state.countries.map((country, index)=>{
+               return(
+                    <div key={index} >
+                        <button className="country-buttons" onClick={()=> this.countryClicked(country)} style={{backgroundColor: this.state.selectedCountry === country ? "#A3D2E2" : "#fff" }}> {country}</button>
                     </div>
-                </div>
-            )
-        })}
-        </div>
-    )
+               ) 
+            })}
+
+            <div>
+            { profiles.map((profile, index) => {
+                return(
+                    <div key={index} className='facebook-card' style={{backgroundColor: this.state.selectedCountry === profile.country ? "#A3D2E2" : "#fff" }}>
+                        <img src={profile.img} alt="facebook" />
+                        <div className='facebook-card-body'>
+                            <p><strong>First Name:</strong> {profile.firstName}</p>
+                            <p><strong>Las Name:</strong> {profile.lastName}</p>
+                            <p><strong>Country:</strong> {profile.country}</p>
+                            <p><strong>Type:</strong> {profile.isStudent ? 'Student' :' Teacher'}</p>
+                        </div>
+                    </div>
+                )
+            })}
+            </div>
+
+
+
+            </div>
+        )
+    }
+    
 }
+
+
+
+
 
 export default Facebook
