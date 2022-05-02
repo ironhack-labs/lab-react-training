f0ace557e666e31cc8f767c346ec09d7f969b4e0
Jose Pereira suarez <johndoe@example.com>
2022-05-03 01:56:57 +0200

    ajuste en iteración 6

--- src/Rating/Rating.js
+++ src/Rating/Rating.js
@@ -6,27 +6,54 @@ const Rating = ({ children }) => {
     let value = Math.round(children);
     let stars = value
 
-    switch (value) {
+    // switch (value) {
 
-        case 0:
-            stars = '☆☆☆☆☆'
+    //     case 0:
+    //         stars = '☆☆☆☆☆'
 
-        case 1:
-            stars = '★☆☆☆☆'
+    //     case 1:
+    //         stars = '★☆☆☆☆'
 
-        case 2:
-            stars = '★★☆☆☆'
+    //     case 2:
+    //         stars = '★★☆☆☆'
 
-        case 3:
-            stars = '★★★☆☆'
+    //     case 3:
+    //         stars = '★★★☆☆'
 
-        case 4:
-            stars = '★★★★☆'
+    //     case 4:
+    //         stars = '★★★★☆'
 
-        case 5:
-            stars = '★★★★★'
+    //     case 5:
+    //         stars = '★★★★★'
+
+    // }
+
+
+    if (value === 0) {
+        stars = <p>☆☆☆☆☆</p>
+    }
+
+    if (value === 1) {
+        stars = <p>★☆☆☆☆</p>
+    }
+
+    if (value === 2) {
+        stars = <p>★★☆☆☆</p>
 
     }
+
+    if (value === 3) {
+        stars = <p>★★★☆☆</p>
+    }
+
+    if (value === 4) {
+        stars = <p>★★★★☆</p>
+    }
+
+    if (value === 5) {
+        stars = <p>★★★★★</p>
+    }
+
 
     return (
         <div className="rating">
@@ -39,30 +66,3 @@ const Rating = ({ children }) => {
 }
 
 export default Rating
-
-
-
-        // if (value === 0) {
-        //     stars = <p>☆☆☆☆☆</p>
-        // }
-
-        // if (value === 1) {
-        //     stars = <p>★☆☆☆☆</p>
-        // }
-
-        // if (value === 2) {
-        //     stars = <p>★★☆☆☆</p>
-
-        // }
-
-        // if (value === 3) {
-        //     stars = <p>★★★☆☆</p>
-        // }
-
-        // if (value === 4) {
-        //     stars = <p>★★★★☆</p>
-        // }
-
-        // if (value === 5) {
-        //     stars = <p>★★★★★</p>
-        // }
