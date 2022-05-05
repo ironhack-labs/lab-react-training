66bab2d5358dedc1201f845ec3f2413209a6e132
LucasPerezDev <LucasPerezDev@gmail.com>
2022-05-05 19:05:50 -0400

    Completed iteration 6

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -3,8 +3,14 @@ function BoxColor(props) {
     return (
         <div style={{
             'padding': '2rem 8rem',
-            'backgroundColor': `rgb(${props.r}, ${props.g}, ${props.b}`
-        }}
-        
+            'backgroundColor': `rgb(${props.r}, ${props.g}, ${props.b}`,
+            'color': `color-contrast(rgb(${props.r}, ${props.g}, ${props.b}) vs white, black )`
+        }}>
+        <p> 
+        {`rgb({props.r}, ${props.g}, ${props.b})`}
+        </p>
+        </div>
     )
 }
+
+export default BoxColor;
---
+++ src/components/CreditCard.js
@@ -0,0 +1,147 @@
+import masterCard from '../assets/images/master-card.svg';
+import visa from '../assets/images/visa.png';
+
+function CreditCard(props) {
+
+    const { type, number, expirationMonth, expirationYear, bank, owner, bgColor, color } = props;
+    
+    let cardLogo;
+
+    if(type === 'Visa') {
+        cardLogo = visa;
+    } else {
+        cardLogo = masterCard;
+    }
+
+    let lastFourCardDigits = number.substring(number.length - 4);
+
+    return(
+        <div style={{
+            'backgroundColor': bgColor,
+            'color': color,
+            'pagging': '2rem',
+            width: '400px',
+            marginTop: '2rem',
+            marginLeft: '2rem',
+            borderRadius: '10px',
+        }}>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'end'
+            }}>
+                <img src={cardLogo} alt='card logo' style={{
+                    height: '30px',
+                    width: '50px',
+                    marginTop: '10px',
+                    marginRight: '10px'
+                }}/>
+            </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                margin: '1.5rem 0'
+            }}>
+                <div style={{
+                    display: 'flex'
+                }} className="dotContainer">
+                    <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor:  color         
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                
+                    }}></div>
+                </div>
+                <div style={{
+                    display: 'flex'
+                }} className="dotContainer">
+                    <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                </div>
+                <div style={{
+                    display: 'flex'
+                }} className="dotContainer">
+                    <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                        <div style={{
+                        height: '10px',
+                        width: '10px',
+                        borderRadius: '50%', 
+                        backgroundColor: color                       
+                    }}></div>
+                </div>
+                <div style={{
+                    fontSize: '1.5rem'
+                }}>
+                    {lastFourCardDigits}
+                </div>
+            </div>
+                <div style={{
+                        textAlign: 'left',
+                        marginLeft: '2rem'
+                    }}>
+                    <div> Expires {expirationMonth}/{expirationYear}<span style={{ marginLeft: '2rem' }}>{bank}</span></div>
+                    <div style={{ marginBottom: '1rem' }}>{owner}</div>
+                </div>
+        </div>
+    );
+
+}
+
+export default CreditCard;
