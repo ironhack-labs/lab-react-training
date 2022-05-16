a12622fbf4629cf464f26a951743f8a37c186367
Ariana <pilarsv@outlook.com>
2022-05-15 23:52:48 -0500

    done

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,34 @@
+
+  
+import React from 'react'
+
+const Greetings = (props) => {
+    const {lang, children} = props
+
+    let hello
+    switch (lang){
+        case "de":
+            hello = "Hallo"
+            break;
+        case "en":
+            hello = "Hello"
+            break;
+        case "es":
+            hello = "Hola"
+            break;
+        case "fr":
+            hello = "Bonjour"
+            break;
+        default:
+            hello = "No language selected"
+            break;
+    }
+
+  return (
+    <div  className="greetings" >
+      <p>{hello} {children}</p>
+  </div>)
+  
+}
+
+export default Greetings
---
+++ src/components/Random.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Random = (props) => {
+
+    const {min, max} = props
+
+    const randomNum = Math.floor(Math.random()*(max-min)+min)
+
+  return (
+    <div className="random-card">
+            <p>Random value between {min} and {max} ={">"} {randomNum}</p>
+        </div>
+  )
+}
+
+export default Random
