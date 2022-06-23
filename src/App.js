aa693ac980d878d11bd3c3ee3bc0c5750aea5171
Haroun B <haroun@harryb.dev>
2022-06-23 20:44:23 +0200

    iteration 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Random from './components/Random';
+import BoxColor from './components/BoxColor';
 
 
 function App() {
   return (
     <div className="App">
-      <Random min={1} max={6} />
-      <Random min={1} max={100} />
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
---
+++ src/components/BoxColor.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BoxColor = ({r, g, b}) => {
+  const [rHex, gHex, bHex] = [r, g, b].map(c => parseInt(c).toString(16));
+
+  return (
+    <div style={{ border: `1px solid #000`, margin: `.5rem`, padding: `2rem`, height: `5.5rem`, backgroundColor: `rgb(${r}, ${g}, ${b})` }}>
+      <p>{`rgb(${r}, ${g}, ${b})`}</p>
+      <p>{`#${rHex}${gHex}${bHex}`}</p>
+      </div>
+  )
+}
+
+export default BoxColor
