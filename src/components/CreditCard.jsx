b65fad4848b199e3d7209867c8ccbcb8e305b627
Haroun B <haroun@harryb.dev>
2022-06-25 19:31:07 +0200

    iteration 5

---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,28 @@
+import visa from '../assets/images/visa.png';
+import mastercard from '../assets/images/mastercard.png';
+import './CreditCard.css';
+
+const CreditCard = ({ type, number, expirationMonth, expirationYear, bank, owner, bgColor, color }) => {
+  const logo = type === `Visa` ? visa : mastercard;
+
+  return (
+    <div className="credit-card"
+      style={{
+        backgroundColor: `${bgColor}`,
+        color,
+      }}
+    >
+      <img src={logo} alt={type} />
+      <p className='card-number'>{`⏺⏺⏺⏺ ⏺⏺⏺⏺ ⏺⏺⏺⏺ ` + number.slice(-4)}</p>
+      <div>
+        <div className="info">
+          <p>{`Expires: ${expirationMonth}/${expirationYear}`}</p>
+          <p>{bank}</p>
+        </div>
+        <p className='owner'>{owner}</p>
+      </div>
+    </div>
+  )
+}
+
+export default CreditCard
