ad07efa185360f7286e0062c27df5bcaa11d50a3
Haroun B <haroun@harryb.dev>
2022-06-25 19:43:25 +0200

    iteration 6

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
 import './App.css';
-import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 
 function App() {
   return (
     <div className="App">
-      <CreditCard
-        type="Visa"
-        number="0123456789018845"
-        expirationMonth={3}
-        expirationYear={2021}
-        bank="BNP"
-        owner="Maxence Bouret"
-        bgColor="#11aa99"
-        color="white"
-      />
-
-      <CreditCard
-        type="Master Card"
-        number="0123456789010995"
-        expirationMonth={3}
-        expirationYear={2021}
-        bank="N26"
-        owner="Maxence Bouret"
-        bgColor="#eeeeee"
-        color="#222222"
-      />
-
-      <CreditCard
-        type="Visa"
-        number="0123456789016984"
-        expirationMonth={12}
-        expirationYear={2019}
-        bank="Name of the Bank"
-        owner="Firstname Lastname"
-        bgColor="#ddbb55"
-        color="white"
-      />
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>
     </div>
   );
 }
