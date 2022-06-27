65c8b44046b34132998ed52787f8f1720067a96a
RomainC75 <rom.chenard@gmail.com>
2022-06-27 22:28:41 +0200

    iteration10

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import './Dice.css'
 
 export default function Dice() {
+    const [click, setClick] = useState(false)
+    const [image , setImage ] = useState('./assets/images/dice-empty.png')
+    const base_url = './assets/images/'
+
+    useEffect(()=>{
+
+        setTimeout(()=>{
+            const result = Math.ceil(Math.random()*6)
+            console.log('resultat : ',result)
+            setImage(`${base_url}dice${result}.png`)
+        },1000)
+    },[click])
+
   return (
-    <div>Dice</div>
+    <div className="diceContainer">
+        <img src={image} alt="dice" onClick={()=>setClick(!click)}/>
+    </div>
   )
 }
