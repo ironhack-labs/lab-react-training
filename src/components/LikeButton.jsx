910ee787d861576cd61a91050183d8b7c61b15ea
RomainC75 <rom.chenard@gmail.com>
2022-06-27 10:28:07 +0200

    step 8

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useState} from 'react'
+
+import './likeButton.css'
+
+export default function LikeButton() {
+    const colorsArray = ['purple','blue','green','yellow','orange','red']
+    const [ counter, setCounter ] = useState(0)
+
+    return (
+    <div class="Button" style={{backgroundColor:colorsArray[counter%6]}} onClick={()=>{setCounter(counter+1)}}>{counter} Likes</div>
+  )
+}
