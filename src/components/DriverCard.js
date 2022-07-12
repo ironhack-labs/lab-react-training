1412b19593fdb69e98c610642edac534d907bc12
Joao Vcitor Brito Fuller <fullerjoao@outlook.com>
2022-07-12 12:18:56 +0200

    done

---
+++ src/components/DriverCard.js
@@ -0,0 +1,18 @@
+import Rating from './Rating';
+import './DriverCard.css';
+
+const DriverCard = ({ name, rating, img, car: { model, licensePlate } }) => {
+  return (
+    <div className='driver-card'>
+      <div className="img" style={{ backgroundImage: `url(${img})`}}></div>
+
+      <div className="info">
+        <h2>{name}</h2>
+        <Rating>{rating}</Rating>
+        <p>{`${model} - ${licensePlate}`}</p>
+      </div>
+    </div>
+  )
+}
+
+export default DriverCard; 
---
+++ src/components/Rating.js
@@ -0,0 +1,14 @@
+const Rating = ({children}) => {
+    const rating = Math.round(parseFloat(children));
+    let stars = ``
+  
+    for (let i = 0; i < 5; i++) {
+      stars.length < rating ? stars += `★` : stars += `☆`;
+    }
+  
+    return (
+      <h2>{stars}</h2>
+    );
+  }
+  
+  export default Rating
