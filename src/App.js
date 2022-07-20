5728aff89cdad9b74daf405453ffae07882ad79a
Val Mejia <valemegaestrellalectora@gmail.com>
2022-07-20 00:13:41 -0500

    iteration 0 & 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import IdCard from './components/IdCard';
+import data from "./infocards";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [infocards] = useState(data);
+  const cardpeople = infocards.map((card) => {
+    return (
+      <IdCard 
+        infocards={card}
+      />
+    );
+  });
+
+  return(
+    <div>
+      {cardpeople}
     </div>
+      
+    
   );
-}
+};
 
 export default App;
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,10 @@
+function Greetings (props){
+    const { lang, children } = props.;
+    return(
+      <div>
+        <h1> hola</h1>
+      </div>
+    )
+  }
+  
+  export default Greetings;
