300d809d917d58b5e61f57bce8e4733c76cc91dd
Vale Quiroga <valentinqramos@outlook.com>
2022-07-24 18:25:39 -0500

    Iteration 1

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,19 @@
+
+const IdCard = (props) => {
+  return ( 
+    <div className="idProfile">
+      <div>
+        <img src={props.picture} alt="" />
+      </div>
+      <div>
+        <p> <strong>First name:</strong> {props.firstName} </p>
+        <p> <strong>Last name:</strong> {props.lastName} </p>
+        <p> <strong>Gender:</strong> {props.gender} </p>
+        <p> <strong>Height:</strong> {props.height}</p>
+        <p> <strong>Birth:</strong> </p>
+      </div>
+    </div>
+  );
+}
+
+export default IdCard;
