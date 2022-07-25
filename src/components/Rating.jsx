2d49d2a97c9f95ac640ea706bf391a6232af0e12
Vale Quiroga <valentinqramos@outlook.com>
2022-07-25 11:32:50 -0500

    Iteration 6

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,19 @@
 
 const Rating = (props) => {
+  let stars = Math.round(props.children)
+
+  if(stars === 0){stars = '☆☆☆☆☆'} 
+  else if(stars === 1){stars = '★☆☆☆☆'} 
+  else if(stars === 2){stars = '★★☆☆☆'} 
+  else if(stars === 3){stars = '★★★☆☆'} 
+  else if(stars === 4){stars = '★★★★☆'} 
+  else if(stars === 5){stars = '★★★★★'}
+
   return ( 
-    <div>
-      <p>test</p>
+    <div className="ratingContainer">
+      <p> {stars} </p>
     </div>
   );
 }
-
+// ★☆
 export default Rating;
