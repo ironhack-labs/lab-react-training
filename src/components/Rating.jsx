15c319398f8b415295002e504f9f074bec77ff6c
Paul Tiry <tirypaul@gmail.com>
2022-07-25 21:29:42 +0200

    added key prop to star Icons

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,18 +4,18 @@ import styled from 'styled-components'
 const Rating = ({ children }) => {
   const rating = Math.round(children)
   const starsArr = [
-    <Icon strokeWidth={10} fill="none" />,
-    <Icon strokeWidth={10} fill="none" />,
-    <Icon strokeWidth={10} fill="none" />,
-    <Icon strokeWidth={10} fill="none" />,
-    <Icon strokeWidth={10} fill="none" />,
+    <Icon key={0} strokeWidth={10} fill="none" />,
+    <Icon key={1} strokeWidth={10} fill="none" />,
+    <Icon key={2} strokeWidth={10} fill="none" />,
+    <Icon key={3} strokeWidth={10} fill="none" />,
+    <Icon key={4} strokeWidth={10} fill="none" />,
   ]
 
   return (
     <Wrapper>
       {starsArr.map((star, index) => {
         if (index + 1 <= rating) {
-          return <Icon strokeWidth={10} fill="black" />
+          return <Icon key={index} strokeWidth={10} fill="black" />
         } else {
           return star
         }
