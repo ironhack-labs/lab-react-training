0412e4a4dca0d00f8aa7722474bf2011a8e62a7f
ana luz martinez <almadigital.mx@gmail.com>
2022-07-25 12:20:11 -0700

    iteration 1

---
+++ src/components/IdCard.js
@@ -0,0 +1,26 @@
+import './IdCard.css';
+function IdCard({ lastName, firstName, gender, height, birth, picture }){
+    const stringBirth = birth.toString().slice(0, 15);
+
+    return(
+        <div className="id-card">
+            <img src={picture} alt="" />
+            <div className="data">
+                <p> <b>First Name:</b>  {firstName}</p>
+                <p> <b>Last Name:</b>   {lastName}</p>
+                <p> <b>Gender:</b>  {gender}</p>
+                <p> <b>Height:</b>  {height}</p>
+                <p> <b>Birth:</b>   {stringBirth}</p>
+            </div>
+        </div>
+    );
+}
+export default IdCard;
+
+// function IdCard(){
+//     return(
+//         <>
+//         </>
+//     );
+// }
+// export default IdCard;
