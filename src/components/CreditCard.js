73a4efeb17481ce7362053d75f62336e09c75f94
ana luz martinez <almadigital.mx@gmail.com>
2022-07-26 18:41:40 -0700

    iteration 5 done

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,14 +1,21 @@
 function CreditCard({type, number, expirationMonth, expirationYear, bank,owner, bgColor, color}){
+    
+    let year = expirationYear.toString().slice(-2)
+    const last4Str = String(number).slice(-4); // 👉️ '6789'
+    const last4Num = Number(last4Str)
+
     return(
-        <div style={background-color: `${bgColor}`}>
-            <span>{type}</span>
-            <h6>{number}</h6>
-            <div>
-                <p>Expires {expirationMonth}/{expirationYear}</p>
+    <div className="item-creditcard">
+        <div  className="single-creditcard" style={{backgroundColor: `${bgColor}`, color: `${color}`, borderColor: `${bgColor}`}}>
+            <span className="type">{type}</span>
+            <h6>**** **** **** {last4Num}</h6>
+            <div className="card-data">
+                <p>Expires {expirationMonth}/{year}        {bank}</p><br />
                 <p>{owner}</p>
             </div>
-            <div>{bank}</div>
+
         </div>
+    </div>
     );
 }
 
