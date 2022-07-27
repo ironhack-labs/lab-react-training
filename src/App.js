636181f74694d847bdb4ae3a7d7d1816f40230e7
ana luz martinez <almadigital.mx@gmail.com>
2022-07-26 18:55:08 -0700

    iteration 6 done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greetings from './components/Greetings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 function App() {
   return (
@@ -69,7 +70,13 @@ function App() {
       bgColor="#ddbb55"
       color="white" 
     /></div>
-
+    {/* Iteration #6 */}
+    <Rating>0</Rating>
+    <Rating>1.49</Rating>
+    <Rating>1.5</Rating>
+    <Rating>3</Rating>
+    <Rating>4</Rating>
+    <Rating>5</Rating>
     </div>
   );
 }
