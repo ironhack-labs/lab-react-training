8a2adf5c6912b72efe8f8c1472de67380d791d59
ana luz martinez <almadigital.mx@gmail.com>
2022-07-31 13:40:09 -0700

    iteration 7 done, with stylings

--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,17 +1,41 @@
-import Rating from "./Rating";
+import Rating from './Rating';
 
-function DriverCard({name, rating, img, car}){
+function DriverCard({ name, rating, img, car }) {
+  //from Rating
+  function stars(rating) {
+    if (Math.round(rating) === 0) {
+      return '☆☆☆☆☆';
+    } else if (Math.round(rating) === 1) {
+      return '★☆☆☆☆';
+    } else if (Math.round(rating) === 2) {
+      return '★★☆☆☆';
+    } else if (Math.round(rating) === 3) {
+      return '★★★☆☆';
+    } else if (Math.round(rating) === 4) {
+      return '★★★★☆';
+    } else if (Math.round(rating) === 5) {
+      return '★★★★★';
+    }
+    return '☆☆☆☆☆';
+  }
 
-    return(
-        <div className="id-driver">
-            <img src={img} alt="" />
-            <div className="driver-data">
-                <p> <b>{name}</b></p>
-                <p><Rating> {rating}</Rating></p>
-                <p>{car.model} = {car.licensePlate}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="id-driver">
+      <img src={img} alt="" />
+      <div className="driver-data">
+        <p>
+          {' '}
+          <b>{name}</b>
+        </p>
+        <p>
+          <Rating>{stars(rating)}</Rating>
+        </p>
+        <p>
+          {car.model} = {car.licensePlate}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default DriverCard;
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,26 +1,26 @@
-function Rating({children}){
-        function stars(children){
-        if (Math.round(children) === 0){
-            return '☆☆☆☆☆'
-        } else if (Math.round(children) === 1){
-            return '★☆☆☆☆'
-        } else if (Math.round(children) === 2){
-            return '★★☆☆☆' 
-        } else if (Math.round(children) === 3){
-            return '★★★☆☆'
-        } else if (Math.round(children) === 4){
-            return '★★★★☆'
-                } else if (Math.round(children) === 5){
-            return '★★★★★'
-        } return '☆☆☆☆☆'
+function Rating({ children }) {
+  function stars(children) {
+    if (Math.round(children) === 0) {
+      return '☆☆☆☆☆';
+    } else if (Math.round(children) === 1) {
+      return '★☆☆☆☆';
+    } else if (Math.round(children) === 2) {
+      return '★★☆☆☆';
+    } else if (Math.round(children) === 3) {
+      return '★★★☆☆';
+    } else if (Math.round(children) === 4) {
+      return '★★★★☆';
+    } else if (Math.round(children) === 5) {
+      return '★★★★★';
     }
+    return '☆☆☆☆☆';
+  }
 
-    return (
-        <> 
-        <h3>{stars}</h3>
-        </>
-           
-    );
+  return (
+    <>
+      <h4>{stars(children)}</h4>
+    </>
+  );
 }
 
 export default Rating;
