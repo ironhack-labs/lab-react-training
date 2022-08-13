a63dd28c42609fe65d8355c4a61c00074c886deb
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-13 13:08:35 -0300

    iteration 5 done

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,5 +1,6 @@
 import './CreditCard.css'
 import visaImg from '../assets/images/visa.png'
+import mastercard from '../assets/images/mastercard.svg'
 
 function CreditCard (props) {
     const { type, number, expirationMonth, expirationYear, bank, owner, bgColor, color } = props; 
@@ -8,7 +9,7 @@ function CreditCard (props) {
 
     return (
         <div className='creditcard' style={{backgroundColor:`${bgColor}`, color:`${color}`}}>
-            <p className='type'>{type === "Visa" ? <img src={visaImg} alt='visa'/> : <img src='../assets/images/master-card.svg' alt='master' />} </p>
+            <p className='type'>{type === "Visa" ? <img src={visaImg} alt='visa'/> : <img src={mastercard} alt='master' />} </p>
             <h2 className='number'>{hideNumbers.join(" ")}</h2>
             <p className='expiration'>Expires {expirationMonth}/{expirationYear} <p className='bank'>{bank}</p></p>
             <p className='owner'>{owner}</p>
