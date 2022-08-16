de64be8fb83b533319ddc4548390a471f124d088
Gabriella Hevesy <55456609+napoligab@users.noreply.github.com>
2022-08-16 18:10:25 +0100

    solved lab until 6th

--- src/Components/Rating/Rating.jsx
+++ src/Components/Rating/Rating.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-function Rating() {
-  return <div>Rating</div>;
+function Rating(props) {
+  const { children } = props;
+
+  let roundedRating = Math.round(children);
+
+  switch (roundedRating) {
+    case 0:
+      roundedRating = '☆☆☆☆☆';
+      break;
+    case 1:
+      roundedRating = '★☆☆☆☆';
+      break;
+    case 2:
+      roundedRating = '★★☆☆☆';
+      break;
+    case 3:
+      roundedRating = '★★★☆☆';
+      break;
+    case 4:
+      roundedRating = '★★★★☆';
+      break;
+    case 5:
+      roundedRating = '★★★★★';
+      break;
+    default:
+      return 'N/A';
+  }
+
+  return (
+    <div>
+      <p>{roundedRating}</p>
+    </div>
+  );
 }
 
 export default Rating;
