aead0ad929f616c50acffcec60e7584983c0a1d3
lzaquine <zaquinelucas@gmail.com>
2022-08-17 09:14:22 +0100

    it 6

--- src/App.js
+++ src/App.js
@@ -38,13 +38,13 @@ function App() {
       <BoxColor r={255} g={0} b={0} />
       <BoxColor r={128} g={255} b={0} />
       <hr />
-<Rating>0</Rating>
-<Rating>1.49</Rating>
-<Rating>1.5</Rating>
-<Rating>3</Rating>
-<Rating>4</Rating>
-<Rating>5</Rating>
-<hr/>
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>
+      <hr />
       <CreditCard
         type="Visa"
         number="0123456789018845"
