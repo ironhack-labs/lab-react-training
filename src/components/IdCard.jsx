48e587510e530e34d2b4da2fb823fbe31566a7ea
amandahp <amandahoffmann96@gmail.com>
2022-08-27 14:53:43 -0300

    feat: create card

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,28 @@
+import './IdCard.styles.css'
+
+export const IdCard = (props) => {
+ const {array} = props
+
+ const convertBool = (value) => {
+  return value === true ? "Yes" : "No"
+ }
+
+ return(
+  <>
+  
+    {array.map((content, index) => {
+     return(
+      <div key={`${content.birth} ${index}`} className="card">
+       <img src={content.img} alt={`Picture to ${content.firstName}`} />
+       <ul>
+        <li><b>&nbsp;First Name:</b> &nbsp;{content.firstName}</li>
+        <li><b>&nbsp;Last Name:</b> &nbsp;{content.lastName}</li>
+        <li><b>&nbsp;Countr:</b> &nbsp;{content.country}</li>
+        <li><b>&nbsp;Is Student:</b> &nbsp;{convertBool(content.isStudent)}</li>
+       </ul>
+      </div>
+     )
+    })}
+  </>
+ )
+}
