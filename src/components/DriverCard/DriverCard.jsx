e1fd5769a1d107bdb4a50644bf90659cf36b8cfb
luismunoz83 <javier@Castellanas-MBP.home>
2022-08-29 12:38:33 +0200

    Iteration 7 | Component: DriverCard correct

--- src/components/DriverCard/DriverCard.jsx
+++ src/components/DriverCard/DriverCard.jsx
@@ -1,23 +1,7 @@
 import './DriverCard.css'
+import Rating from '../Rating/Rating';
 
 function DiverCard({ name, rating, img, car }) {
-
-  const startRating = rating => {
-    switch (Math.round(rating)) {
-      case 1:
-        return '★☆☆☆☆'
-      case 2:
-        return '★★☆☆☆';
-      case 3:
-        return '★★★☆☆';
-      case 4:
-        return '★★★★☆';
-      case 5:
-        return '★★★★★';
-      default:
-        return '☆☆☆☆☆'
-    }
-  }
 
   return (
     <div className='driver-card'>
@@ -26,7 +10,7 @@ function DiverCard({ name, rating, img, car }) {
       </div>
       <div>
         <h2>{name}</h2>
-        <h2>{startRating(rating)}</h2>
+        <Rating>{rating}</Rating>
         <div className='d-flex flex-row'>
           <h4>{car.model} - {car.licensePlate}</h4>
         </div>
