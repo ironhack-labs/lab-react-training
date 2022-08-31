f36cefd72446eb0797838bc4e839bfa26b58d5cc
Benjamin Amzzough <amzzough.benjamin@yahoo.fr>
2022-08-31 19:27:11 +0200

    start

---
+++ src/components/IdCard.js
@@ -0,0 +1,17 @@
+function IdCard(props) {
+    return (
+        <div className="IdCard">
+            <div>
+                <img src={props.picture} alt="picture" />
+            </div>
+            <div>
+                <p><b>Firstname</b>: {props.firstName}</p>
+                <p><b>Lastname</b>: {props.lastName}</p>
+                <p><b>Gender</b>: {props.gender}</p>
+                <p><b>Height</b>: {props.height}</p>
+                <p><b>Birth</b>: {props.birth}</p>
+            </div>
+        </div>
+    )
+}
+export default IdCard
