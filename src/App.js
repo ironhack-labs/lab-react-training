2dd552866a398c16d911ef1c021753dfb7f17c8d
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-08-31 18:46:50 +0200

    feat: iteration 5 without styles

--- src/App.js
+++ src/App.js
@@ -35,17 +35,45 @@ function App() {
         <Greetings lang="fr">François</Greetings>
 
         <h1 className="mt-3 fw-bold py-2">Random</h1>
-        <Random min={1} max={6}/>
-        <Random min={1} max={100}/>
+        <Random min={1} max={6} />
+        <Random min={1} max={100} />
 
         <h1 className="mt-3 fw-bold py-2">BoxColor</h1>
         <BoxColor r={255} g={0} b={0} />
-        <BoxColor r={128} g={255} b={0} className={"text-dark"} />
+        <BoxColor r={128} g={255} b={0} className={'text-dark'} />
 
         <h1 className="mt-3 fw-bold py-2">CreditCard</h1>
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white"
+        />
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        />
 
-
-
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white"
+        />
       </div>
     </div>
   );
--- src/components/credit-card/CreditCard.js
+++ src/components/credit-card/CreditCard.js
@@ -1,23 +1,57 @@
-import visa from '../../assets/images/visa.png'
-import mastercard from '../../assets/images/martercad-logo.png'
+import visa from '../../assets/images/visa.png';
+import mastercard from '../../assets/images/martercad-logo.png';
 
-function CreditCard({ type, number, expirationMonth, expirationYear, bank, owner, bgColor, color}) {
-  let card = "";
-  if (type === "Visa") {
-    card = visa
-  } else {
-    card = mastercard
-  }
+function CreditCard({
+  type,
+  number,
+  expirationMonth,
+  expirationYear,
+  bank,
+  owner,
+  bgColor,
+  color,
+}) {
+  let card = '';
+  type === 'Visa' ? (card = visa) : (card = mastercard);
 
+  let cardNumber = number.slice(-4);
 
-  return(
-    <div className="d-flex flex-column">
-      <img src={card} alt={type} />
+  let month = { ...expirationMonth };
+  expirationMonth < 10 ? (month = '0' + expirationMonth) : (month = expirationMonth);
+
+  let year = expirationYear.toString().slice(-2);
+
+  return (
+    <div className="d-flex flex-column" style={{ backgroundColor: `${bgColor}`, color: `${color}` }}>
+        <img src={card} alt={type} className="align-self-end"/>
+      <div className="d-flex">
+      <div className="d-flex gap-1 px-2 align-self-center my-2">
+        <i className="fa fa-circle"></i>
+        <i className="fa fa-circle"></i>
+        <i className="fa fa-circle"></i>
+        <i className="fa fa-circle"></i>
+      </div>
+      <div className="d-flex gap-1 px-2 align-self-center my-2">
+        <i className='fa fa-circle'></i>
+        <i className='fa fa-circle'></i>
+        <i className='fa fa-circle'></i>
+        <i className='fa fa-circle'></i>
+      </div>
+      <div className="d-flex gap-1 px-2 align-self-center my-2">
+        <i className='fa fa-circle'></i>
+        <i className='fa fa-circle'></i>
+        <i className='fa fa-circle'></i>
+        <i className='fa fa-circle'></i>
+      </div>
+        <h1 className="d-flex gap-1 align-self-center my-2">{cardNumber}</h1>
+        <div className="d-flex">
+          <h4>Expires: {month}/{year}</h4>
+          <h4>{bank}</h4>
+          <h4>{owner}</h4>
+        </div>
+      </div>
     </div>
-  )
-
-
-
+  );
 }
 
 export default CreditCard;
