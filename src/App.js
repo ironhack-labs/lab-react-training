60cba6fc568372e5451c6c14c00137bf2ede224d
Iury Flores <iury.flores@gmail.com>
2022-09-03 16:13:18 -0300

    Interaction 2 - didnt know if I understand, but works just fine like the example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import IdCard from './components/IdCard';
 import './App.css';
+import Greetings from './components/Greetings';
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
         birth={new Date('1988-05-11')}
         picture="https://randomuser.me/api/portraits/women/44.jpg"
       />
+      <Greetings lang='de' children='Hallo Ludwing' />
+      <Greetings lang='fr' children='Bonjour François'/>
     </div>
   );
 }
---
+++ src/components/Greetings.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greetings = ({ children }) => {
+  return <div className="card">{children}</div>;
+};
+
+export default Greetings;
