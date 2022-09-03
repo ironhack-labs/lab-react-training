06da8b0a090e490e49435dfc79bfe140bd7bd573
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-09-03 20:22:48 -0300

    Iteração 5 | Componente:CreditCard

---
+++ src/components/CreditCard.js
@@ -0,0 +1,36 @@
+import './CreditCard.css'
+import visa from '../assets/images/visa.png'
+import mastercard from '../assets/images/mastercard.png'
+
+
+const CreditCard = (props) => {
+
+  const encryptNumber = (number) => {
+    let encrypt = '**** **** **** ' + number.slice(-4);
+
+    return encrypt
+
+  }
+
+  return (
+    <div id="container-card" style={{ fontSize: '2rem', backgroundColor: `${props.bgColor}`, color: `${props.color}` }}>
+
+      <div className="bandeira-card">
+        <img alt="logo" src={props.type == 'Visa' ? visa : mastercard}></img>
+      </div>
+
+      <div className="item-cardCenter">
+        <p style={{ margin: 0 }}>{encryptNumber(props.number)}</p>
+      </div>
+
+      <div className="item-card">
+        <p>Experies {props.expirationMonth}/{props.expirationYear}   {props.bank}</p>
+      </div>
+
+      <div className="item-card">
+        <p>{props.owner}</p>
+      </div>
+    </div>)
+}
+
+export default CreditCard
