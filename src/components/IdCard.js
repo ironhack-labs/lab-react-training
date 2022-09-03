512ae8418278f32db29336cdc09d7e531bd71ae1
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-09-03 15:29:26 -0300

    iteration 1 | Component: IdCard

---
+++ src/components/IdCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './IdCard.css';
+
+const IdCard = (props) => {
+  return (<>
+    <div id="container-Card">
+      <img alt={''} src={props.picture}></img>
+      <div id='item-Card'>
+        <p><b>First Name: </b>{props.firstName}</p>
+        <p><b>Last Name: </b>{props.lastName}</p>
+        <p><b>Gender: </b>{props.gender}</p>
+        <p><b>Height: </b>{props.height}</p>
+        <p><b>Birth: </b>{props.birth ? props.birth.toDateString() : new Date()}</p>
+      </div>
+    </div>
+  </>)
+
+}
+
+export default IdCard
+
+
+
