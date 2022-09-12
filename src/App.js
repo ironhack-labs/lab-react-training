677595b8789dcc4caa1c7d8f72f0e78beaf29f99
Guillaume <guillaume.chatriot@gmail.com>
2022-09-12 18:02:05 +0200

    iteration 4 finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard/IdCard';
+import Greetings from './components/Greetings/Greetings';
+import Random from './components/Random/Random';
+import BoxColor from './components/BoxColor/BoxColor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <IdCard
+          lastName="Chatriot"
+          firstName="Guillaume"
+          gender="male"
+          height={'179cm'}
+          birth={new Date('1988-11-09')}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+
+        <IdCard
+          lastName="Costa"
+          firstName="Ana"
+          gender="female"
+          height={'180cm'}
+          birth={new Date('1988-10-04')}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+
+      <div>
+        <Greetings lang={'de'} children={'Gunther'} />
+        <Greetings lang={'fr'} children={'Jacques'} />
+        <Greetings lang={'es'} children={'Pablo'} />
+        <Greetings lang={'en'} children={'Charles'} />
+      </div>
+
+      <div>
+        <Random min={1} max={6} />
+        <Random min={1} max={100} />
+      </div>
+      <div>
+        <BoxColor r={255} g={0} b={0} />
+        <BoxColor r={128} g={255} b={0} />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Greetings/Greetings.jsx
@@ -0,0 +1,31 @@
+import './Greetings.css';
+
+function Greetings(params) {
+  let correctGreeting;
+  switch (params.lang) {
+    case 'de':
+      correctGreeting = 'Hallo';
+      break;
+    case 'en':
+      correctGreeting = 'Hello';
+      break;
+    case 'es':
+      correctGreeting = 'Hola';
+      break;
+    case 'fr':
+      correctGreeting = 'Bonjour';
+      break;
+    default:
+      correctGreeting = 'Hello';
+      break;
+  }
+  return (
+    <div className="border">
+      <p>
+        {correctGreeting} {params.children}
+      </p>
+    </div>
+  );
+}
+
+export default Greetings;
