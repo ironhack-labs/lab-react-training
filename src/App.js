3baeb60aa3a74e1306b0466979630e3c6213acbd
Guillermo Cubells <guillermocubells@gmail.com>
2022-09-17 16:36:33 +0200

    feature:iteration2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import IdCard from './components/IdCard';
-import Moment from 'moment';
-
+import Greetings from './components/Greetings';
 
 function App() {
   return (
     <div className="app">
-      <IdCard
-        lastName="Doe"
-        firstName="John"
-        gender="male"
-        height={178}
-        birth={new Date('1992-07-14')}
-        picture="https://randomuser.me/api/portraits/men/44.jpg"
-      />
-      <IdCard
-        lastName="Delores "
-        firstName="Obrien"
-        gender="female"
-        height={172}
-        birth={Date('1988-05-11')}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      />
+      <div>
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height={178}
+          birth={new Date('1992-07-14')}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+      </div>
+      <div>
+        <IdCard
+          lastName="Delores "
+          firstName="Obrien"
+          gender="female"
+          height={172}
+          birth={new Date('1988-05-11')}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+      <div>
+        <Greetings lang="de">Ludwig</Greetings>
+      </div>
+      <div>
+        <Greetings lang="fr">François</Greetings>
+      </div>
     </div>
   );
 }
--- src/components/IdCard.js
+++ src/components/IdCard.js
@@ -3,11 +3,11 @@ import Moment from 'moment';
 function IdCard(props) {
   return (
     <div>
-      <p className="lastName">{props.lastName}</p>
-      <p className="firstName">{props.firstName}</p>
-      <p className="gender">{props.gender}</p>
-      <p className="height">{props.height}</p>
-      <p className="birth">{Moment(props.birth).format("MMM Do YYYY")}</p>
+      <p className="lastName">Last Name: {props.lastName}</p>
+      <p className="firstName"> First Name:{props.firstName}</p>
+      <p className="gender"> Gender: {props.gender}</p>
+      <p className="height">Height: {props.height/100}m</p>
+      <p className="birth">Birth: {Moment(props.birth).format("MMM Do YYYY")}</p>
       <img className="picture" src={props.picture} alt="picture"></img>
     </div>
   );
