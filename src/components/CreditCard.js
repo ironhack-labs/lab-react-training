a98a286915365382dab1b8f63a2abdbf54e86cd2
Guillermo Cubells <guillermocubells@gmail.com>
2022-09-17 18:19:05 +0200

    feature:iteration 5

---
+++ src/components/CreditCard.js
@@ -0,0 +1,30 @@
+function CreditCard(props) {
+  const {
+    type,
+    number,
+    expirationMonth,
+    expirationYear,
+    bank,
+    owner,
+    bgColor,
+    color,
+  } = props;
+  return (
+    <div style={{ backgroundColor: `${bgColor}`, color: `${color}` }}>
+      <div>{type}</div>
+      <div>
+        <h4>{number}</h4>
+      </div>
+      <div>
+        <h4>
+          {expirationMonth}/{expirationYear}
+        </h4>
+        <h4>{bank}</h4>
+        <h4>{type}</h4>
+        <h4>{owner}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default CreditCard;
