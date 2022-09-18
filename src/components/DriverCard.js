f11e2d517c59ade0102678788091ee43387534a8
Maxime Erkens <mhmserkens@gmail.com>
2022-09-18 15:04:23 +0200

    iteration 7

--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,3 +1,31 @@
-function DriverCard() {}
+function DriverCard({ name, rating, img, car }) {
+  const { model, licensePlate } = car;
+  const roundedRating = Math.round(rating);
+  return (
+    <div>
+      <span className="name">{name}</span>
+      {(() => {
+        switch (roundedRating) {
+          case 0:
+            return <img alt="none" src="images/No-Stars.PNG" />;
+          case 1:
+            return <img alt="one" src="images/One-Star.PNG" />;
+          case 2:
+            return <img alt="two" src="images/Two-Stars.PNG" />;
+          case 3:
+            return <img alt="three" src="images/Three-Stars.PNG" />;
+          case 4:
+            return <img alt="four" src="images/Four-Stars.PNG" />;
+          case 5:
+            return <img alt="five" src="images/Five-Stars.PNG" />;
+          default:
+            return <img alt="none" src="images/No-Stars.PNG" />;
+        }
+      })()}
+      <span className="car">{model}</span>
+      <span className="licensePlate">{licensePlate}</span>
+    </div>
+  );
+}
 
 export default DriverCard;
--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,19 +3,19 @@ function Rating({ children }) {
   //console.log(roundedNumber);
   switch (roundedNumber) {
     case 0:
-      return <img src="images/No-Stars.PNG" />;
+      return <img alt="none" src="images/No-Stars.PNG" />;
     case 1:
-      return <img src="images/One-Star.PNG" />;
+      return <img alt="one" src="images/One-Star.PNG" />;
     case 2:
-      return <img src="images/Two-Stars.PNG" />;
+      return <img alt="two" src="images/Two-Stars.PNG" />;
     case 3:
-      return <img src="images/Three-Stars.PNG" />;
+      return <img alt="three" src="images/Three-Stars.PNG" />;
     case 4:
-      return <img src="images/Four-Stars.PNG" />;
+      return <img alt="four" src="images/Four-Stars.PNG" />;
     case 5:
-      return <img src="images/Five-Stars.PNG" />;
+      return <img alt="five" src="images/Five-Stars.PNG" />;
     default:
-      return <img src="images/No-Stars.PNG" />;
+      return <img alt="none" src="images/No-Stars.PNG" />;
   }
 }
 
