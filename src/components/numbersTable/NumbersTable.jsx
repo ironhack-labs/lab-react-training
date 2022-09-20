36c660f863ebcd58f2fb40da1cc37c01e2196611
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-09-20 21:56:05 +0200

    Hasta Iteracion 14

---
+++ src/components/numbersTable/NumbersTable.jsx
@@ -0,0 +1,15 @@
+const NumbersTable = ((props) => {
+
+    let cellsList = []
+    for (let i = 1; i <= props.limit; i++) cellsList.push(i)
+    return (
+        <div className="numbersTable">
+            {cellsList.map(cell => {
+                return (<div key={cell} className="umberCell" style={{ backgroundColor: cell & 1 === 1 ? "white" : "red" }}>{cell}</div>);
+            })}
+        </div>
+    )
+
+})
+
+export default NumbersTable
