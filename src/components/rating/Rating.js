2f4a27d82a99e4471b081338690d0445afd7e13a
Iván Rodríguez <82571096+Ivan180687@users.noreply.github.com>
2022-10-14 16:46:49 +0200

    rating component mejorado

--- src/components/rating/Rating.js
+++ src/components/rating/Rating.js
@@ -1,19 +1,20 @@
 import "./Rating.css"
 
 function Rating({ children }) {
-  const star1 = (Math.round(children) < 1) ? "-o" : ""
-  const star2 = (Math.round(children) < 2) ? "-o" : ""
-  const star3 = (Math.round(children) < 3) ? "-o" : ""
-  const star4 = (Math.round(children) < 4) ? "-o" : ""
-  const star5 = (Math.round(children) < 5) ? "-o" : ""
+  const star = (Math.round(children))
+  // const star1 = (Math.round(children) < 1) ? "-o" : " text-warning"
+  // const star2 = (Math.round(children) < 2) ? "-o" : " text-warning"
+  // const star3 = (Math.round(children) < 3) ? "-o" : " text-warning"
+  // const star4 = (Math.round(children) < 4) ? "-o" : " text-warning"
+  // const star5 = (Math.round(children) < 5) ? "-o" : " text-warning"
 
   return(
     <div className="stars">
-      <i className={`star fa fa-star${star1} fa-lg`} />
-      <i className={`star fa fa-star${star2} fa-lg`} />
-      <i className={`star fa fa-star${star3} fa-lg`} />
-      <i className={`star fa fa-star${star4} fa-lg`} />
-      <i className={`star fa fa-star${star5} fa-lg`} />
+      <i className={`star fa fa-star${star < 1 ? "-o" : " text-warning"} fa-lg t`} />
+      <i className={`star fa fa-star${star < 2 ? "-o" : " text-warning"} fa-lg t`} />
+      <i className={`star fa fa-star${star < 3 ? "-o" : " text-warning"} fa-lg t`} />
+      <i className={`star fa fa-star${star < 4 ? "-o" : " text-warning"} fa-lg t`} />
+      <i className={`star fa fa-star${star < 5 ? "-o" : " text-warning"} fa-lg t`} />
     </div>
   )
 }
