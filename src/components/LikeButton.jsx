6a5bbcd61fd98b6f438a58524ac0eeb043c62e81
hvkst <bernd@hvkst.de>
2022-10-28 13:57:59 +0200

    Iteration VIII-Bonus

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react';
 
 function LikeButton({ text }) {
   const [count, setCount] = useState(0);
+  const [bgColor, setBgColor] = useState('rgba(255,100,50,0.5)');
+
+  function getRandomColor() {
+    const random255 = () => {
+      return Math.floor(Math.random() * 255);
+    };
+    return `rgba(${random255()},${random255()},${random255()},0.5)`;
+  }
 
   function handleClick() {
     setCount(count + 1);
+    setBgColor(getRandomColor());
   }
 
   return (
-    <button style={{ padding: '5px', margin: '10px' }} onClick={handleClick}>
+    <button
+      style={{ padding: '5px', margin: '10px', backgroundColor: bgColor }}
+      onClick={handleClick}
+    >
       {count} {text}
     </button>
   );
