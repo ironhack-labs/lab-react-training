d3175fe83dec374333ae3839da769bae6f1517da
Patrick-Groot <patrick@candis.io>
2022-11-01 09:53:55 +0100

    Iteration 1

---
+++ src/components/IdCard.js
@@ -0,0 +1,26 @@
+function IdCard(props) {
+  const { lastName, firstName, gender, height, birth, picture } = props;
+  return (
+    <div className="IdCard">
+      <img src={props.picture} alt={`${props.firstName} ${props.lastName}`} />
+      <div className="IdData">
+        <span className="bolder">First name: </span>
+        <span>{props.firstName}</span>
+        <br />
+        <span className="bolder">Last name: </span>
+        <span>{props.lastName}</span>
+        <br />
+        <span className="bolder">Gender: </span>
+        <span>{props.gender}</span>
+        <br />
+        <span className="bolder">Height: </span>
+        <span>{props.height}</span>
+        <br />
+        <span className="bolder">Birth: </span>
+        <span>{props.birth.toLocaleDateString()}</span>
+      </div>
+    </div>
+  );
+}
+
+export default IdCard;
