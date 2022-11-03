196ac8444c704b2d685b054115b5854fdb57bd36
Patrick-Groot <patrick@candis.io>
2022-11-03 12:42:31 +0100

    Add rgb to hex

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -1,4 +1,9 @@
 export default function BoxColor(props) {
+
+  function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
   return (
     <div
       className="box"
@@ -7,6 +12,7 @@ export default function BoxColor(props) {
       <p>
         rgb({props.r},{props.g},{props.b})
       </p>
+      <p>{rgbToHex(props.r, props.g, props.b)}</p>
     </div>
   );
 }
