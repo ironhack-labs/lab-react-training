25cd76aac7d3f8c4bc0fe828e10517a184770534
Jandson Costa <jandsoncosta55@gmail.com>
2022-11-23 13:47:08 -0300

    save app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IdCard</h1>
+      <div className="IdCard box">
+        <img src="https://randomuser.me/api/portraits/men/44.jpg" alt="" />
+        <div class="right">
+          <strong>First name</strong>
+          ": " "John"
+          <br></br>
+          <strong>Last name</strong>
+          ": " "Doe"
+          <br></br>
+          <strong>Gender</strong>
+          ": " "male"
+          <br></br>
+          <strong>Height</strong>
+          ": " "1.78" "m "<br></br>
+          <strong>Birth</strong>
+          ": " "Mon Jul 13 1992"
+          <br></br>
+        </div>
+      </div>
+      <div class="IdCard box">
+        <img src="https://randomuser.me/api/portraits/women/8.jpg" alt="" />
+        <div class ="right">
+          <strong>First name</strong>
+          ": " Obrien
+          <br></br>
+          <strong>Last name</strong>
+          ": " Delores
+          <br></br>
+          <strong>Gender</strong>
+          ": "female "<br></br>
+          <strong>Height</strong>
+          ": " "1.72" "m<br></br>
+          <strong>Birth</strong>
+          ": " Mon May 10 1993
+          <br></br>
+        </div>
+      </div>
     </div>
   );
 }
