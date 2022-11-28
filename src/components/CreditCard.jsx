a0ed9533dd81c04e716f02552a587bcef8783ab7
Juliana Gómez <julicodes29@gmail.com>
2022-11-28 23:23:29 +0100

    Ultimos arreglos

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -3,17 +3,17 @@ import './CreditCard.css'
 const CreditCard = props => {
     
 
-    const { type, number, expirationYear, bank, owner, color,bgColor } = props
+    const { type, number, expirationYear, bank, owner, color,bgColor,expirationMonth } = props
     const hashCard = `**** **** **** ${number[12]}${number[13]}${number[14]}${number[15]}`;
     
     return (
         
             <div className="CreditCard"style={{ backgroundColor: `${bgColor}`, color: `${color}` }}>,
-                <h3> Type {type}</h3>
+                <h3> {type}</h3>
                 <p> {hashCard} </p>
-                <p> {expirationYear}</p>
-                <p>  {owner}</p>
-                <p>  {bank}</p>
+            <p> {expirationMonth}/{expirationYear}</p>
+                <p>{owner}</p>
+                <p>{bank}</p>
             </div>
       
     )
