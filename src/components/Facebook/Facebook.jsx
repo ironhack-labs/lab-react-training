fca80514caae309d4bc11061eaf23d8b3b1d0726
cbrioneslopez <cbrioneslopez96@gmail.com>
2022-11-30 00:19:45 +0100

    Añadido Filtro y ordenaciones (14)

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -3,11 +3,40 @@ import { useState } from "react"
 import "./Facebook.css"
 const Facebook = () => {
 
+    const [initialProfiles, setProfiles] = useState(profiles)
+
     const [country, setCountry] = useState("")
+
+    const [filter, setFilter] = useState("")
 
     const handlEvent = e => {
         setCountry(e.target.value)
     }
+
+    const sortByName = () => {
+
+        const profilesCopy = [...initialProfiles]
+
+        const sortedByName = profilesCopy.sort((a, b) => a.firstName > b.firstName ? 1 : -1)
+
+        setProfiles(sortedByName)
+    }
+    const sortByLastName = () => {
+
+        const profilesCopy = [...initialProfiles]
+
+        const sortedByLastName = profilesCopy.sort((a, b) => a.lastName > b.lastName ? 1 : -1)
+
+        setProfiles(sortedByLastName)
+    }
+    const filterByName = (event) => {
+        setFilter(event.target.value)
+        const filteredList = initialProfiles.filter(elm =>
+            elm.firstName.toLowerCase().includes(filter) || elm.lastName.toLowerCase().includes(filter)
+        )
+        setProfiles(filteredList)
+    }
+
 
     return (
 
@@ -19,9 +48,13 @@ const Facebook = () => {
                 <button onClick={handlEvent} value="Germany">Germany</button>
                 <button onClick={handlEvent} value="Nigeria">Nigeria</button>
                 <button onClick={handlEvent} value="Sweden">Sweden</button>
+                <button onClick={sortByName}>Ordenar por Nombre</button>
+                <button onClick={sortByLastName}>Ordenar por Apellido</button>
+                <input type="text" name="" onChange={filterByName}></input>
             </div>
+
             {
-                profiles.map((profile, index) => {
+                initialProfiles.map((profile, index) => {
                     return (
                         <div key={index} className="profileCard"
                             style={country === "All" || country === profile.country ? { backgroundColor: '#A3D2E2' } : {}}
