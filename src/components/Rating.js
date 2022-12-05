4a992c0a7a559ecebf6c6879ecf8dcfd4377f91f
Adam Hannath <ahannath@gmail.com>
2022-12-05 19:47:07 +0100

    iteration 6

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,6 +1,31 @@
 const Rating = (children) => {
     let starsShown = Math.round(children)
     let rating
-    switch
+    switch (starsShown){
+        case 0:
+            rating = "☆☆☆☆☆"
+            break;
+        case 1:
+            rating = "★☆☆☆☆"
+            break;
+        case 2:
+            rating = "★★☆☆☆"
+            break; 
+        case 3:
+            rating = "★★★☆☆"
+            break; 
+        case 4:
+            rating = "★★★★☆"
+            break;
+        case 5:
+            rating = "★★★★★"
+            break;
+        default:
+            rating = "Rating System Error"
+    };
+    
+    return  (
+        <div><p className="starRating" style={{"font-size": 50, "text-align": "left"}}>{rating}</p></div>
+    )
 }
 export default Rating
