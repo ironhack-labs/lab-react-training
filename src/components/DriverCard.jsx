f4f5a843f10b8b4dc2be9ea65026eb89cb0fcf47
Necrosz <g.alexisarellano@gmail.com>
2023-02-21 23:23:51 -0600

    Alexis Arellano

---
+++ src/components/DriverCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const DriverCard = (props) => {
+
+  const { name, rating, img, car} = props
+
+
+  const filledStars = Math.round(rating);
+  const emptyStars = 5 - filledStars;
+  const filledStar = '★';
+  const emptyStar = '☆';
+
+
+  return (
+    <div className=''>
+      <div className='DriverCard'>
+      <img className='imgDriver' src={img}/>
+
+       <div>
+       <p>{name}</p>
+       {[...Array(filledStars)].map((_, i) => (
+       <span key={i}>{filledStar}</span>
+     ))}
+     {[...Array(emptyStars)].map((_, i) => (
+       <span key={i}>{emptyStar}</span>
+     ))}
+
+     <p>{car.model}-{car.licensePlate}</p>
+       </div> 
+      </div>
+
+    </div>
+  )
+}
+
+export default DriverCard
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Greetings = (props) => {
+
+  const { lang, children } = props
+
+  if (lang === "de") {
+    return (<div className='div'>
+    <p> Hallo {children}</p>
+    </div>
+  )} else {
+
+    return (<div className='div'>
+    <p>Bonjour {children}</p>
+    </div>
+  )}
+}
+
+export default Greetings
