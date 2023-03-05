28a5d24356d2f6f63f124ae9745a77d681f638a8
silvi-hdez <silviahdez.b@gmail.com>
2023-03-05 11:58:10 +0100

    feat: Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
+import Greetings from './components/Greetings';
 import IdCard from './components/IdCard';
 
 function App() {
   return (
     <div className="App">
-      <IdCard
-        lastName="Doe"
-        firstName="John"
-        gender="male"
-        height={178}
-        birth={new Date('1992-07-14').toLocaleDateString()}
-        picture="https://randomuser.me/api/portraits/men/44.jpg"
-      />
+      <div className='iteration1'>
+        <h3>Iteration 1</h3>
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height={178}
+          birth={new Date('1992-07-14').toLocaleDateString()}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
 
-      <IdCard
-        lastName="Delores"
-        firstName="Obrien"
-        gender="female"
-        height={172}
-        birth={new Date('1988-05-11').toLocaleDateString()}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      />
+        <IdCard
+          lastName="Delores"
+          firstName="Obrien"
+          gender="female"
+          height={172}
+          birth={new Date('1988-05-11').toLocaleDateString()}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
+      <div className='iteration2'>
+        <h3>Iteration 2</h3>
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
+      </div>
     </div>
   );
 }
