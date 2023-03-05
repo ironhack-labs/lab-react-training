03c4afba1f35e29161376fed3aa2cbc1d420c624
silvi-hdez <silviahdez.b@gmail.com>
2023-03-05 10:42:46 +0100

    Iteration 1 done

--- src/components/IdCard/index.jsx
+++ src/components/IdCard/index.jsx
@@ -3,12 +3,15 @@ import './index.css'
 function IdCard ({lastName, firstName, gender, height, birth, picture }) {
     return (
         <div className='IdCard'>
+            
             <img src={picture}/>
-            <p>First name: {firstName}</p>
-            <p>Last name: {lastName}</p>
-            <p>Gender: {gender}</p>
-            <p>Heigh: {height}</p>
-            <p>Birth: {birth}</p>
+            <ul>
+                <li><b>First name:</b> {firstName}</li>
+                <li><b>Last name:</b> {lastName}</li>
+                <li><b>Gender:</b> {gender}</li>
+                <li><b>Heigh:</b> {height}</li>
+                <li><b>Birth:</b> {birth}</li>
+            </ul>
         </div>
     );
 };
