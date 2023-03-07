4593105540ee19c70c2bb7d28722c3f16fcaa26d
pianattero <mariapianattero@gmail.com>
2023-03-07 19:24:36 +0100

    feat: iteration 3 done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 import Iteration1 from './iterations/iteration1';
 import Iteration2 from './iterations/iteration2';
+import Iteration3 from './iterations/iteration3';
 
 function App() {
   return (
     <div className="App">
+      <strong>ITERATION 1</strong>
       <Iteration1 />
+
+      <strong>ITERATION 2</strong>
       <Iteration2 />
+
+      <strong>ITERATION 3</strong>
+      <Iteration3 />
     </div>
   );
 }
---
+++ src/components/Random/index.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+import React from 'react';
+
+function Random({ min, max }) {
+  const randomNum = Math.floor(Math.random() * (max - min) + min);
+
+  return (
+    <div className="Random">
+      <p>
+        Random value between {min} and {max} =&gt; {randomNum}
+      </p>
+    </div>
+  );
+}
+
+export default Random;
