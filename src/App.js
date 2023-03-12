eb6e3c7064ab5f2326dcd326bf09fc808261f563
Carlos Lopez <carlopex97@gmail.com>
2023-03-13 00:00:53 +0100

    iteration 5 done full

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
-import Random from './components/Random'
+import Random from './components/Random';
 import BoxColor from './components/BoxColor';
+import CreditCard from './components/CreditCard/CreditCard';
 
 function App() {
   return (
     <div className="App ">
       <div>
-        <h1 className='ms-3'>IdCard</h1>
+        <h1 className="ms-3">IdCard</h1>
         <IdCard
           lastName="Doe"
           firstName="John"
@@ -28,20 +29,58 @@ function App() {
           picture="https://randomuser.me/api/portraits/women/44.jpg"
         />
       </div>
-      <div >
-        <h1 className='ms-3'>Greetings</h1>
+      <div>
+        <h1 className="ms-3">Greetings</h1>
         <Greetings lang="de">Ludwig</Greetings>
         <Greetings lang="fr">François</Greetings>
       </div>
-      <div >
-        <h1 className='ms-3'>Random</h1>
-        <Random min={1} max={6}/>
-        <Random min={1} max={100}/>
+      <div>
+        <h1 className="ms-3">Random</h1>
+        <Random min={1} max={6} />
+        <Random min={1} max={100} />
       </div>
-      <div >
-        <h1 className='ms-3'>BoxColor</h1>
+      <div>
+        <h1 className="ms-3">BoxColor</h1>
         <BoxColor r={255} g={0} b={0} />
-        <BoxColor r={128} g={255} b={0} />    
+        <BoxColor r={128} g={255} b={0} />
+      </div>
+      <div>
+        <h1 className="ms-3">CreditCard</h1>
+        <div className="row align-items-center">
+          <div className='col p-1'>
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white"
+        />
+</div><div className='col p-1'>
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        /></div><div className='col p-1'>
+
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white"
+        /></div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/CreditCard/CreditCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import visaImg from "../../assets/images/visa.png"
+import masterCardImg from "../../assets/images/mc_symbol_opt_63_2x.png"
+import './CreditCard.css'
+
+export default function CreditCard({type, number, expirationMonth, expirationYear, bank, owner, bgColor, color}) {
+  const cardLogo = type === "Visa" ? visaImg : masterCardImg;
+  if(expirationMonth <10){
+    expirationMonth = "0" + expirationMonth
+}
+const divStyle = {
+  backgroundColor: bgColor,
+  color: color,
+  }
+  return (
+    <div className="creditCard" style={divStyle}>
+      <div className="logoBanco">
+        <img src={cardLogo} alt="card logo" style={{width: '80px', height: '40px'}} />
+      </div>
+      <div className="targetNumber">
+        <h4>●●●● ●●●● ●●●● {number.slice(-4)}</h4>
+      </div>
+      <div className="expiration">
+        <p>Expires: {expirationMonth}/{expirationYear %100}</p>
+        <p class="bank">{bank}</p> 
+      </div>
+      <div className="owner">
+        <p>{owner}</p>
+      </div>
+    </div>
+  )
+}
