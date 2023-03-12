28077e001b86367a63789dbc253d85c0001a8c6a
Daniel Olea <oleaonize@gmail.com>
2023-03-12 11:52:31 +0100

    It.14 without bonus

--- src/components/FaceBook/FaceBook.js
+++ src/components/FaceBook/FaceBook.js
@@ -4,16 +4,27 @@ import React, { Component } from 'react';
 
 class FaceBook extends Component {
  state = {
-    profiles: profilesJSON
+    profiles: profilesJSON,
+    filter: ""
  }
 
+ handleFilter = (event) =>{
+    this.setState({filter: event.target.value})
+ }
     render() {
         const {profiles} = this.state
         return (
             <div className="FaceBook">
+            <div className="btn-container">
+            <button onClick={this.handleFilter} value="">All</button>
+            <button onClick={this.handleFilter} value="England">England</button>
+            <button onClick={this.handleFilter} value="USA">USA</button>   
+            <button onClick={this.handleFilter} value="Malaysia">Malaysia</button> 
+            <button onClick={this.handleFilter} value="Germany">Germany</button>
+            </div>
 
             {profiles.map(profile => {
-                return <div className="profilecard"> 
+                return <div key={profile.className} className="profilecard" style={profile.country === this.state.filter ? {backgroundColor: 'aquamarine'} : null}> 
                 <img src={profile.img} alt="" />
                 <div className="infocard">
                 <p><strong>First Name: </strong>{profile.firstName}</p>
