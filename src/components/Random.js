c3da789714f3334c7e49c76dd36521f861902761
Victor Lopez <victorlpz2301@gmail.com>
2023-03-13 22:28:29 +0100

    Iteration 3 Done

---
+++ src/components/Random.js
@@ -0,0 +1,17 @@
+function Random({ min, max }) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min)
+  return (
+      <div className="mb-3 border border-1 border-dark m-3">
+        <p className="m-2">Random value between {min} and {max}  =>  {randomNumber}</p>
+      </div>
+  )
+};
+
+
+Random.defaultProps = {
+  min: '',
+  max: '',
+};
+
+
+export default Random
