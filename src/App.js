a60d3b51af2b32480f68d573c45106b2e549539a
Victor Lopez <victorlpz2301@gmail.com>
2023-03-15 12:38:03 +0100

    Iteration 5 Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import BoxColor from './components/BoxColor';
+import CreditCard from './components/CreditCard';
 import Greetings from './components/Greetings';
 import IdCard from './components/IdCard';
 import Random from './components/Random';
@@ -9,34 +10,67 @@ function App() {
     <div className="App">
 
       <h1 className='text-center mt-3'>Iteration 1</h1>
-     <IdCard
-      lastName='Doe'
-      firstName='John'
-      gender='male'
-      height={178}
-      birth="1992-07-14"
-      picture="https://randomuser.me/api/portraits/men/44.jpg"
-     />
-     <IdCard
-      lastName='Delores '
-      firstName='Obrien'
-      gender='female'
-      height={172}
-      birth="1988-05-11"
-      picture="https://randomuser.me/api/portraits/women/44.jpg"
-    />
+      <IdCard
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth="1992-07-14"
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <IdCard
+        lastName='Delores '
+        firstName='Obrien'
+        gender='female'
+        height={172}
+        birth="1988-05-11"
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
 
-    <h1 className='text-center'>Iteration 2</h1>
-    <Greetings lang="de">Ludwig</Greetings>
-    <Greetings lang="fr">François</Greetings>
+      <h1 className='text-center'>Iteration 2</h1>
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
 
-    <h1 className='text-center'>Iteration 3</h1>
-    <Random min={1} max={6}/>
-    <Random min={1} max={100}/>
+      <h1 className='text-center'>Iteration 3</h1>
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
 
-    <h1 className='text-center'>Iteration 4</h1>
-    <BoxColor r={250} g={0} b={0} />
-    <BoxColor r={128} g={255} b={0} />
+      <h1 className='text-center'>Iteration 4</h1>
+      <BoxColor r={250} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
+
+      <h1 className='text-center'>Iteration 5</h1>
+      <CreditCard
+        type="Visa"
+        number="0123456789018845"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="BNP"
+        owner="Maxence Bouret"
+        bgColor="#11aa99"
+        color="white"
+      />
+      <CreditCard
+        type="Master Card"
+        number="0123456789010995"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="N26"
+        owner="Maxence Bouret"
+        bgColor="#eeeeee"
+        color="#222222"
+      />
+
+      <CreditCard
+        type="Visa"
+        number="0123456789016984"
+        expirationMonth={12}
+        expirationYear={2019}
+        bank="Name of the Bank"
+        owner="Firstname Lastname"
+        bgColor="#ddbb55"
+        color="white"
+      />
 
     </div>
   );
---
+++ src/components/CreditCard.js
@@ -0,0 +1,55 @@
+function CreditCard({ type, number, expirationMonth, expirationYear, bank, owner, bgColor, color }) {
+
+  let src = ''
+  if (type === "Visa") {
+    src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Visa_2014.svg/1024px-Visa_2014.svg.png"
+  } else if (type === "Master Card") {
+    src = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/MasterCard_early_1990s_logo.svg/1024px-MasterCard_early_1990s_logo.svg.png"
+  }
+
+
+  const divStyle = {
+    width: 70,
+  }
+
+  const formatNumber = number.replace(/\d{4}(?=\d)/g, '$& ')
+
+  const hiddenDigits = formatNumber.slice(0, -4).replace(/[0-9]/g, '*');
+  const visibleDigits = formatNumber.slice(-4);
+
+  const year = expirationYear.toString().slice(-2)
+
+  return (
+    <div className={`card m-3 shadow text-${color}`} style={{ backgroundColor: bgColor}}>
+      <div className="card-body">
+        <div className="text-end">
+        <img className="me-2 mt-2" src={src} alt="visa" style={divStyle}></img>
+        </div>
+        <div className="text-center mt-4 mb-4">
+          <h1 className="card-text">{hiddenDigits}{visibleDigits}</h1>
+        </div>
+        <div className="d-flex flex-row ms-3">
+          <p className="mb-0">Expires {expirationMonth}/{year}</p>
+          <h5 className="ms-5 mb-0" >{bank}</h5>
+        </div>
+        <div className="pt-0">
+          <h4 className="mb-3 ms-3">{owner}</h4>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+
+CreditCard.defaultProps = {
+  type: '',
+  number: '',
+  expirationMonth: '',
+  expirationYear: '',
+  bank: '',
+  owner: '',
+  bgColor: '',
+  color: '',
+};
+
+export default CreditCard
