c1e19c0009330db2a45e129af9ee94ceb71cb935
ficapp2.0 <ficapp2.0@gmail.com>
2023-03-16 18:06:22 +0100

    feat: iteration 7

--- src/App.js
+++ src/App.js
@@ -51,6 +51,8 @@ function App() {
       </div>
       <h1 className="text-start ms-3">DriveCard</h1>
       <DriverCard name="Travis Kalanick" rating={4.2} img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428" car={{ model: "Toyota Corolla Altis", licensePlate: "CO42DE" }} />
+      <DriverCard name="Dara Khosrowshahi" rating={4.9} img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg" car={{   model: "Audi A3",   licensePlate: "BE33ER" }}
+      />
     </div>
   );
 }
--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -5,10 +5,10 @@ function DriveCard({name, rating, img, car}) {
   return (
     <div className="card m-3 text-white" style={{backgroundColor: `#425cbb`}}>
       <div className="row g-0">
-        <div className="col-2 d-flex" style={{maxWidth:"150px"}}>
-          <img src={img} className="img-fluid float-start p-3" alt="..." style={{objectFit: 'cover', borderRadius: '50%'}}/>
+        <div className="col-6 d-flex justify-content-end">
+          <img src={img} className="img-fluid float-start p-3" alt="..." style={{objectFit: 'cover', borderRadius: '50%', maxWidth:"150px"}}/>
         </div>
-        <div className="col-10">
+        <div className="col-6">
           <div className="card-body text-start">
             <ul className='p-0'>
               <li style={{textTransform: 'uppercase'}}><b>{name}</b></li>
