0465f78de6dc8def0da8ded6dfec3c4f65c7b88e
Joao Vitor <joao-vitor.fede@hpe.com>
2023-03-16 22:33:48 -0300

    interaction 1 done

---
+++ src/components/IdCard.js
@@ -0,0 +1,19 @@
+
+function IdCard ({lastName, firstName, gender, height, birth, picture}) {
+    return(
+        <div className="IdCard">
+            <div className="cardPicture">
+                <img src={picture} alt='profile' />
+            </div>
+            <div className="InfoCard">
+                <p><b>First name: </b> {firstName} </p>
+                <p><b>Last Name: </b> {lastName} </p>
+                <p><b>Gender: </b> {gender} </p>
+                <p><b>Height: </b> {height} </p>
+                <p><b>Birth: </b> {birth} </p>
+            </div>
+        </div>
+    );
+};
+
+export default IdCard;
