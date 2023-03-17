60070d2d6c2dca6f15c947ac462e245f073e371d
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2023-03-17 13:05:21 +0100

    Iteration 2

--- src/components/IdCard/IdCardList.jsx
+++ src/components/IdCard/IdCardList.jsx
@@ -1,25 +1,31 @@
 import IdCard from './IdCard';
+import Greetings from './Greetings';
 
 
 function IdCardList() {
   return <section>
-  <IdCard
-  lastName='Doe'
-  firstName='John'
-  gender='male'
-  height='178'
-  birth="1992-07-14"
-  picture="https://randomuser.me/api/portraits/men/44.jpg"
-/>
+    <IdCard
+    lastName='Doe'
+    firstName='John'
+    gender='male'
+    height='178'
+    birth="1992-07-14"
+    picture="https://randomuser.me/api/portraits/men/44.jpg"
+  />
 
-<IdCard
-  lastName='Delores '
-  firstName='Obrien'
-  gender='female'
-  height='172'
-  birth="1988-05-11"
-  picture="https://randomuser.me/api/portraits/women/44.jpg"
-/>
+  <Greetings lang="de">Ludwig</Greetings>
+
+  <IdCard
+    lastName='Delores '
+    firstName='Obrien'
+    gender='female'
+    height='172'
+    birth="1988-05-11"
+    picture="https://randomuser.me/api/portraits/women/44.jpg"
+  />
+
+  <Greetings lang="fr">François</Greetings>
+
   </section>
 }
 
