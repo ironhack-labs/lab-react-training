71ae08e7e91540c45549de281c9cbf64a0efc67f
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2023-03-17 20:37:37 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard/IdCard';
+
+const cardsArray = [  
+  {  
+    lastName: 'Doe',
+    firstName: 'John',
+    gender: 'male',
+    height: 178,
+    birth: "1992-07-14",
+    picture: "https://randomuser.me/api/portraits/men/9.jpg"  
+  },
+  {    
+    lastName: 'Delores ',
+    firstName: 'Obrien',
+    gender: 'female',
+    height: 172,
+    birth: "1988-05-11",
+    picture: "https://randomuser.me/api/portraits/women/44.jpg"    
+  },  
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IdCard card={ cardsArray[0] }/>
+      <IdCard card={ cardsArray[1] }/>
     </div>
   );
 }
---
+++ src/components/IdCard/IdCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function IdCard({ card }) {
+  return (
+    <div className='card d-flex flex-row container m-3 align-items-center'>
+      <div className='m-2'><img src={card.picture} alt={card.firstName} /></div>
+      <div className='d-flex justify-content-center'>
+        <ul>
+          <li><strong>First name:</strong> {card.firstName}</li>
+          <li><strong>Last name:</strong> {card.lastName} </li>
+          <li><strong>Gender:</strong> {card.gender}</li>
+          <li><strong>Height:</strong> {card.height}</li>
+          <li><strong>Birth:</strong> {card.birth}</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default IdCard
