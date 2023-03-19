488bb495759f53d3e02292b909093e49e611fcd1
silvia Edreira <silviaedreiragg@gmail.com>
2023-03-19 12:58:03 +0100

    first iteration done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <div className="cards">
+      <div className="user">
+        <IdCard
+          birth={new Date("1992-07-14")}
+          firstName='John'
+          gender='male'
+          height={178}
+          lastName='Doe'
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+      </div>
+      
+      <div className="user">
+        <IdCard
+      lastName='Delores '
+      firstName='Obrien'
+      gender='female'
+      height={172}
+      birth={new Date("1988-05-11")}
+      picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
     </div>
+</div>
   );
 }
 
---
+++ src/components/IdCard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function IdCard({ lastName, firstName, gender, height, birth, picture }) {
+        return (
+          <div>
+            <div>
+              <img className='card-img' src={picture} alt={firstName} />
+            </div>
+            <div>
+              <p>
+                <span className='bold-text'>First Name:</span> {firstName}
+              </p>
+              <p>
+                <span className='bold-text'>Last Name:</span> {lastName}
+              </p>
+              <p>
+                <span className='bold-text'>Gender:</span> {gender}
+              </p>
+              <p>
+                <span className='bold-text'>Height:</span> {height}
+              </p>
+              <p>
+                <span className='bold-text'>Birth:</span> {birth.toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+        );
+}
+
+export default IdCard
