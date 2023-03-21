e9c1451e9dcf914cc334ad820385e460bdc4aefe
AlbaGremar <albagmarquez@gmail.com>
2023-03-21 21:10:05 +0100

    itinetation1

---
+++ src/components/IdCard.js
@@ -0,0 +1,27 @@
+function IdCard(props) {
+  return (
+    <div className="card">
+      <div>
+        <img src={props.picture} alt="image" />
+      </div>
+      <div>
+        <p><b> First Name: </b> {props.firstName}</p>
+        <p><b> Last Name: </b> {props.lastName}</p>
+        <p><b> Gender: </b> {props.gender}</p>
+        <p><b> Height: </b> {props.height}</p>
+        <p><b> Birth: </b> {props.birth}</p>
+      </div>
+    </div>
+
+  )
+}
+
+IdCard.defaultProps = {
+  firstName: '',
+  lastName: '',
+  gender: '',
+  height: '',
+  birth: '',
+}
+
+export default IdCard
