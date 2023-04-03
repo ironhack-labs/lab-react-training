619ac693442691f935311eb8295712a88d21a350
Victor Lopez <victorlpz2301@gmail.com>
2023-04-03 19:53:32 +0200

    Iteration 14 Bonus 1 Done

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -26,19 +26,27 @@ export default Facebook
 
 function Profile({ profiles, selectedCountry }) {
 
+  const [displayInfo, setDisplayInfo] = useState(false)
+
   const color = selectedCountry === profiles.country ? "bg-primary" : ""
+
+  const handleInformation = () => {
+    setDisplayInfo(!displayInfo)
+  }
 
   return (
     <div className={`d-flex flex-row mb-3 border border-1 border-dark m-3 ${color}`}>
       <div>
-        <img className="m-2" src={profiles.img} style={{ width: 150 }} alt="ProfilePicture" />
+        <img className="m-2" src={profiles.img} style={{ width: 150 }} alt="ProfilePicture" onClick={handleInformation} />
       </div>
+      {displayInfo && (<>
       <div className="mt-2 me-5">
         <p className="m-0"><b> First Name: </b> {profiles.firstName}</p>
         <p className="m-0"><b> Last Name: </b> {profiles.lastName}</p>
         <p className="m-0"><b> Country: </b> {profiles.country}</p>
         <p className="m-0"><b> Type: </b> {profiles.isStudent ? "Student" : "Teacher"}</p>
       </div>
+      </> )}
     </div>
   )
 }
