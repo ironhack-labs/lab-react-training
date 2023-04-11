b91505e24242c76f55cf710287375d09a6573503
Johannes Chorzempa <jchorzempa@posteo.org>
2023-04-11 06:31:45 +0200

    Solve iteration 13 & 14

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
@@ -14,125 +13,119 @@ import Carousel from './components/Carousel';
 import NumbersTable from './components/NumbersTable';
 import FaceBook from './components/FaceBook';
 
-
 function App() {
   return (
     <div className="App">
+      <IdCard
+        lastName="Doe"
+        firstName="John"
+        gender="male"
+        height={178}
+        birth={new Date('1992-07-14')}
+        picture="https://xsgames.co/randomusers/assets/avatars/male/29.jpg"
+      />
 
-<IdCard
-  lastName='Doe'
-  firstName='John'
-  gender='male'
-  height={178}
-  birth={new Date("1992-07-14")}
-  picture="https://xsgames.co/randomusers/assets/avatars/male/29.jpg"
-/>
+      <IdCard
+        lastName="Delores "
+        firstName="Obrien"
+        gender="female"
+        height={172}
+        birth={new Date('1988-05-11')}
+        picture="https://xsgames.co/randomusers/assets/avatars/female/29.jpg"
+      />
 
-<IdCard
-  lastName='Delores '
-  firstName='Obrien'
-  gender='female'
-  height={172}
-  birth={new Date("1988-05-11")}
-  picture="https://xsgames.co/randomusers/assets/avatars/female/29.jpg"
-/>
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+      <Greetings lang="es">Sergio</Greetings>
+      <Greetings lang="en">John</Greetings>
 
-<Greetings lang="de">Ludwig</Greetings>
-<Greetings lang="fr">François</Greetings>
-<Greetings lang="es">Sergio</Greetings>
-<Greetings lang="en">John</Greetings>
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
 
-<Random min={1} max={6}/>
-<Random min={1} max={100}/>
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
 
-<BoxColor r={255} g={0} b={0} />
-<BoxColor r={128} g={255} b={0} />
+      <CreditCard
+        type="Visa"
+        number="0123456789018845"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="BNP"
+        owner="Maxence Bouret"
+        bgColor="#11aa99"
+        color="white"
+      />
 
-<CreditCard
-  type="Visa"
-  number="0123456789018845"
-  expirationMonth={3}
-  expirationYear={2021}
-  bank="BNP"
-  owner="Maxence Bouret"
-  bgColor="#11aa99"
-  color="white"
-/>
+      <CreditCard
+        type="Master Card"
+        number="0123456789010995"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="N26"
+        owner="Maxence Bouret"
+        bgColor="#eeeeee"
+        color="#222222"
+      />
 
-<CreditCard
-  type="Master Card"
-  number="0123456789010995"
-  expirationMonth={3}
-  expirationYear={2021}
-  bank="N26"
-  owner="Maxence Bouret"
-  bgColor="#eeeeee"
-  color="#222222"
-/>
+      <CreditCard
+        type="Visa"
+        number="0123456789016984"
+        expirationMonth={12}
+        expirationYear={2019}
+        bank="Name of the Bank"
+        owner="Firstname Lastname"
+        bgColor="#ddbb55"
+        color="white"
+      />
 
-<CreditCard
-  type="Visa"
-  number="0123456789016984"
-  expirationMonth={12}
-  expirationYear={2019}
-  bank="Name of the Bank"
-  owner="Firstname Lastname"
-  bgColor="#ddbb55"
-  color="white"
-/>
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>
 
-<Rating>0</Rating>
-<Rating>1.49</Rating>
-<Rating>1.5</Rating>
-<Rating>3</Rating>
-<Rating>4</Rating>
-<Rating>5</Rating>
+      <DriverCard
+        name="Travis Kalanick"
+        rating={4.2}
+        img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+        car={{
+          model: 'Toyota Corolla Altis',
+          licensePlate: 'CO42DE',
+        }}
+      />
 
-<DriverCard
-  name="Travis Kalanick"
-  rating={4.2}
-  img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
-  car={{
-    model: "Toyota Corolla Altis",
-    licensePlate: "CO42DE"
-  }}
-/>
+      <DriverCard
+        name="Dara Khosrowshahi"
+        rating={4.9}
+        img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+        car={{
+          model: 'Audi A3',
+          licensePlate: 'BE33ER',
+        }}
+      />
 
-<DriverCard
-  name="Dara Khosrowshahi"
-  rating={4.9}
-  img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
-  car={{
-    model: "Audi A3",
-    licensePlate: "BE33ER"
-  }}
-/>
+      <LikeButton />
 
-<LikeButton />
+      <ClickablePicture
+        img="./maxence.png"
+        imgClicked="./maxence-glasses.png"
+      />
 
-<ClickablePicture
-  img='./maxence.png'
-  imgClicked='./maxence-glasses.png'
-/>
+      <Dice />
 
-<Dice />
+      <Carousel
+        images={[
+          'https://xsgames.co/randomusers/assets/avatars/male/50.jpg',
+          'https://xsgames.co/randomusers/assets/avatars/female/45.jpg',
+          'https://xsgames.co/randomusers/assets/avatars/male/8.jpg',
+          'https://xsgames.co/randomusers/assets/avatars/female/21.jpg',
+        ]}
+      />
 
-<Carousel
-  images={[
-    'https://xsgames.co/randomusers/assets/avatars/male/50.jpg',
-    'https://xsgames.co/randomusers/assets/avatars/female/45.jpg',
-    'https://xsgames.co/randomusers/assets/avatars/male/8.jpg',
-    'https://xsgames.co/randomusers/assets/avatars/female/21.jpg'
-  ]}
-/>
+      <NumbersTable limit={12} />
 
-
-<NumbersTable limit={12} />
-
-<FaceBook />
-
-
-
+      <FaceBook />
     </div>
   );
 }
--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,22 +1,69 @@
-import profiles from '../data/berlin.json'
+import profiles from '../data/berlin.json';
 
 const FaceBook = () => {
+  const selectedCard = (e) => {
+    const country = e.target.innerText;
+    const cards = document.querySelectorAll('.facebook-card');
+    cards.forEach((card) => {
+      if (card.dataset.country === country) {
+        card.style.backgroundColor = '#a3d2e2';
+      } else {
+        card.style.backgroundColor = 'white';
+      }
+    });
+  };
 
-    const faces = [...profiles]
-
+  const faceCard = [...profiles].map((face) => {
     return (
-
-        <div>
-            {faces.map((face) => {
-                <div>
-                    <img src={face.img} alt="" />
-
-                </div>
-            })}
+      <div
+        className="facebook-card"
+        key={face.firstName}
+        data-country={face.country}
+      >
+        <div className="profile-picture">
+          <img src={face.img} alt="a person's face" />
         </div>
-    )
+        <div className="face-text">
+          <span>
+            <strong>First name: </strong>
+            {face.firstName}
+          </span>
+          <span>
+            <strong>Last Name: </strong>
+            {face.lastName}
+          </span>
+          <span>
+            <strong>Country: </strong>
+            {face.country}
+          </span>
+          <span>
+            <strong>Type: </strong>
+            {face.isStudent ? 'Student' : 'Teacher'}
+          </span>
+        </div>
+      </div>
+    );
+  });
 
+  return (
+    <div>
+      <div>
+        <button onClick={selectedCard}>USA</button>
+        <button onClick={selectedCard}>England</button>
+        <button onClick={selectedCard}>Malaysia</button>
+        <button onClick={selectedCard}>Germany</button>
+        <button onClick={selectedCard}>Sweden</button>
+        <button onClick={selectedCard}>Nigeria</button>
+        <button onClick={selectedCard}>Italy</button>
+        <button onClick={selectedCard}>Scotland</button>
+        <button onClick={selectedCard}>Kazakhstan</button>
+        <button onClick={selectedCard}>Russia</button>
+        <button onClick={selectedCard}>Catalonia</button>
+        <button onClick={selectedCard}>France</button>
+      </div>
+      {faceCard}
+    </div>
+  );
+};
 
-}
-
-export default FaceBook
+export default FaceBook;
