5b0016faa37cb6ee0e11185891a4c4eedb8bdcd7
Wilkins Morales <wilkinscom@gmail.com>
2023-04-23 21:29:07 +0200

    it8 likebutton

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
-import DriverCard from './components/DriverCard';
+import LikeButton from './components/LikeButton';
 
 function App() {
   return (
     <div className="App">
-      <DriverCard
-        name="Travis Kalanick"
-        rating={4.2}
-        img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg"
-        car={{
-          model: 'Toyota Corolla Altis',
-          licensePlate: 'CO42DE',
-        }}
-      />
-
-      <DriverCard
-        name="Dara Khosrowshahi"
-        rating={4.9}
-        img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
-        car={{
-          model: 'Audi A3',
-          licensePlate: 'BE33ER',
-        }}
-      />
+     <LikeButton />
+     <LikeButton />
     </div>
   );
 }
---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState } from 'react';
+
+const LikeButton = () => {
+  const [likes, setLikes] = useState(0);
+  const [color, setColor] = useState('purple');
+
+  const colors = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
+
+  const addLike = () => {
+    setLikes((prevLikes) => prevLikes + 1);
+
+    colors.forEach((arrayColor, index) => {
+      if (color === arrayColor) {
+        if (index == colors.length - 1) {
+          setColor(colors[0]);
+        } else {
+          setColor(colors[index + 1]);
+        }
+      }
+    });
+  };
+
+  return (
+    <div>
+      <button
+        className="m-2 text-light"
+        style={{ backgroundColor: color }}
+        onClick={addLike}
+      >
+        {likes} {likes === 1 ? <span>Like</span> : <span>Likes</span>}
+      </button>
+    </div>
+  );
+};
+
+export default LikeButton;
