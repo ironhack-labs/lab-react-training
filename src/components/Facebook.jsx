d796272f498677bc9a5d16885803eca54c1cd91b
michael07200 <mickmhtong@gmail.com>
2023-04-25 21:27:03 +0200

    build: Iteration 14

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import profiles from '../data/berlin.json';
 
 const Facebook = () => {
+  const uniqleCoutry = [...new Set(profiles.map((profile) => profile.country))];
+  const options = ['All', ...uniqleCoutry];
+  const [selectedCountry, setSelectedCountry] = React.useState(options[0]);
+
+  const handleSelect = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
   return (
     <>
+      {options.map((option) => (
+        <button
+          key={option}
+          value={option}
+          onClick={handleSelect}
+          style={{
+            backgroundColor: option === selectedCountry ? 'blue' : 'white',
+          }}
+        >
+          {option}
+        </button>
+      ))}
+
       {profiles.map((profile) => (
-        <div className="card">
+        <div
+          className="card"
+          style={{
+            backgroundColor:
+              profile.country === selectedCountry ? 'blue' : 'white',
+          }}
+        >
           <img src={profile.img} alt="profile" />
           <div>
             <p>
