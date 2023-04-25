0f91f4017bdcc5fa23868e9987d1028141835481
michael07200 <mickmhtong@gmail.com>
2023-04-25 20:19:14 +0200

    build: Iteration 3

---
+++ src/components/Random.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Random = ({ min, max }) => {
+  return (
+    <div>{`Random value between ${min} and ${max} => 
+    ${Math.floor(Math.random() * (max - min) + min)}`}</div>
+  );
+};
+
+export default Random;
