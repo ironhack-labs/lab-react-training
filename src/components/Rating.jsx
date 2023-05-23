a64df9700aeacb460ce15f813c77323ccece5f89
Niroj167 <gniroj964@gmail.com>
2023-05-23 17:05:12 +0100

    lab-done

---
+++ src/components/Rating.jsx
@@ -0,0 +1,13 @@
+const Rating = ({ children }) => {
+    const maxStars = 5;
+    const fullStar = '★';
+    const emptyStar = '☆';
+  
+    const roundedRating = Math.round(children);
+    const filledStars = fullStar.repeat(roundedRating);
+    const emptyStars = emptyStar.repeat(maxStars - roundedRating);
+  
+    return <div>{filledStars}{emptyStars}</div>;
+  };
+  
+export default Rating;
