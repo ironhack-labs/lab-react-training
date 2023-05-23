67e5048ea3540a6130016baec18274eb4191fc80
Lucas Telles <lltellesss@gmail.com>
2023-05-23 14:12:50 -0300

    lab done

---
+++ src/components/BoxColor.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+    const BoxColor = ({ r, g, b }) => {
+        const background = {
+            backgroundColor: `rgb(${r}, ${g}, ${b})`,
+
+        };
+
+        return (
+            <div style={background}>
+                RGB: {r}, {g}, {b}
+            </div>
+        );
+    };
+
+    export default BoxColor;
