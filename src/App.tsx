2a1772645b6145837e0b590b878a96cf9ebf479e
laibaahmed04 <laibaahmed04@gmail.com>
2023-05-24 20:10:08 +0500

    iteration 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import Cards from "./components/id_Card";
-import "bootstrap/dist/css/bootstrap.css";
-import boy from "./assets/boy1.jpg";
-import girl from "./assets/girl1.jpg";
+import 'bootstrap/dist/css/bootstrap.css';
+import Greet from './components/Greetings';
 function display() {
-  let last_name = ["Ahmed", "Ali"];
-  let first_name = ["Laiba", "Umer"];
-  let gender = ["Female", "Male"];
-  let height = [1.7, 1.9];
-  let birth = [new Date("1990-03-23"), new Date("1990-01-14")];
-  let picture = [girl, boy];
-
   return (
     <>
-      <div>
-        {last_name.map((name, index) => (
-          <Cards
-            key={index}
-            last_name={name}
-            first_name={first_name[index]}
-            gender={gender[index]}
-            height={height[index]}
-            birth={birth[index]}
-            picture={picture[index]}
-          />
-        ))}
-      </div>
+      <Greet lang="de">Ludwig</Greet>
+      <Greet lang="fr">François</Greet>
     </>
   );
 }
