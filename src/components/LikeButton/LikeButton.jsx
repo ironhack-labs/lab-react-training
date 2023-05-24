0d4900aa21a2324183a9e57609a3e0d72410fdd0
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-05-24 09:15:22 +0200

    con botón de color

--- src/components/LikeButton/LikeButton.jsx
+++ src/components/LikeButton/LikeButton.jsx
@@ -3,20 +3,20 @@ import { useState } from 'react'
 
 const LikeButton = () => {
     const [counterLike, setCounterLike] = useState(0)
+    const [colorLike, setColorLike] = useState("")
+    const colors = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
 
     const handleCounterLike = () => {
         setCounterLike(counterLike + 1)
-        // const colors = ['purple', 'blue', 'green', 'yellow', 'orange', 'red'];
 
-        // const colorRandom = colors[Math.floor(Math.random() * 6)]
-        // const custonColor = { backgroundColor: colorRandom }
+        const colorRandom = colors[Math.floor(Math.random() * 6)]
+        setColorLike(colorRandom)
 
     }
 
     return (
         <section className="LikeButton">
-            <button onClick={(handleCounterLike)}>Count{counterLike}</button>
-            {/* <button className='button' style={custonColor} onClick={(handleCounterLike)}>Count{counterLike}</button> */}
+            <button onClick={(handleCounterLike)} style={{ backgroundColor: colorLike }}>Count{counterLike} </button>
         </section >
 
     )
