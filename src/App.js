40bd764cd028db112f110c8267c40f9caaabc54b
raqueldfrutos <raqueldfrutos@gmail.com>
2023-05-27 18:55:27 +0200

    Iteration 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard';
+import DataArray from './data/berlin.json';
+import Greetings from './components/Greetings';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IdCard
+        lastName="Doe"
+        firstName="John"
+        gender="male"
+        height={178}
+        birth={new Date('1992-07-14')}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <IdCard
+        lastName="Delores "
+        firstName="Obrien"
+        gender="female"
+        height={172}
+        birth={new Date('1988-05-11')}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
+
+      <Greetings lang="de">Ludwig</Greetings>
+      <Greetings lang="fr">François</Greetings>
+      <Greetings lang="es">Raquel</Greetings>
     </div>
   );
 }
---
+++ src/components/Greetings.jsx
@@ -0,0 +1,21 @@
+function Greetings (props) {
+
+    let greeting = "";
+    if( props.lang === "de") {
+        greeting = <span>Hallo</span>
+    } else if (props.lang ==="fr") {
+        greeting =<span>Bonjour</span>
+    } else if (props.lang === "en") {
+        greeting =<span>Hello</span>
+    } else {
+        greeting =<span>Hola</span>
+    }
+    
+    return (
+        <div className="Greeting">
+            <p>{greeting} {props.children}</p>
+        </div>
+    )
+}
+
+export default Greetings;
