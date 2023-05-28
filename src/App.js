81c295549da21765c5881c18b1d602ded543cf43
“Zuzana <zuzana@getyourguide.com>
2023-05-28 21:58:05 +0200

    Iteration 6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greetings from './components/Greetings';
 import Random from './components/Random';
 import Boxcolor from './components/Boxcolor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 function App() {
   const cardArray = [ {  
@@ -79,6 +80,14 @@ function App() {
           bgColor="#ddbb55"
           color="white" 
         />
+      </div>
+      <div>
+          <Rating>0</Rating>
+          <Rating>1.49</Rating>
+          <Rating>1.5</Rating>
+          <Rating>3</Rating>
+          <Rating>4</Rating>
+          <Rating>5</Rating>
       </div>
       </div>
   );
