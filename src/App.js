ae5de57a64e92513e58ded51b66b7ba2a6575319
“Zuzana <zuzana@getyourguide.com>
2023-05-28 21:35:18 +0200

    Iteration 5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random';
 import Boxcolor from './components/Boxcolor';
+import CreditCard from './components/CreditCard';
 
 function App() {
   const cardArray = [ {  
@@ -45,7 +46,40 @@ function App() {
         <Boxcolor r={255} g={0} b={0} />
         <Boxcolor r={128} g={255} b={0} />
       </div>
-
+      <div>
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white" 
+        />
+            
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        />
+            
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white" 
+        />
+      </div>
       </div>
   );
 }
---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,22 @@
+import visa from "../assets/images/visa.png";
+import master from "../assets/images/mastercard.png";
+
+function CreditCard(details) {
+  const typeimage = (details.type === "Visa") ? visa : master;
+  const lastFour = details.number.slice(details.number.length-4,details.number.length);
+  
+  return (
+    <div className="ccdiv" style={{
+        backgroundColor: `${details.bgColor}`, color: `${details.color}`
+      }}>
+
+      <img className="cardlogo" alt="type of card" src={typeimage}/>
+
+      <p className="cardnumbers">•••• •••• •••• {lastFour}</p>
+      <p className="smallercard">Expires {details.expirationMonth}/{details.expirationYear}    {details.bank}</p>
+      <p className="smallercard">{details.owner}</p>
+    </div>
+  )
+}
+
+export default CreditCard
