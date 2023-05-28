b51a8b30d5dad061644f403eff725bdf96123a55
“Zuzana <zuzana@getyourguide.com>
2023-05-28 17:44:42 +0200

    Iteration 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard';
 
 function App() {
+  const cardArray = [ {  
+    // id: 1,
+    lastName: "Delores",
+    firstName:'Obrien',
+    gender: 'female',
+    height: 172,
+    birth: Date("1988-05-11"),
+    picture: "https://randomuser.me/api/portraits/women/44.jpg"}
+
+, { 
+  // id: 2,
+    lastName: "Doe",
+    firstName:'John',
+    gender: 'male',
+    height: 178,
+    birth: Date("1992-07-14"),
+    picture: "https://randomuser.me/api/portraits/men/44.jpg"
+} ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+     {cardArray.map (card => {
+      return <IdCard eachCard={card}/>
+     }
+
+     )}
+    
+      </div>
+  
   );
 }
 
---
+++ src/components/IdCard.jsx
@@ -0,0 +1,18 @@
+function IdCard({eachCard}) {
+        return (
+            <div className="cardDiv">
+                <div>
+                    <img className="cardImage" src={eachCard.picture} alt="user image"/>
+                </div>
+                <div className="cardTextDiv">
+                    <p>Name: {eachCard.firstName}</p>
+                    <p>Last name: {eachCard.lastName}</p>
+                    <p>Gender: {eachCard.gender}</p>
+                    <p>Height: {eachCard.height}</p>
+                    <p>Birth: {eachCard.birth}</p>
+                </div>   
+            </div>
+  )
+}
+
+export default IdCard
