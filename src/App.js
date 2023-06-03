db4fca35c48b5be28ebafae370956f5e6c7e32d7
Wiebke Lueneburg <git@fraulueneburg.de>
2023-06-03 18:44:34 +0200

    some more styling just for fun

--- src/App.js
+++ src/App.js
@@ -50,52 +50,51 @@ function App() {
       </div>
 
       <h2>Iteration 4</h2>
-      <BoxColor r={255} g={0} b={0} />
-      <BoxColor r={128} g={255} b={0} />
+      <div className="grid">
+        <BoxColor r={255} g={0} b={0} />
+        <BoxColor r={128} g={255} b={0} />
+      </div>
 
       <h2>Iteration 3</h2>
-      <Random min={1} max={6} />
-      <Random min={1} max={100} />
+      <div className="grid">
+        <article>
+          <Random min={1} max={6} />
+        </article>
+        <article>
+          <Random min={1} max={100} />
+        </article>
+      </div>
 
       <h2>Iteration 2</h2>
-      <div class="grid">
-        <div>
-          <Greetings lang="de">Ludwig</Greetings>
-        </div>
-        <div>
-          <Greetings lang="fr">François</Greetings>
-        </div>
+      <div className="grid">
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
       </div>
-      <div class="grid">
-        <div>
-          <Greetings lang="es">Silvia</Greetings>
-        </div>
-        <div>
-          <Greetings lang="en">Shaun 😉</Greetings>
-        </div>
+      <div className="grid">
+        <Greetings lang="es">Silvia</Greetings>
+        <Greetings lang="en">Shaun 😉</Greetings>
       </div>
-      <div class="grid">
-        <div>
-          <Greetings>nameless Esperanto speaker</Greetings>
-        </div>
+      <div className="grid">
+        <Greetings>nameless Esperanto speaker</Greetings>
       </div>
 
       <h2>Iteration 1</h2>
-      <IdCard
-        lastName="Doe"
-        firstName="John"
-        height={178}
-        birth={new Date('1992-07-14')}
-        picture="https://randomuser.me/api/portraits/men/44.jpg"
-      />
-
-      <IdCard
-        lastName="Delores "
-        firstName="Obrien"
-        height={172}
-        birth={new Date('1988-05-11')}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
-      />
+      <div className="grid">
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          height={178}
+          birth={new Date('1992-07-14')}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+        <IdCard
+          lastName="Delores "
+          firstName="Obrien"
+          height={172}
+          birth={new Date('1988-05-11')}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
+      </div>
     </div>
   );
 }
