c0a922aee528f76e1fdcd12a7b72ecedc5b773ba
olgam <olgamartyniuk88@gmail.com>
2023-06-03 15:02:55 +0200

    fixing id card

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -3,11 +3,11 @@ function IdCard(props) {
       <div className="idCard-box">
         <img src={props.picture} alt={props.firstName} />
         <div className="text-data-idCard">
-            <p>First name: {props.firstName}</p>
-            <p>Last name: {props.lastName}</p>
-            <p>Gender: {props.gender}</p>
-            <p>Height: {props.height} cm</p>
-            <p>Birth: {props.birth}</p>
+            <p><span className="text-bold">First name: </span><span>{props.firstName}</span></p>
+            <p><span className="text-bold">Last name: </span><span>{props.lastName}</span></p>
+            <p><span className="text-bold">Gender: </span><span>{props.gender}</span></p>
+            <p><span className="text-bold">Height: </span><span>{props.height} cm</span></p>
+            <p><span className="text-bold">Birth: </span><span>{props.birth}</span></p>
         </div>
       </div>
     );
