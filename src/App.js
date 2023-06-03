4536f24deb08b1c635bccc58061fa4f06df2b6ac
“Zuzana <zuzana@getyourguide.com>
2023-06-03 15:08:55 +0200

    iteration 10 - carousel

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DriverCard from './components/DriverCard';
 import LikeButton from "./components/LikeButton"
 import ClickablePicture from "./components/ClickablePicture"
 import Dice from "./components/Dice"
+import Carousel from "./components/Carousel"
 
 function App() {
   const cardArray = [ {  
@@ -123,6 +124,16 @@ function App() {
 
             {/* Iteration 10 - Dice */}
             <Dice />
+
+            {/* Iteration 11 - Carousel */}
+            <Carousel
+              images={[
+                'https://randomuser.me/api/portraits/women/1.jpg',
+                'https://randomuser.me/api/portraits/men/1.jpg',
+                'https://randomuser.me/api/portraits/women/2.jpg',
+                'https://randomuser.me/api/portraits/men/2.jpg'
+              ]}
+            />
 
           
       </div>
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+
+function Carousel({images}) {
+    const [index, setIndex] = useState(0);
+    const goRight = () => {setIndex( (index + 1) % images.length-1) };
+    const goLeft = () => { setIndex ( (index - 1 + images.length ) % images.length )};
+
+  return (
+    <div>
+      <button onClick={goLeft}>Left</button>
+      <img alt="carousel" src={images[index]}/>
+      <button onClick={goRight}>Right</button>
+    </div>
+  )
+}
+
+export default Carousel
