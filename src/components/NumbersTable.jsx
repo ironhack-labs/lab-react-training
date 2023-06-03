5991a8951a819a836fa0164b3068fd718d4b4e4f
Wiebke Lueneburg <git@fraulueneburg.de>
2023-06-04 00:10:24 +0200

    finished iteration 12

---
+++ src/components/NumbersTable.jsx
@@ -0,0 +1,17 @@
+function NumbersTable({ limit }) {
+  const numArr = [];
+  for (let i = 1; i <= limit; i++) {
+    numArr.push(i);
+  }
+  console.log(numArr);
+
+  return (
+    <div className="numbers-table grid">
+      {numArr.map((num) => (
+        <article key={num}>{num}</article>
+      ))}
+    </div>
+  );
+}
+
+export default NumbersTable;
