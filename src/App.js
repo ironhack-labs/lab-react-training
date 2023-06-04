9537f03b6c442ad13e393e3b223b6aa6775ad3c4
MaaX59 <Maax.strandberg@gmail.com>
2023-06-04 18:41:57 +0200

    it 7 fini

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Random from './components/Random.jsx'
 import BoxColor from './components/BoxColor.jsx'
 import CreditCard from './components/CreditCard.jsx';
 import Rating from './components/Rating';
+import DriverCard from './components/DriverCard';
 
 
 function App() {
@@ -90,6 +91,26 @@ function App() {
       <Rating>3</Rating>
       <Rating>4</Rating>
       <Rating>5</Rating>
+
+      {/* Iteration VII */}
+
+      <DriverCard
+        name="Travis Kalanick"
+        rating={4.2}
+        img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+        car={{
+          model: "Toyota Corolla Altis",
+          licensePlate: "CO42DE"
+        }} />
+      <DriverCard
+  name="Dara Khosrowshahi"
+  rating={4.9}
+  img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+  car={{
+    model: "Audi A3",
+    licensePlate: "BE33ER"
+  }}
+/>
       
       
 
---
+++ src/components/DriverCard.jsx
@@ -0,0 +1,53 @@
+
+function DriverCard(props) {
+
+    const roundedNumber = Math.round(props.rating);
+
+    let star = ""
+    switch (roundedNumber) {
+        case 1:
+            star = "★☆☆☆☆";
+            break;
+        case 2:
+            star = "★★☆☆☆";
+            break;
+        case 3:
+            star = "★★★☆☆"
+            break;
+        case 4:
+            star = "★★★★☆"
+            break;
+        case 5:
+           star = "★★★★★"
+            break;
+        default:
+            star = "☆☆☆☆☆"
+        }
+
+
+    console.log(roundedNumber)
+    console.log(props);
+
+    return (
+        <div className="driverCard">
+            <div className="photo">
+                <img src={props.img} alt={props.firstName}/>
+
+            </div>
+            <div className="info">
+                <h4>{props.name}</h4>
+                <h4>{star}</h4>
+                <h4>{props.car.model}{props.car.licensePlate}</h4>
+                  
+
+            </div>
+
+
+
+
+
+        </div>
+    )
+
+}
+export default DriverCard;
