a4048d31ac95cd99183ac09962a638db80856cda
Nicolas_M <nicolas.meot@live.fr>
2023-06-10 15:12:02 +0200

    4th iteration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings'
 import Random from './components/Random';
+import BoxColor from './components/BoxColor';
 
 function App() {
   return (
@@ -27,6 +28,8 @@ function App() {
       <Greetings lang="fr">François</Greetings>
       <Random min={1} max={6}/>
       <Random min={1} max={100}/>
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
