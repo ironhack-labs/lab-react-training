44864db61b80b55b18bec269087188470a2fc866
Nicolas_M <nicolas.meot@live.fr>
2023-06-17 15:10:27 +0200

    6th iteration

--- src/App.js
+++ src/App.js
@@ -1,69 +1,78 @@
 import './App.css';
 import IdCard from './components/IdCard';
-import Greetings from './components/Greetings'
+import Greetings from './components/Greetings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 function App() {
   return (
     <div className="App">
       <IdCard
-        lastName='Doe'
-        firstName='John'
-        gender='male'
+        lastName="Doe"
+        firstName="John"
+        gender="male"
         height={178}
-        birth={new Date("1992-07-14")}
+        birth={new Date('1992-07-14')}
         picture="https://randomuser.me/api/portraits/men/44.jpg"
       />
       <IdCard
-        lastName='Delores '
-        firstName='Obrien'
-        gender='female'
+        lastName="Delores "
+        firstName="Obrien"
+        gender="female"
         height={172}
-        birth={new Date("1988-05-11")}
+        birth={new Date('1988-05-11')}
         picture="https://randomuser.me/api/portraits/women/44.jpg"
       />
       <Greetings lang="de">Ludwig</Greetings>
       <Greetings lang="fr">François</Greetings>
-      <Random min={1} max={6}/>
-      <Random min={1} max={100}/>
+      <Random min={1} max={6} />
+      <Random min={1} max={100} />
       <BoxColor r={255} g={0} b={0} />
       <BoxColor r={128} g={255} b={0} />
-      <div className='creditCard'>
-      <CreditCard
-  type="Visa"
-  number="0123456789018845"
-  expirationMonth={3}
-  expirationYear={2021}
-  bank="BNP"
-  owner="Maxence Bouret"
-  bgColor="#11aa99"
-  color="white" 
-/>
-    
-<CreditCard
-  type="Master Card"
-  number="0123456789010995"
-  expirationMonth={3}
-  expirationYear={2021}
-  bank="N26"
-  owner="Maxence Bouret"
-  bgColor="#eeeeee"
-  color="#222222"
-/>
-    
-<CreditCard
-  type="Visa"
-  number="0123456789016984"
-  expirationMonth={12}
-  expirationYear={2019}
-  bank="Name of the Bank"
-  owner="Firstname Lastname"
-  bgColor="#ddbb55"
-  color="white" 
-/>
-</div>
+      <div className="creditCard">
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white"
+        />
+
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        />
+
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white"
+        />
+      </div>
+      <div className='ratings'>
+        <div className="rate"><Rating >0</Rating></div>  
+        <div className="rate"><Rating >1.49</Rating> </div>
+        <div className="rate"><Rating >1.5</Rating></div>
+        <div className="rate"><Rating >3</Rating></div>
+        <div className="rate"><Rating >4</Rating> </div>
+        <div className="rate"><Rating >5</Rating></div>
+      </div>
     </div>
   );
 }
