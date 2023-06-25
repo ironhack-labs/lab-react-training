b7fc95c81834e75d065f7780f89e7e9b0463a5a9
gtildis <gretil_15@hotmail.com>
2023-06-25 15:02:19 +0200

    Iteration 6 | Component: Rating

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Greetings from './components/Greatings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 function App() {
   return (
     <div className="App">
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>
       <Random min={1} max={6} />
       <Random min={1} max={100} />
       <CreditCard
