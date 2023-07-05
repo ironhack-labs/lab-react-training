10beab6c0790a4a79acb8ebf637479dc442c81dd
jonp001 <jonperez01@outlook.com>
2023-07-05 14:09:51 -0400

    iteration 6 done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Greetings from './components/Greetings';
 import Random from './components/Random';
 import BoxColor from './components/BoxColor';
 import CreditCard from './components/CreditCard';
+import Rating from './components/Rating';
 
 function App() {
   return (
@@ -73,6 +74,13 @@ function App() {
   bgColor="#ddbb55"
   color="white" 
 />
+
+<Rating>0</Rating>
+<Rating>1.49</Rating>
+<Rating>1.5</Rating>
+<Rating>3</Rating>
+<Rating>4</Rating>
+<Rating>5</Rating>
 
     </div>
   );
