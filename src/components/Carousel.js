4ec0706da57f40d5e9f04bea2c30b1d21ab566dd
jonp001 <jonperez01@outlook.com>
2023-07-11 17:00:46 -0400

    iteration 11 completed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function Carousel() {
+export default function Carousel( {images} ) {
+
+
+  const [currentImageIndex, setImageIndex] = useState(() => 0);
+
+  const handleLeftClick = () => {
+    setImageIndex(prevIndex => (prevIndex === 0 ? images.length - 1 : prevIndex -1 ));
+  };
+  
+
+  const handleRightClick = () => {
+    setImageIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1 ));
+  }
+
+
+
   return (
-    <div>Carousel</div>
+<div className="carousel">
+  <img src={images[currentImageIndex]} alt="a person" />
+  <button onClick={handleLeftClick}>Left</button>
+  <button onClick={handleRightClick}>Right</button>
+</div>
   )
 }
