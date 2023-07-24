0178f627ded391584d663a996cc0f29611143b25
Walter Bernal <wltrbm@gmail.com>
2023-07-24 00:06:01 -0700

    Iteration 5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import IdCard from './components/IdCard'
 import Greetings from './components/Greetings';
 import Random from './components/Random';
-import BoxColor from './components/BoxColor';
+import BoxColor from './components/BoxColor'
+import CreditCard from './components/CreditCard'
 
 function App() {
   return (
@@ -21,6 +22,17 @@ function App() {
       <Random min={5} max={10} />
 
       <BoxColor r={128} g={255} b={0} />
+
+      <CreditCard
+        type="Master Card"
+        number="0123456789010995"
+        expirationMonth={3}
+        expirationYear={2021}
+        bank="N26"
+        owner="Maxence Bouret"
+        bgColor="#eeeeee"
+        color="#222222"
+      />
     </div>
   );
 }
---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,43 @@
+
+
+const CreditCard = (props) => {
+  const {
+    type,
+    number,
+    expirationMonth,
+    expirationYear,
+    bank,
+    owner,
+    bgColor,
+    color,
+  } = props
+
+  const logoSrc = type === 'Visa' ? 'visa.png' : 'master-card.svg'
+  const styleSetup = {
+    backgroundColor: `${bgColor}`,
+    color: `${color}`
+  }
+
+  return (
+    <div className="CreditCard" style={styleSetup}>
+      <div className="card-logo">
+        <img src={`${process.env.PUBLIC_URL}/${logoSrc}`} alt="logo" />
+      </div>
+
+      <div classNanme="card-number">
+        <span>⏺⏺⏺⏺ ⏺⏺⏺⏺ ⏺⏺⏺⏺ {number.slice(12)}</span>
+      </div>
+      <div className="card-data">
+        <span>
+          Expires {String(expirationMonth)}/{String(expirationYear).slice(2)}
+        </span>
+        <span>{bank}</span>
+      </div>
+      <div className="card-owner">
+        <span>{owner}</span>
+      </div>
+    </div>
+  );
+}
+
+export default CreditCard
