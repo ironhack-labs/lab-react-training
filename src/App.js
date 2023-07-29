849355193166ba53315fb5aede3c436c2243b8aa
Walter Bernal <wltrbm@gmail.com>
2023-07-29 09:48:03 -0700

    Iteration 10

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Rating from './components/Rating'
 import DriverCard from './components/DriverCard'
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
+import Dice from './components/Dice';
 
 function App() {
   const scoreToStars = (rating) => {
@@ -110,7 +111,9 @@ function App() {
       <LikeButton />
       <LikeButton />
 
-      <ClickablePicture img='maxence.png' imgClicked='maxence-glasses.png'/>
+      <ClickablePicture img='maxence.png' imgClicked='maxence-glasses.png' />
+      
+      <Dice/>
     </div>
   );
 }
