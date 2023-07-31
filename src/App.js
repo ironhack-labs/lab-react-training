4717533f12e6de2be8a438266a5f4e379b948ac2
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-07-31 12:22:50 +0100

    Iteration 4- Without Bonus

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random';
+import BoxColor from './components/BoxColor';
 
 function App() {
 
@@ -30,6 +31,9 @@ function App() {
 
       <Random min={1} max={6}/>
       <Random min={1} max={100}/>
+
+      <BoxColor r={255} g={0} b={0} />
+      <BoxColor r={128} g={255} b={0} />
     </div>
   );
 }
