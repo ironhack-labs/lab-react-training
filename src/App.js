c561c1a958cea4c26321cd6eb98c591db996da5c
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-08-01 15:52:34 +0100

    Iteration 10

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Rating from './components/Rating';
 import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
+import Dice from './components/Dice';
 
 function App() {
 
@@ -115,6 +116,8 @@ function App() {
   img='maxence.png'
   imgClicked='maxence-glasses.png'
 />
+
+<Dice />
     </div>
   );
 }
---
+++ src/components/Dice.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import dice1 from '../assets/images/dice1.png';
+import dice2 from '../assets/images/dice2.png';
+import dice3 from '../assets/images/dice3.png';
+import dice4 from '../assets/images/dice4.png';
+import dice5 from '../assets/images/dice5.png';
+import dice6 from '../assets/images/dice6.png';
+import diceEmpty from '../assets/images/dice-empty.png';
+
+const Dice = () => {
+    
+    const [dice, setDice] = useState(dice3)
+
+    const dices = [dice1, dice2, dice3, dice4, dice5, dice6]
+
+    const handleClick = () => {
+        setDice(diceEmpty)
+
+        setTimeout(() => {
+            setDice(dices[Math.floor(Math.random() * dices.length)])
+        }, 1000)
+    }
+    
+    return <img src={dice} onClick={handleClick} width="60px"/>
+}
+
+export default Dice;
