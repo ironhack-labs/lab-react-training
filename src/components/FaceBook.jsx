06963903c136715b534463e0e1aebbbeabdd3b5f
Andres Franco <andresfranla@gmail.com>
2023-08-03 12:08:33 -0600

    Iteration 14 done

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -1,8 +1,27 @@
+import { useState } from 'react'
 import berlinDataJSON from '../data/berlin.json'
 
 function FaceBook() {
+  const [filter,setFilter] = useState('...')
+  const countries = [ "All", "England", "USA", "Malaysia", "Germany", "..." ]
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value)
+  }
+
   return (
     <div className="FaceBook">
+      <div className='filterButtons'>
+        {countries.map((country, i) => {
+          <button
+          key={country + 1}
+          value={country}
+          onClick={handleFilter}
+          style={country === filter ? {backgroundColor: 'lightcyan'} : {backgroundColor:'white'}}>
+            {country}
+          </button>
+        })}
+      </div>
       {
         berlinDataJSON.map((profile, i) => {
           const { firstName, lastName, country, img, isStudent } = profile
