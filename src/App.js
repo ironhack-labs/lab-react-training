3460d09540ddf789a5f798b98bf3f152c6ef77c6
Andres Franco <andresfranla@gmail.com>
2023-08-03 12:14:02 -0600

    Iteration 15 done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Dice from './components/Dice';
 import Carousel from './components/Carousel';
 import NumbersTable from './components/NumbersTable';
 import FaceBook from './components/FaceBook';
+import SignupPage from './components/SignupPage';
 
 const greetings = [
   { lang: 'en' },
@@ -134,6 +135,7 @@ function App() {
   <NumbersTable limit={12} />
 
   <FaceBook/>
+  <SignupPage/>
     </div>
   );
 }
