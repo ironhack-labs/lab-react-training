9d9c1ef0a4d4b90b0321059b95d233df0a276cb2
Isabella Paiva <isabellanpaiva@gmail.com>
2023-08-21 20:12:59 +0200

    completed

---
+++ react-trainning/src/components/Greetings/Greetings.jsx
@@ -0,0 +1,33 @@
+const Greetings = ({ lang, children }) => {
+
+    let greeting = ''
+
+    switch (lang) {
+
+        case 'de':
+            greeting = 'Hallo';
+            break
+
+        case 'en':
+            greeting = 'Hello';
+            break
+
+        case 'es':
+            greeting = 'Hola';
+            break
+
+        case 'fr':
+            greeting = 'Bonjour';
+            break
+    }
+
+    return (
+        <>
+            <div>
+                <p> {greeting}, {children}</p>
+            </div>
+        </>
+    )
+}
+
+export default Greetings 
