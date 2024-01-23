4bd4ed3e32420022ca31094bbc0033dff9221886
Christian Doeller <mail@christiandoeller.de>
2024-01-23 12:08:56 +0100

    halfway through iteration 14, this lab is too huge.

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import profilesArray from '../data/berlin.json';
 
 function FaceBook() {
-  //   console.log(profiles);
+  const [profilesToDisplay, setProfilesToDisplay] = useState(profilesArray);
 
+  //   console.log(profiles);
   /*
 [
   {"firstName": "Aaron", 
@@ -12,34 +14,100 @@ function FaceBook() {
   "isStudent": false},
   */
 
-  return profilesArray.map((element) => {
-    return (
-      <div className="facebook-container" key={element.img}>
-        <div className="facebook-image-container">
-          <img className="facebook-image" src={element.img} alt="" />
+  // display contacts
+  const displayFacebook = () => {
+    return profilesToDisplay.map((element) => {
+      return (
+        <div className="facebook-container" key={element.img}>
+          <div className="facebook-image-container">
+            <img className="facebook-image" src={element.img} alt="" />
+          </div>
+          <div className="facebook-information-container">
+            <ul>
+              <li>
+                <span className="facebook-list-headline">First Name:</span>
+                {element.firstName}
+              </li>
+              <li>
+                <span className="facebook-list-headline">Last Name: </span>
+                {element.lastName}{' '}
+              </li>
+              <li>
+                <span className="facebook-list-headline">Country: </span>
+                {element.country}
+              </li>
+              <li>
+                <span className="facebook-list-headline">Type: </span>
+                {element.isStudent ? 'Student' : 'Teacher'}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="facebook-information-container">
-          <ul>
-            <li>
-              <span className="facebook-list-headline">First Name:</span>
-              {element.firstName}
-            </li>
-            <li>
-              <span className="facebook-list-headline">Last Name: </span>
-              {element.lastName}{' '}
-            </li>
-            <li>
-              <span className="facebook-list-headline">Country: </span>
-              {element.country}
-            </li>
-            <li>
-              <span className="facebook-list-headline">Type: </span>
-              {element.isStudent ? 'Student' : 'Teacher'}
-            </li>
-          </ul>
-        </div>
-      </div>
+      );
+    });
+  };
+
+  // filter profiles
+  const filterByCountry = (country) => {
+    setProfilesToDisplay(
+      profilesArray.filter((element) => {
+        if (country === 'all') {
+          return element;
+        } else {
+          return element.country === country;
+        }
+      })
     );
-  });
+  };
+
+  // return component
+  return (
+    <div className="facebook-page">
+      <div className="sort-button-container">
+        <button
+          className="country-sort-button"
+          onClick={() => {
+            filterByCountry('all');
+          }}
+        >
+          All
+        </button>
+        <button
+          className="country-sort-button"
+          onClick={() => {
+            filterByCountry('England');
+          }}
+        >
+          England
+        </button>
+        <button
+          className="country-sort-button"
+          onClick={() => {
+            filterByCountry('USA');
+          }}
+        >
+          USA
+        </button>
+        <button
+          className="country-sort-button"
+          onClick={() => {
+            filterByCountry('Malaysia');
+          }}
+        >
+          Malaysia
+        </button>
+        <button
+          className="country-sort-button"
+          onClick={() => {
+            filterByCountry('Germany');
+          }}
+        >
+          Germany
+        </button>
+        <button className="country-sort-button">...</button>
+      </div>
+      {displayFacebook()}
+    </div>
+  );
 }
 export default FaceBook;
