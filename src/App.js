79e5fc95565ff5523d26838a6970243a917236a7
Arnau Ramos <arnauramos21@gmail.com>
2024-03-05 14:25:35 +0100

    changed ratings values in app.js

--- src/App.js
+++ src/App.js
@@ -77,8 +77,8 @@ function App() {
       </div>
       <h2>Ratings</h2>
       <Rating>0</Rating>
-      <Rating>1</Rating>
-      <Rating>2</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
       <Rating>3</Rating>
       <Rating>4</Rating>
       <Rating>5</Rating>
